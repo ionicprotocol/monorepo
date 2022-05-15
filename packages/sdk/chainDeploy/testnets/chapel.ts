c18e26226a5667abfef9b870cdf8035530031dce
carlomazzaferro <carlo.mazzaferro@gmail.com>
2022-05-15 09:44:10 +0200

    move all assets to its own module

--- packages/sdk/chainDeploy/testnets/chapel.ts
+++ packages/sdk/chainDeploy/testnets/chapel.ts
@@ -1,81 +1,61 @@
-import { ChainDeployConfig, ChainlinkFeedBaseCurrency, deployChainlinkOracle, deployUniswapOracle } from "../helpers";
+import {
+  ChainDeployConfig,
+  ChainlinkFeedBaseCurrency,
+  deployChainlinkOracle,
+  deployUniswapOracle,
+} from "../helpers";
 import { ethers } from "ethers";
-import { Asset, ChainlinkAsset } from "../helpers/types";
+import { ChainlinkAsset } from "../helpers/types";
+import { assetSymbols, chapelAssets } from "../../src/chainConfig/assets";
+import { SupportedAsset } from "../../src/Fuse/types";
 
-const CHAPEL_WTOKEN = "0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd";
 
 export const deployConfig: ChainDeployConfig = {
-  wtoken: CHAPEL_WTOKEN,
+  wtoken: chapelAssets.find((a: SupportedAsset) => a.symbol === assetSymbols.WBNB)!.underlying,
   nativeTokenUsdChainlinkFeed: "0x2514895c72f50D8bd4B4F9b1110F0D6bD2c97526",
   nativeTokenName: "Binance Coin Token (Testnet)",
   nativeTokenSymbol: "TBNB",
-  stableToken: "0xeD24FC36d5Ee211Ea25A80239Fb8C4Cfd80f12Ee",
-  wBTCToken: "0x6ce8dA28E2f864420840cF74474eFf5fD80E65B8",
+  stableToken: chapelAssets.find((a: SupportedAsset) => a.symbol === assetSymbols.BUSD)!.underlying,
+  wBTCToken: chapelAssets.find((a: SupportedAsset) => a.symbol === assetSymbols.BTCB)!.underlying,
   blocksPerYear: 20 * 24 * 365 * 60,
   uniswap: {
     hardcoded: [
       {
         name: "Binance Bitcoin",
         symbol: "BTCB",
-        address: "0x6ce8dA28E2f864420840cF74474eFf5fD80E65B8",
+        address: chapelAssets.find((a: SupportedAsset) => a.symbol === assetSymbols.BTCB)!.underlying,
       },
     ],
     uniswapData: [],
     // see: https://bsc.kiemtienonline360.com/ for addresses
-    pairInitHashCode: ethers.utils.hexlify("0xecba335299a6693cb2ebc4782e74669b84290b6378ea3a3873c7231a8d7d1074"),
+    pairInitHashCode: ethers.utils.hexlify(
+      "0xecba335299a6693cb2ebc4782e74669b84290b6378ea3a3873c7231a8d7d1074"
+    ),
     uniswapV2RouterAddress: "0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3",
     uniswapV2FactoryAddress: "0xB7926C0430Afb07AA7DEfDE6DA862aE0Bde767bc",
     uniswapOracleInitialDeployTokens: [
       {
-        token: "0x8a9424745056Eb399FD19a0EC26A14316684e274", // DAI
-        baseToken: CHAPEL_WTOKEN,
+        token: chapelAssets.find((a: SupportedAsset) => a.symbol === assetSymbols.DAI)!.underlying,
+        baseToken: chapelAssets.find((a: SupportedAsset) => a.symbol === assetSymbols.WBNB)!.underlying,
       },
       {
-        token: "0xDAcbdeCc2992a63390d108e8507B98c7E2B5584a", // SAFEMOON
-        baseToken: CHAPEL_WTOKEN,
+        token: chapelAssets.find((a: SupportedAsset) => a.symbol === assetSymbols.SAFEMOON)!.underlying,
+        baseToken: chapelAssets.find((a: SupportedAsset) => a.symbol === assetSymbols.WBNB)!.underlying,
       },
       {
-        token: "0x7ef95a0FEE0Dd31b22626fA2e10Ee6A223F8a684", // USDT
-        baseToken: "0x78867BbEeF44f2326bF8DDd1941a4439382EF2A7", // BUSD
+        token: chapelAssets.find((a: SupportedAsset) => a.symbol === assetSymbols.USDT)!.underlying,
+        baseToken: chapelAssets.find((a: SupportedAsset) => a.symbol === assetSymbols.BUSD)!.underlying,
       },
     ],
   },
 };
 
-export const assets: Asset[] = [
-  {
-    symbol: "BUSD",
-    underlying: "0x78867BbEeF44f2326bF8DDd1941a4439382EF2A7",
-    name: "Binance USD",
-    decimals: 18,
-  },
-  {
-    symbol: "BTCB",
-    underlying: "0x6ce8da28e2f864420840cf74474eff5fd80e65b8",
-    name: "Binance BTC",
-    decimals: 18,
-  },
-  {
-    symbol: "DAI",
-    underlying: "0xEC5dCb5Dbf4B114C9d0F65BcCAb49EC54F6A0867",
-    name: "Binance DAI",
-    decimals: 18,
-  },
-  {
-    symbol: "ETH",
-    underlying: "0x8babbb98678facc7342735486c851abd7a0d17ca",
-    name: "Binance ETH",
-    decimals: 18,
-  },
-  {
-    symbol: "USDT",
-    underlying: "0x7ef95a0FEE0Dd31b22626fA2e10Ee6A223F8a684",
-    name: "Binance Tether",
-    decimals: 18,
-  },
-];
-
-export const deploy = async ({ run, ethers, getNamedAccounts, deployments }): Promise<void> => {
+export const deploy = async ({
+  run,
+  ethers,
+  getNamedAccounts,
+  deployments,
+}): Promise<void> => {
   ////
   //// ORACLES
   const chainlinkAssets: ChainlinkAsset[] = [
@@ -102,13 +82,19 @@ export const deploy = async ({ run, ethers, getNamedAccounts, deployments }): Pr
     getNamedAccounts,
     deployments,
     deployConfig,
-    assets,
+    assets: chapelAssets,
     chainlinkAssets,
     run,
   });
   ////
 
   //// Uniswap Oracle
-  await deployUniswapOracle({ run, ethers, getNamedAccounts, deployments, deployConfig });
+  await deployUniswapOracle({
+    run,
+    ethers,
+    getNamedAccounts,
+    deployments,
+    deployConfig,
+  });
   ////
 };
