7a78e81ff8da5a5986e0509dd72efdb44975ba38
pczek <pczek@users.noreply.github.com>
2022-05-30 17:57:35 +0200

    imports new font

--- packages/ui/pages/_document.tsx
+++ packages/ui/pages/_document.tsx
@@ -13,8 +13,10 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Manrope:wght@300;400;500;600;700;800&amp;display=swap"
+            href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
             rel="stylesheet"
           />
           <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
