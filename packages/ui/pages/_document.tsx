f7c358e7650510d30e4bc2bdcf97ab0be433837f
pczek <839848+peetzweg@users.noreply.github.com>
2022-05-31 10:36:18 +0200

    adds new favicon and let's it work with metamask closes #26

--- packages/ui/pages/_document.tsx
+++ packages/ui/pages/_document.tsx
@@ -19,14 +19,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
             rel="stylesheet"
           />
-          <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-          <link rel="manifest" href="/favicon/site.webmanifest" />
-          <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#c2b487" />
-          <meta name="msapplication-TileColor" content="#282828" />
-          <meta name="theme-color" content="#282828"></meta>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
         </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
