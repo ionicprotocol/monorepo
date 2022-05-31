a41166cdb10532ffa7c77b3bb92a3c9a72989981
smartdevcom <smart.dev.cop@gmail.com>
2022-05-31 10:18:45 +1000

    added banner component

--- packages/ui/components/shared/Layout.tsx
+++ packages/ui/components/shared/Layout.tsx
@@ -1,6 +1,7 @@
 import { Container } from '@chakra-ui/react';
 import { ReactNode } from 'react';
 
+import { Banner } from '@ui/components/shared/Banner';
 import LoadingOverlay from '@ui/components/shared/LoadingOverlay';
 import { useRari } from '@ui/context/RariContext';
 import { useColors } from '@ui/hooks/useColors';
@@ -21,6 +22,12 @@ const Layout = ({ children }: { children: ReactNode }) => {
         crossAxisAlignment="center"
         bgColor={cPage.primary.bgColor}
       >
+        <Banner
+          text="Midas just launched, use at your own risk. "
+          linkText="Read about Smart Contract Risk here."
+          linkUrl="https://www.icba.org/newsroom/blogs/main-street-matters/2021/11/12/the-challenges-and-risks-of-smart-contracts"
+          status="warning"
+        ></Banner>
         <Container maxWidth="8xl">
           <Column
             width={isMobile ? '100%' : '96%'}
