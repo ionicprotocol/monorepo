f2c6815b1e41badd1a3296896facc2a6f6e54f3c
pczek <839848+peetzweg@users.noreply.github.com>
2022-06-03 15:35:47 +0200

    fix createMarket hh Task

--- packages/sdk/tasks/createMarket.ts
+++ packages/sdk/tasks/createMarket.ts
@@ -1,5 +1,8 @@
 import { task, types } from "hardhat/config";
 
+import { DelegateContractName, MarketPluginConfig } from "../dist/esm/src";
+import { RewardsPluginConfig, StandardPluginConfig } from "../dist/esm/src/types";
+
 // npx hardhat market:create --asset-config Test,deployer,CErc20Delegate,0x90e68fdb102c850D852126Af8fd1419A07636cd7,0x6c7De8de3d8c92246328488aC6AF8f8E46A1628f,0.1,0.9,1,0,true,"","","" --network localhost
 
 export default task("market:create", "Create Market")
@@ -41,27 +44,60 @@ export default task("market:create", "Create Market")
 
     const assetConfig = assets.find((a) => a.symbol === symbol);
 
-    console.log(
-      `Creating market for token ${assetConfig.underlying}, pool ${poolName}, impl: ${assetConfig.delegateContractName}`
-    );
-    assetConfig.delegateContractName = taskArgs.delegateContractName
-      ? taskArgs.delegateContractName
-      : assetConfig.delegateContractName;
-    assetConfig.plugin = taskArgs.plugin ? taskArgs.plugin : assetConfig.plugin;
-
-    if (taskArgs.rewardsDistributors) {
-      const rds: Array<string> = taskArgs.rewardsDistributors.split(",");
-      const rts: Array<string> = taskArgs.rewardTokens.split(",");
-      if (rds.length !== rts.length) {
-        throw "Length of RDs and RTs must be equal";
+    // Prepare Custom Plugin if specified
+    let plugin: MarketPluginConfig | undefined = undefined;
+    if (taskArgs.delegateContractName) {
+      switch (taskArgs.delegateContractName) {
+        case DelegateContractName.CErc20PluginDelegate:
+          {
+            plugin = {
+              cTokenContract: DelegateContractName.CErc20PluginDelegate,
+              strategyAddress: taskArgs.plugin,
+              strategyName: "Custom Plugin",
+              strategyCode: "custom-plugin",
+            } as StandardPluginConfig;
+          }
+          break;
+        case DelegateContractName.CErc20PluginRewardsDelegate:
+          {
+            let flywheels: RewardsPluginConfig["flywheels"] | undefined = undefined;
+            if (taskArgs.rewardsDistributors) {
+              const rds: Array<string> = taskArgs.rewardsDistributors.split(",");
+              const rts: Array<string> = taskArgs.rewardTokens.split(",");
+              if (rds.length !== rts.length) {
+                throw "Length of RDs and RTs must be equal";
+              }
+              flywheels = rds.map((r, i) => {
+                return {
+                  address: r,
+                  rewardToken: rts[i],
+                };
+              });
+            }
+            plugin = {
+              cTokenContract: DelegateContractName.CErc20PluginRewardsDelegate,
+              strategyAddress: taskArgs.plugin,
+              strategyName: "Custom Plugin",
+              strategyCode: "custom-plugin",
+              flywheels,
+            } as RewardsPluginConfig;
+          }
+          break;
+        case DelegateContractName.CErc20Delegate: {
+          plugin = undefined;
+          break;
+        }
+        default:
+          console.warn("Unsupported delegateContractName");
       }
-      assetConfig.rewardsDistributorConfig = rds.map((r, i) => {
-        return {
-          rewardsDistributor: r,
-          rewardToken: rts[i],
-        };
-      });
     }
+
+    assetConfig.plugin = plugin;
+
+    console.log(
+      `Creating market for token ${assetConfig.underlying}, pool ${poolName}, plugin: ${assetConfig.plugin?.cTokenContract}`
+    );
+
     console.log("Asset config: ", assetConfig);
     const [assetAddress, implementationAddress, interestRateModel, receipt] = await sdk.deployAsset(
       sdk.JumpRateModelConf,
