d465c2175534058b4238db134299e8af01f099f7
pczek <839848+peetzweg@users.noreply.github.com>
2022-06-03 16:40:15 +0200

    refactors useFusePoolData to not overwrite Native price causing confusion!

--- packages/ui/hooks/useFusePoolData.ts
+++ packages/ui/hooks/useFusePoolData.ts
@@ -4,39 +4,57 @@ import { useQuery } from 'react-query';
 import { useRari } from '@ui/context/RariContext';
 import { useUSDPrice } from '@ui/hooks/useUSDPrice';
 
+export interface AdaptedFuseAsset extends NativePricedFuseAsset {
+  supplyBalanceFiat: number;
+  borrowBalanceFiat: number;
+  totalSupplyFiat: number;
+  totalBorrowFiat: number;
+  liquidityFiat: number;
+}
+
+export interface AdaptedFusePoolData extends FusePoolData {
+  assets: AdaptedFuseAsset[];
+  totalLiquidityFiat: number;
+  totalSuppliedFiat: number;
+  totalBorrowedFiat: number;
+  totalSupplyBalanceFiat: number;
+  totalBorrowBalanceFiat: number;
+}
+
 export const useFusePoolData = (poolId: string) => {
   const { fuse, currentChain, address, coingeckoId } = useRari();
   const { data: usdPrice } = useUSDPrice(coingeckoId);
 
-  return useQuery<FusePoolData | null>(
+  return useQuery<AdaptedFusePoolData | null>(
     ['useFusePoolData', currentChain.id, poolId, address, usdPrice],
     async () => {
       if (!usdPrice) return null;
 
       const res = await fuse.fetchFusePoolData(poolId, address);
-      const assetsWithPrice: NativePricedFuseAsset[] = [];
+      const assetsWithPrice: AdaptedFuseAsset[] = [];
       if (res.assets && res.assets.length !== 0) {
         res.assets.map((asset) => {
           assetsWithPrice.push({
             ...asset,
-            supplyBalanceNative: asset.supplyBalanceNative * usdPrice,
-            borrowBalanceNative: asset.borrowBalanceNative * usdPrice,
-            totalSupplyNative: asset.totalSupplyNative * usdPrice,
-            totalBorrowNative: asset.totalBorrowNative * usdPrice,
-            liquidityNative: asset.liquidityNative * usdPrice,
+            supplyBalanceFiat: asset.supplyBalanceNative * usdPrice,
+            borrowBalanceFiat: asset.borrowBalanceNative * usdPrice,
+            totalSupplyFiat: asset.totalSupplyNative * usdPrice,
+            totalBorrowFiat: asset.totalBorrowNative * usdPrice,
+            liquidityFiat: asset.liquidityNative * usdPrice,
           });
         });
       }
-
-      return {
+      const adaptedFusePoolData: AdaptedFusePoolData = {
         ...res,
         assets: assetsWithPrice,
-        totalLiquidityNative: res.totalLiquidityNative * usdPrice,
-        totalSuppliedNative: res.totalSuppliedNative * usdPrice,
-        totalBorrowedNative: res.totalBorrowedNative * usdPrice,
-        totalSupplyBalanceNative: res.totalSupplyBalanceNative * usdPrice,
-        totalBorrowBalanceNative: res.totalBorrowBalanceNative * usdPrice,
-      } as FusePoolData;
+        totalLiquidityFiat: res.totalLiquidityNative * usdPrice,
+        totalSuppliedFiat: res.totalSuppliedNative * usdPrice,
+        totalBorrowedFiat: res.totalBorrowedNative * usdPrice,
+        totalSupplyBalanceFiat: res.totalSupplyBalanceNative * usdPrice,
+        totalBorrowBalanceFiat: res.totalBorrowBalanceNative * usdPrice,
+      };
+
+      return adaptedFusePoolData;
     },
     { enabled: !!usdPrice }
   );
