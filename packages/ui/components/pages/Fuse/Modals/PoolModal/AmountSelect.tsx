35a07adf2b3757a38d18fbe2f90799dfe7d0301e
Isa Noguchi <isanoguchi91@gmail.com>
2022-06-06 11:28:59 -0400

    updated ui and ux for borrow slider

--- packages/ui/components/pages/Fuse/Modals/PoolModal/AmountSelect.tsx
+++ packages/ui/components/pages/Fuse/Modals/PoolModal/AmountSelect.tsx
@@ -27,6 +27,8 @@ import LogRocket from 'logrocket';
 import { ReactNode, useState } from 'react';
 import { useQuery } from 'react-query';
 
+import MaxBorrowSlider from './MaxBorrowSlider';
+
 import DashboardBox from '@ui/components/shared/DashboardBox';
 import Loader from '@ui/components/shared/Loader';
 import { ModalDivider } from '@ui/components/shared/Modal';
@@ -45,7 +47,6 @@ import { convertMantissaToAPR, convertMantissaToAPY } from '@ui/utils/apyUtils';
 import { smallUsdFormatter } from '@ui/utils/bigUtils';
 import { Center, Column, Row, useIsMobile } from '@ui/utils/chakraUtils';
 import { handleGenericError } from '@ui/utils/errorHandling';
-import MaxBorrowSlider from './MaxBorrowSlider';
 
 const AmountSelect = ({
   assets,
@@ -74,6 +75,18 @@ const AmountSelect = ({
   const [enableAsCollateral, setEnableAsCollateral] = useState(showEnableAsCollateral);
 
   const { cCard, cSwitch } = useColors();
+
+  const getBorrowLimit = async () => {
+    const borrowLimitBN = (await fetchMaxAmount(
+      mode,
+      fuse,
+      address,
+      asset,
+      currentChain.id
+    )) as BigNumber;
+
+    return Number(utils.formatUnits(borrowLimitBN));
+  };
 
   const updateAmount = (newAmount: string) => {
     if (newAmount.startsWith('-') || !newAmount) {
@@ -232,7 +245,6 @@ const AmountSelect = ({
     <Column
       mainAxisAlignment="flex-start"
       crossAxisAlignment="flex-start"
-      height={showEnableAsCollateral ? '605px' : '525px'}
       bg={cCard.bgColor}
       color={cCard.txtColor}
       borderRadius={16}
@@ -288,6 +300,7 @@ const AmountSelect = ({
             px={4}
             py={4}
             height="100%"
+            width="100%"
           >
             <Column mainAxisAlignment="flex-start" crossAxisAlignment="flex-start" width="100%">
               <TabBar
@@ -299,7 +312,13 @@ const AmountSelect = ({
               />
 
               <DashboardBox width="100%" height="70px" mt={3}>
-                <Row p={4} mainAxisAlignment="space-between" crossAxisAlignment="center" expand>
+                <Row
+                  width="100%"
+                  p={4}
+                  mainAxisAlignment="space-between"
+                  crossAxisAlignment="center"
+                  expand
+                >
                   <AmountInput
                     displayAmount={userEnteredAmount}
                     updateAmount={updateAmount}
@@ -319,8 +338,9 @@ const AmountSelect = ({
                   />
                 </Row>
               </DashboardBox>
-              <MaxBorrowSlider/>
-
+              {mode === FundOperationMode.BORROW && (
+                <MaxBorrowSlider getBorrowLimit={getBorrowLimit} updateAmount={updateAmount} />
+              )}
             </Column>
 
             <StatsColumn
@@ -698,9 +718,11 @@ const TokenNameAndMaxButton = ({
         </Heading>
       </Row>
 
-      <Button height={8} onClick={setToMax} isLoading={isMaxLoading}>
-        MAX
-      </Button>
+      {mode !== FundOperationMode.BORROW && (
+        <Button height={8} onClick={setToMax} isLoading={isMaxLoading}>
+          MAX
+        </Button>
+      )}
     </Row>
   );
 };
@@ -808,7 +830,7 @@ async function fetchMaxAmount(
     )) as BigNumber;
 
     if (maxBorrow) {
-      return utils.parseUnits((Number(utils.formatUnits(maxBorrow)) * 0.75).toString());
+      return maxBorrow;
     } else {
       throw new Error('Could not fetch your max borrow amount! Code: ');
     }
--- packages/ui/components/pages/Fuse/Modals/PoolModal/MaxBorrowSlider.tsx
+++ packages/ui/components/pages/Fuse/Modals/PoolModal/MaxBorrowSlider.tsx
@@ -1,33 +1,117 @@
-import 'rc-tooltip/assets/bootstrap.css';
-import Slider from 'rc-slider';
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  Box,
+  CloseButton,
+  Slider,
+  SliderFilledTrack,
+  SliderMark,
+  SliderThumb,
+  SliderTrack,
+  Tooltip,
+  useDisclosure,
+} from '@chakra-ui/react';
+import { useCallback, useEffect, useState } from 'react';
 
-const { createSliderWithTooltip } = Slider;
-const Range = createSliderWithTooltip(Slider.Range);
-const { Handle } = Slider;
+enum Color {
+  default = 'teal',
+  warning = 'pink',
+}
 
-const handle = props => {
-  const { value, dragging, index, ...restProps } = props;
+const colorLimit = 75;
+
+function MaxBorrowSlider({
+  updateAmount,
+  getBorrowLimit,
+}: {
+  updateAmount: (amount: string) => void;
+  getBorrowLimit: () => Promise<number>;
+}) {
+  const [sliderValue, setSliderValue] = useState(5);
+  const [showTooltip, setShowTooltip] = useState(false);
+  const [sliderColor, setSliderColor] = useState('teal');
+  const [borrowLimit, setBorrowLimit] = useState<number>(0);
+
+  const fetchBorrowLimit = useCallback(async () => {
+    const borrowLimit = await getBorrowLimit();
+    setBorrowLimit(borrowLimit);
+    updateAmount(((borrowLimit * sliderValue) / 100).toString());
+  }, []);
+
+  useEffect(() => {
+    fetchBorrowLimit();
+  }, [fetchBorrowLimit]);
+
+  const handleSliderValueChange = (v: number) => {
+    if (v >= colorLimit && sliderColor === Color.default) {
+      setSliderColor(Color.warning);
+      onOpen();
+    } else if (v < colorLimit && sliderColor === Color.warning) {
+      setSliderColor(Color.default);
+    }
+
+    setSliderValue(v);
+
+    const borrowAmount = ((borrowLimit * sliderValue) / 100).toString();
+    updateAmount(borrowAmount);
+  };
+
+  const { isOpen: isVisible, onClose, onOpen } = useDisclosure({ defaultIsOpen: false });
+
   return (
-    <SliderTooltip
-      prefixCls="rc-slider-tooltip"
-      overlay={`${value} %`}
-      visible={dragging}
-      placement="top"
-      key={index}
-    >
-      <Handle value={value} {...restProps} />
-    </SliderTooltip>
+    <Box width="100%">
+      <Slider
+        id="slider"
+        defaultValue={50}
+        min={0}
+        max={100}
+        colorScheme={sliderColor}
+        mt={4}
+        onChange={handleSliderValueChange}
+        onMouseEnter={() => setShowTooltip(true)}
+        onMouseLeave={() => setShowTooltip(false)}
+      >
+        <SliderMark value={25} mt="1" ml="-2.5" fontSize="sm">
+          25%
+        </SliderMark>
+        <SliderMark value={50} mt="1" ml="-2.5" fontSize="sm">
+          50%
+        </SliderMark>
+        <SliderMark value={75} mt="1" ml="-2.5" fontSize="sm">
+          75%
+        </SliderMark>
+        <SliderTrack>
+          <SliderFilledTrack />
+        </SliderTrack>
+        <Tooltip
+          hasArrow
+          bg="teal.500"
+          color="white"
+          placement="top"
+          isOpen={showTooltip}
+          label={`${sliderValue}%`}
+        >
+          <SliderThumb />
+        </Tooltip>
+      </Slider>
+      {isVisible && (
+        <Alert mt={4} status="error">
+          <AlertIcon />
+          <Box>
+            <AlertDescription>Max borrow rate is not recommended to go over 75%</AlertDescription>
+          </Box>
+          <CloseButton
+            alignSelf="flex-start"
+            position="relative"
+            right={-1}
+            top={-1}
+            onClick={onClose}
+          />
+        </Alert>
+      )}
+    </Box>
   );
-};
+}
 
-const wrapperStyle = { width: 400, margin: 50 };
-
-const MaxBorrowSlider = () => (
-  <div>
-    <div style={wrapperStyle}>
-      <Slider min={0} max={100} defaultValue={3} />
-    </div>
-  </div>
-)
-
-export default MaxBorrowSlider
+export default MaxBorrowSlider;
