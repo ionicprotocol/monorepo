465ab39ce035787841e275f58284270e4363e056
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2022-06-08 18:21:00 +0300

    setting the pause guardian before pausing the minting

--- packages/sdk/tasks/pauseMarketMinting.ts
+++ packages/sdk/tasks/pauseMarketMinting.ts
@@ -1,23 +1,43 @@
 import { task, types } from "hardhat/config";
-import { Comptroller, CToken } from "../lib/contracts/typechain";
+import { CToken } from "../lib/contracts/typechain/CToken";
+import { Comptroller } from "../lib/contracts/typechain/Comptroller";
+import { providers, constants } from "ethers";
 
-// npx hardhat market-mint-pause --market 0xCTokenAddress --admin 0x304aE8f9300e09c8B33bb1a8AE1c14A6253a5F4D --paused true --network bsc
+// npx hardhat market-mint-pause --market 0xCTokenAddress --admin deployer --paused true --network bsc
 
-export default task("market:mint:pause", "Pauses minting on a market")
+export default task("market-mint-pause", "Pauses minting on a market")
     .addParam("market", "The address of the CToken", undefined, types.string)
     .addParam("admin", "Named account from which to pause the minting on the market", "deployer", types.string)
     .addOptionalParam("paused", "If the market should be paused or not", "true", types.boolean)
     .setAction(async (taskArgs, hre) => {
+            let tx: providers.TransactionResponse;
+
         const admin = await hre.ethers.getNamedSigner(taskArgs.admin);
 
-        const market: CToken = await hre.ethers.getContractAt("CToken", taskArgs.market, admin) as CToken;
+        const market: CToken = await hre.ethers.getContractAt("CToken.sol:CToken", taskArgs.market, admin) as CToken;
         const comptroller = await market.comptroller();
-        const pool: Comptroller = await hre.ethers.getContractAt("Comptroller", comptroller, admin) as Comptroller;
-        await pool._setMintPaused(market.address, taskArgs.paused);
+        const pool: Comptroller = await hre.ethers.getContractAt("Comptroller.sol:Comptroller", comptroller, admin) as Comptroller;
+
+        const currentPauseGuardian = await pool.pauseGuardian();
+        if (currentPauseGuardian === constants.AddressZero) {
+                tx = await pool._setPauseGuardian(admin.address);
+                await tx.wait();
+                console.log(`Set the pause guardian to ${admin.address}`);
+        }
 
         const isPaused: boolean = await pool.mintGuardianPaused(market.address);
+        if (isPaused != taskArgs.paused) {
+                tx = await pool._setMintPaused(market.address, taskArgs.paused);
+                await tx.wait();
 
-        console.log(`The market at ${market.address} minting pause has been to ${isPaused}`);
+                console.log(`Market pause tx ${tx.hash}`);
+        } else {
+                console.log(`No need to set the minting pause to ${taskArgs.paused} as it is already set to that value`);
+        }
 
-        return isPaused;
+        const isPausedAfter: boolean = await pool.mintGuardianPaused(market.address);
+
+        console.log(`The market at ${market.address} minting pause has been to ${isPausedAfter}`);
+
+        return isPausedAfter;
     });
