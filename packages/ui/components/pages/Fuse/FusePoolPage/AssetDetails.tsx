6168a2b91771b86fd6a3a36ad9c8b9f83bfbac26
pczek <839848+peetzweg@users.noreply.github.com>
2022-06-14 13:37:33 +0200

    refactors PoolInfoBox into two seperate components and using grid layout to reduce own component clutter.

---
+++ packages/ui/components/pages/Fuse/FusePoolPage/AssetDetails.tsx
@@ -0,0 +1,285 @@
+import { Box, Heading, Select, Skeleton, Spinner, Stack, Text } from '@chakra-ui/react';
+import { NativePricedFuseAsset } from '@midas-capital/sdk';
+import { utils } from 'ethers';
+import dynamic from 'next/dynamic';
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+
+import CaptionedStat from '@ui/components/shared/CaptionedStat';
+import { MidasBox } from '@ui/components/shared/MidasBox';
+import { ModalDivider } from '@ui/components/shared/Modal';
+import { useRari } from '@ui/context/RariContext';
+import { useColors } from '@ui/hooks/useColors';
+import { useFusePoolData } from '@ui/hooks/useFusePoolData';
+import { useTokenData } from '@ui/hooks/useTokenData';
+import { shortUsdFormatter } from '@ui/utils/bigUtils';
+import { Center, Column, Row, useIsMobile } from '@ui/utils/chakraUtils';
+import { FuseUtilizationChartOptions } from '@ui/utils/chartOptions';
+import { convertIRMtoCurve } from '@ui/utils/convertIRMtoCurve';
+
+const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
+
+export const AssetDetails = ({ data }: { data: ReturnType<typeof useFusePoolData>['data'] }) => {
+  const isMobile = useIsMobile();
+
+  return (
+    <MidasBox height={isMobile ? 'auto' : '450px'}>
+      {data ? (
+        data.assets.length > 0 ? (
+          <AssetAndOtherInfo assets={data.assets} />
+        ) : (
+          <Center height="100%">{'There are no assets in this pool.'}</Center>
+        )
+      ) : (
+        <Column mainAxisAlignment="flex-start" crossAxisAlignment="flex-start" height="100%" pb={3}>
+          <Heading
+            size="sm"
+            px={4}
+            py={5}
+            display="flex"
+            width="100%"
+            justifyContent="space-between"
+          >
+            <Text>{`Asset stats`}</Text>
+            <Skeleton display="inline" w="100px"></Skeleton>
+          </Heading>
+          <Stack width="100%" height="100%" mx="auto">
+            <Skeleton height="50%" />
+            <Skeleton height="50%" />
+          </Stack>
+        </Column>
+      )}
+    </MidasBox>
+  );
+};
+
+const AssetOption = ({ asset }: { asset: NativePricedFuseAsset }) => {
+  const { data: tokenData } = useTokenData(asset.underlyingToken);
+  const { cPage } = useColors();
+
+  return (
+    <option value={asset.cToken} key={asset.cToken} style={{ color: cPage.primary.txtColor }}>
+      {tokenData?.symbol ?? asset.underlyingSymbol}
+    </option>
+  );
+};
+
+const AssetAndOtherInfo = ({ assets }: { assets: NativePricedFuseAsset[] }) => {
+  const { fuse, currentChain } = useRari();
+
+  const [selectedAsset, setSelectedAsset] = useState(assets.length > 3 ? assets[2] : assets[0]);
+  const { data: selectedTokenData } = useTokenData(selectedAsset.underlyingToken);
+  const selectedAssetUtilization =
+    selectedAsset.totalSupply.toString() === '0'
+      ? 0
+      : parseFloat(
+          // Use Max.min() to cap util at 100%
+          Math.min(
+            (Number(utils.formatUnits(selectedAsset.totalBorrow, 18)) /
+              Number(utils.formatUnits(selectedAsset.totalSupply, 18))) *
+              100,
+            100
+          ).toFixed(0)
+        );
+  const { data } = useQuery(['ChartData', selectedAsset.cToken], async () => {
+    const interestRateModel = await fuse.getInterestRateModel(selectedAsset.cToken);
+
+    if (interestRateModel === null) {
+      return { borrowerRates: null, supplierRates: null };
+    }
+
+    return convertIRMtoCurve(interestRateModel, currentChain.id);
+  });
+
+  const isMobile = useIsMobile();
+
+  const { cChart } = useColors();
+
+  return (
+    <Column
+      mainAxisAlignment="flex-start"
+      crossAxisAlignment="flex-start"
+      width="100%"
+      height="100%"
+    >
+      <Row
+        mainAxisAlignment="space-between"
+        crossAxisAlignment="center"
+        height="60px"
+        width="100%"
+        px={4}
+        flexShrink={0}
+      >
+        <Heading size="sm" py={3}>
+          {`${selectedTokenData?.symbol ?? selectedAsset.underlyingSymbol} Stats`}
+        </Heading>
+
+        <Select
+          width="auto"
+          onChange={(event) => {
+            setSelectedAsset(
+              assets.find((asset) => asset.cToken === event.target.value) || assets[0]
+            );
+          }}
+          value={selectedAsset.cToken}
+          cursor="pointer"
+        >
+          {assets.map((asset) => (
+            <AssetOption asset={asset} key={asset.cToken} />
+          ))}
+        </Select>
+      </Row>
+
+      <ModalDivider />
+
+      <Box
+        height="200px"
+        width="100%"
+        color="#000000"
+        overflow="hidden"
+        px={3}
+        className="hide-bottom-tooltip"
+        flexShrink={0}
+      >
+        {data ? (
+          data.supplierRates === null ? (
+            <Center color="#FFFFFF" height="100%">
+              <Text>No graph is available for this asset(&apos)s interest curves.</Text>
+            </Center>
+          ) : (
+            <Chart
+              options={{
+                ...FuseUtilizationChartOptions,
+                annotations: {
+                  points: [
+                    {
+                      x: selectedAssetUtilization,
+                      y: data.borrowerRates[selectedAssetUtilization].y,
+                      marker: {
+                        size: 6,
+                        fillColor: '#FFF',
+                        strokeColor: '#DDDCDC',
+                      },
+                    },
+                    {
+                      x: selectedAssetUtilization,
+                      y: data.supplierRates[selectedAssetUtilization].y,
+                      marker: {
+                        size: 6,
+                        fillColor: cChart.tokenColor,
+                        strokeColor: '#FFF',
+                      },
+                    },
+                  ],
+                  xaxis: [
+                    {
+                      x: selectedAssetUtilization,
+                      label: {
+                        text: 'Current Utilization',
+                        orientation: 'horizontal',
+                        style: {
+                          background: cChart.labelBgColor,
+                          color: '#000',
+                        },
+                        // offsetX: 40,
+                      },
+                    },
+                  ],
+                },
+
+                colors: [cChart.borrowColor, cChart.tokenColor],
+              }}
+              type="line"
+              width="100%"
+              height="100%"
+              series={[
+                {
+                  name: 'Borrow Rate',
+                  data: data.borrowerRates,
+                },
+                {
+                  name: 'Deposit Rate',
+                  data: data.supplierRates,
+                },
+              ]}
+            />
+          )
+        ) : (
+          <Center height="100%" color="#FFFFFF">
+            <Spinner />
+          </Center>
+        )}
+      </Box>
+
+      <ModalDivider />
+
+      <Row
+        mainAxisAlignment="space-around"
+        crossAxisAlignment="center"
+        height="100%"
+        width="100%"
+        pt={4}
+        px={4}
+        pb={2}
+      >
+        <CaptionedStat
+          stat={Number(utils.formatUnits(selectedAsset.collateralFactor, 16)).toFixed(0) + '%'}
+          statSize="lg"
+          captionSize="xs"
+          caption={'Collateral Factor'}
+          crossAxisAlignment="center"
+          captionFirst={true}
+        />
+
+        <CaptionedStat
+          stat={Number(utils.formatUnits(selectedAsset.reserveFactor, 16)).toFixed(0) + '%'}
+          statSize="lg"
+          captionSize="xs"
+          caption={'Reserve Factor'}
+          crossAxisAlignment="center"
+          captionFirst={true}
+        />
+      </Row>
+
+      <ModalDivider />
+
+      <Row
+        mainAxisAlignment="space-around"
+        crossAxisAlignment="center"
+        height="100%"
+        width="100%"
+        p={4}
+        mt={3}
+      >
+        <CaptionedStat
+          stat={shortUsdFormatter(selectedAsset.totalSupplyNative)}
+          statSize="lg"
+          captionSize="xs"
+          caption={'Total Supplied'}
+          crossAxisAlignment="center"
+          captionFirst={true}
+        />
+
+        {isMobile ? null : (
+          <CaptionedStat
+            stat={selectedAsset.utilization.toFixed(0) + '%'}
+            statSize="lg"
+            captionSize="xs"
+            caption={'Utilization'}
+            crossAxisAlignment="center"
+            captionFirst={true}
+          />
+        )}
+
+        <CaptionedStat
+          stat={shortUsdFormatter(selectedAsset.totalBorrowNative)}
+          statSize="lg"
+          captionSize="xs"
+          caption={'Total Borrowed'}
+          crossAxisAlignment="center"
+          captionFirst={true}
+        />
+      </Row>
+    </Column>
+  );
+};
--- packages/ui/components/pages/Fuse/FusePoolPage/index.tsx
+++ packages/ui/components/pages/Fuse/FusePoolPage/index.tsx
@@ -4,6 +4,8 @@ import {
   Box,
   Divider,
   Flex,
+  Grid,
+  GridItem,
   Heading,
   HStack,
   SimpleGrid,
@@ -13,15 +15,17 @@ import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { memo } from 'react';
 
+import { AssetDetails } from './AssetDetails';
+import PoolDetails from './PoolDetails';
+
 import FuseNavbar from '@ui/components/pages/Fuse/FuseNavbar';
 import { BorrowList } from '@ui/components/pages/Fuse/FusePoolPage/BorrowList';
 import { CollateralRatioBar } from '@ui/components/pages/Fuse/FusePoolPage/CollateralRatioBar';
-import { PoolDashboardBox } from '@ui/components/pages/Fuse/FusePoolPage/PoolDashboardBox';
-import { PoolInfoBox } from '@ui/components/pages/Fuse/FusePoolPage/PoolInfoBox';
 import { PoolStat } from '@ui/components/pages/Fuse/FusePoolPage/PoolStat';
 import { RewardsBanner } from '@ui/components/pages/Fuse/FusePoolPage/RewardsBanner';
 import { SupplyList } from '@ui/components/pages/Fuse/FusePoolPage/SupplyList';
 import { CTokenIcon } from '@ui/components/shared/CTokenIcon';
+import { MidasBox } from '@ui/components/shared/MidasBox';
 import PageTransitionLayout from '@ui/components/shared/PageTransitionLayout';
 import { useRari } from '@ui/context/RariContext';
 import { useFlywheelRewardsForPool } from '@ui/hooks/rewards/useFlywheelRewardsForPool';
@@ -58,6 +62,7 @@ const FusePoolPage = memo(() => {
           alignItems="flex-start"
           bgColor={cPage.primary.bgColor}
           justifyContent="flex-start"
+          mb={20}
         >
           <FuseNavbar />
           <HStack width={'100%'} mx="auto" spacing={6}>
@@ -117,25 +122,31 @@ const FusePoolPage = memo(() => {
               </SimpleGrid>
             </Box>
           </Box>
+
           {
             /* If they have some asset enabled as collateral, show the collateral ratio bar */
             data && data.assets.some((asset) => asset.membership) ? (
-              <CollateralRatioBar assets={data.assets} borrowFiat={data.totalBorrowBalanceFiat} />
+              <CollateralRatioBar
+                assets={data.assets}
+                borrowFiat={data.totalBorrowBalanceFiat}
+                my={4}
+              />
             ) : null
           }
-          <RowOrColumn
-            width={'100%'}
-            mainAxisAlignment="flex-start"
-            crossAxisAlignment="flex-start"
-            bgColor={cPage.primary.bgColor}
-            color={cPage.primary.txtColor}
-            mx="auto"
-            mt={4}
-            pb={4}
-            isRow={!isMobile}
-            alignItems="stretch"
+
+          <Grid
+            templateColumns={{
+              base: 'repeat(1, 1fr)',
+              lg: 'repeat(2, 1fr)',
+            }}
+            templateRows={{
+              base: 'repeat(4, auto)',
+              lg: 'repeat(2, auto)',
+            }}
+            w="100%"
+            gap={4}
           >
-            <PoolDashboardBox pb={2} width={isMobile ? '100%' : '50%'} borderRadius={12}>
+            <MidasBox pb={3}>
               {data ? (
                 <SupplyList
                   assets={data.assets}
@@ -146,15 +157,9 @@ const FusePoolPage = memo(() => {
               ) : (
                 <TableSkeleton tableHeading="Your Supply Balance" />
               )}
-            </PoolDashboardBox>
+            </MidasBox>
 
-            <PoolDashboardBox
-              ml={isMobile ? 0 : 4}
-              mt={isMobile ? 4 : 0}
-              pb={2}
-              borderRadius={12}
-              width={isMobile ? '100%' : '50%'}
-            >
+            <MidasBox pb={3}>
               {data ? (
                 <BorrowList
                   comptrollerAddress={data.comptroller}
@@ -164,11 +169,12 @@ const FusePoolPage = memo(() => {
               ) : (
                 <TableSkeleton tableHeading="Your Borrow Balance" />
               )}
-            </PoolDashboardBox>
-          </RowOrColumn>
+            </MidasBox>
 
-          <PoolInfoBox data={data} />
-          <Box h={'20'} />
+            <PoolDetails data={data} />
+
+            <AssetDetails data={data} />
+          </Grid>
         </Flex>
       </PageTransitionLayout>
     </>
