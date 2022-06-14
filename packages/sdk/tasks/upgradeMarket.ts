5c013cdcfba97a1608e81264388f054d0f5de261
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2022-06-14 23:25:47 +0300

    upgrade market hardhat task; being tested currently

--- packages/sdk/tasks/upgradeMarket.ts
+++ packages/sdk/tasks/upgradeMarket.ts
@@ -1,22 +1,24 @@
-import {task, types} from "hardhat/config";
-import { MarketConfig } from "../src";
-import {assetSymbols} from "../src/chainConfig";
-import {deployERC4626Plugin} from "../chainDeploy/helpers/erc4626Plugins";
-import {deployConfig} from "../chainDeploy/testnets/chapel";
-import {DeployedAsset} from "../tests/utils/pool";
-import {MasterPriceOracle, SimplePriceOracle} from "../lib/contracts/typechain";
+import { task, types } from "hardhat/config";
+import { DelegateContractName } from "../src";
+import { CErc20Delegate } from "../lib/contracts/typechain";
+import { TransactionReceipt } from "@ethersproject/abstract-provider";
+import { constants, Contract } from "ethers";
 
 // npx hardhat market:upgrade --network chapel
 
 export default task("market:upgrade", "Upgrades a market's implementation")
-    .addParam("deployer", "Named account from which to upgrade the implementation", "deployer", types.string)
-    .setAction(async (taskArgs, { ethers, getChainId, getNamedAccounts, deployments, run}) => {
-        // const bob = await ethers.getNamedSigner("bob");
-        const signer = await ethers.getNamedSigner(taskArgs.deployer);
-        // await bob.sendTransaction({value: ethers.utils.parseEther("0.5"), to: signer.address});
+    .addParam("poolName", "Name of pool", undefined, types.string)
+    .addParam("symbol", "Asset symbol", undefined, types.string)
+    .addParam("admin", "Named account that is an admin of the pool", "deployer", types.string)
+    .addOptionalParam("newImplementationAddress", "The address of the new implementation", undefined, types.string)
+    .addOptionalParam("strategyCode", "If using strategy, pass its code", undefined, types.string)
+    .setAction(async (taskArgs, { ethers, getChainId, deployments}) => {
+        const poolName = taskArgs.poolName;
+        const symbol = taskArgs.symbol;
+        const strategyCode = taskArgs.strategyCode;
+        const signer = await ethers.getNamedSigner(taskArgs.admin);
 
-        // @ts-ignore
-        const enumsModule = await import("../src/enums");
+
         // @ts-ignore
         const assetModule = await import("../tests/utils/assets");
         // @ts-ignore
@@ -25,85 +27,80 @@ export default task("market:upgrade", "Upgrades a market's implementation")
         const fuseModule = await import("../tests/utils/fuseSdk");
         const sdk = await fuseModule.getOrCreateFuse();
 
-        const fuseFeeDistributor = await ethers.getContractAt("FuseFeeDistributor", sdk.contracts.FuseFeeDistributor.address);
+        const pool = await poolModule.getPoolByName(poolName, sdk);
+        const assets = await assetModule.getAssetsConf(
+            pool.comptroller,
+            sdk.contracts.FuseFeeDistributor.address,
+            sdk.irms.JumpRateModel.address,
+            ethers,
+            poolName
+        );
 
-        const fee = await fuseFeeDistributor.defaultInterestFeeRate();
-        console.log(`ffd fee ${fee}`);
+        const assetConfig = assets.find((a) => a.symbol === symbol);
 
-        // for bsc mainnet      0x5373C052Df65b317e48D6CAD8Bb8AC50995e9459
+        const newImplementationAddress = taskArgs.newImplementationAddress ?
+            taskArgs.newImplementationAddress
+            : sdk.chainDeployment[assetConfig.plugin.cTokenContract].address;
 
-        const chainId = await getChainId();
-        if (chainId == "97") {
-            const assets = sdk.supportedAssets;
-            const busd = assets.find((a) => a.symbol === assetSymbols.BUSD)!.underlying;
+        if (strategyCode) {
+            assetConfig.plugin = sdk.chainPlugins[assetConfig.underlying].find((p) => p.strategyCode === strategyCode);
 
-            const deployedSpo = await ethers.getContractOrNull("SimplePriceOracle", signer);
+            const market = pool.assets.find((a) => a.underlyingSymbol == symbol);
+            const cTokenInstance = new Contract(
+                market.cToken,
+                sdk.chainDeployment[DelegateContractName.CErc20Delegate].abi,
+                signer
+            ) as CErc20Delegate;
 
-            if (!deployedSpo) {
-                const spo = await deployments.deploy("SimplePriceOracle", {
-                    from: signer.address,
-                    args: [],
-                    log: true,
-                });
-                if (spo.transactionHash) await ethers.provider.waitForTransaction(spo.transactionHash);
-                console.log("SimplePriceOracle: ", spo.address);
-
-                const busdArray = [];
-                const busdOracleArray = [];
-                busdArray.push(busd);
-                busdOracleArray.push(spo.address);
-
-                const mpo = await ethers.getContract("MasterPriceOracle", signer) as MasterPriceOracle;
-
-                let tx = await mpo.add(busdArray, busdOracleArray);
-
-                await tx.wait();
-                console.log(
-                    `Master Price Oracle updated for tokens ${busdArray.join(", ")} with oracles ${busdOracleArray.join(", ")}`
-                );
-            }
-
-            const simplePO = await ethers.getContract("SimplePriceOracle", signer) as SimplePriceOracle;
-            let tx = await simplePO.setDirectPrice(busd, ethers.utils.parseEther("2"));
-            await tx.wait();
-            console.log(`Simple Price Oracle updated for busd`);
-
-            // for chapel           0x401d109d1eab112f80ab0035b78f01d6092c9097
-            // const fusePoolDirectory = await ethers.getContract("FusePoolDirectory", signer);
-            const poolAddress = "0x3d12762B0460da2fE8D731E03706f38aE2dD633a"; // pool name: velikos4 0x3d12762B0460da2fE8D731E03706f38aE2dD633a
-            // chapel irm 0x6527539967fd5448ad43a83e9a4c232e0ba7daa1
-            const interestRateModelAddress = "0xb5Ef3b4418D05021816CA3735B013440781e8651"; // Jump rate model "0x6527539967fd5448ad43a83e9a4c232e0ba7daa1";
-
-            const assetConfig: MarketConfig = {
-                underlying: busd,
-                comptroller: poolAddress,
-                fuseFeeDistributor: fuseFeeDistributor.address,
-                interestRateModel: interestRateModelAddress,
-                name: "velikos token",
-                symbol: "VMT",
-                collateralFactor: 75,
-                reserveFactor: 15,
-                adminFee: 0,
-                bypassPriceFeedCheck: true,
-            };
-            // await deployERC4626Plugin({ ethers, getNamedAccounts, deployments, run, deployConfig, dynamicFlywheels });
-
-            assetConfig.plugin = sdk.chainPlugins[assetConfig.underlying].find((p) => p.strategyCode === "MockERC4626_BUSD");
-
-            console.log(
-                `Creating market for token ${assetConfig.underlying}, pool ${poolAddress}, impl: ${
-                    assetConfig.plugin ? assetConfig.plugin.cTokenContract : enumsModule.DelegateContractName.CErc20Delegate
-                }`
+            const abiCoder = new ethers.utils.AbiCoder();
+            const implementationData = abiCoder.encode(["address"], [assetConfig.plugin.strategyAddress]);
+            // TODO what adds newImplementationAddress to FFD.cErc20DelegateWhitelist ?
+            console.log(`Setting implementation to ${newImplementationAddress}`);
+            const setImplementationTx = await cTokenInstance._setImplementationSafe(
+                newImplementationAddress,
+                false,
+                implementationData
             );
-            const deployedAssets = await poolModule.deployAssets([assetConfig], signer);
-            for (let i=0; i < deployedAssets.length; i++) {
-                const deployedAsset: DeployedAsset = deployedAssets[i];
-                console.log(
-                    `Created market at address ${deployedAsset.assetAddress}, implementation address ${deployedAsset.implementationAddress}, symbol: ${deployedAsset.symbol}`
-                );
-            }
-        } else if(chainId == "56") {
-            // await fuseFeeDistributor._setDefaultInterestFeeRate(ethers.utils.parseEther("0.1"));
 
+            const receipt: TransactionReceipt = await setImplementationTx.wait();
+            if (receipt.status != constants.One.toNumber()) {
+                throw `Failed set implementation to ${assetConfig.plugin.cTokenContract}`;
+            }
+            console.log(`Implementation successfully set to ${assetConfig.plugin.cTokenContract}`);
+
+            // TODO CErc20PluginRewardsDelegate
+            // // Further actions for `CErc20PluginRewardsDelegate`
+            // if (assetConfig.plugin.cTokenContract === DelegateContractName.CErc20PluginRewardsDelegate) {
+            //     // Add Flywheels as RewardsDistributors to Pool
+            //     const rdsOfComptroller = await comptroller.callStatic.getRewardsDistributors();
+            //     // TODO https://github.com/Midas-Protocol/monorepo/issues/166
+            //     const cToken: CErc20PluginRewardsDelegate = this.getCErc20PluginRewardsInstance(cErc20DelegatorAddress);
+            //     for (const flywheelConfig of assetConfig.plugin.flywheels) {
+            //         if (rdsOfComptroller.includes(flywheelConfig.address)) continue;
+            //
+            //         const addRdTx = await comptroller._addRewardsDistributor(flywheelConfig.address, {
+            //             from: options.from,
+            //         });
+            //         const addRdTxReceipt: TransactionReceipt = await addRdTx.wait();
+            //
+            //         if (addRdTxReceipt.status != constants.One.toNumber()) {
+            //             throw `Failed set add RD to pool ${flywheelConfig.address}`;
+            //         }
+            //         const approveTx = await cToken["approve(address,address)"](
+            //             flywheelConfig.rewardToken,
+            //             flywheelConfig.address,
+            //             {
+            //                 from: options.from,
+            //             }
+            //         );
+            //         const approveTxReceipt = await approveTx.wait();
+            //         if (approveTxReceipt.status != constants.One.toNumber()) {
+            //             throw `Failed to approve to pool ${flywheelConfig.address}`;
+            //         }
+            //
+            //         console.log(approveTxReceipt.status);
+            //         console.log("Approval succeeded");
+            //     }
+            // }
         }
     });
