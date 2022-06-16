d2384f054f7d78c6cb19083fcb5ebcbfa97e5568
smartdevcom <smart.dev.cop@gmail.com>
2022-06-16 22:57:57 +1000

    fixed APR, APY calculation

--- packages/ui/hooks/fuse/usePoolDetails.ts
+++ packages/ui/hooks/fuse/usePoolDetails.ts
@@ -1,9 +1,12 @@
 import { NativePricedFuseAsset } from '@midas-capital/sdk';
 import { useMemo } from 'react';
 
+import { useRari } from '@ui/context/RariContext';
+import { getBlockTimePerMinuteByChainId } from '@ui/networkData/index';
 import { convertMantissaToAPR, convertMantissaToAPY } from '@ui/utils/apyUtils';
 
 export const usePoolDetails = (assets: NativePricedFuseAsset[] | undefined) => {
+  const { currentChain } = useRari();
   return useMemo(() => {
     if (assets && assets.length) {
       let mostSuppliedAsset = assets[0];
@@ -14,14 +17,28 @@ export const usePoolDetails = (assets: NativePricedFuseAsset[] | undefined) => {
           mostSuppliedAsset = asset;
         }
         if (
-          convertMantissaToAPY(asset.supplyRatePerBlock, 365) >
-          convertMantissaToAPY(topLendingAPYAsset.supplyRatePerBlock, 365)
+          convertMantissaToAPY(
+            asset.supplyRatePerBlock,
+            getBlockTimePerMinuteByChainId(currentChain.id),
+            365
+          ) >
+          convertMantissaToAPY(
+            topLendingAPYAsset.supplyRatePerBlock,
+            getBlockTimePerMinuteByChainId(currentChain.id),
+            365
+          )
         ) {
           topLendingAPYAsset = asset;
         }
         if (
-          convertMantissaToAPR(asset.borrowRatePerBlock) >
-          convertMantissaToAPR(topBorrowAPRAsset.borrowRatePerBlock)
+          convertMantissaToAPR(
+            asset.borrowRatePerBlock,
+            getBlockTimePerMinuteByChainId(currentChain.id)
+          ) >
+          convertMantissaToAPR(
+            topBorrowAPRAsset.borrowRatePerBlock,
+            getBlockTimePerMinuteByChainId(currentChain.id)
+          )
         ) {
           topBorrowAPRAsset = asset;
         }
