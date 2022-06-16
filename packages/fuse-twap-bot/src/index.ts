28ed863e77a4fac4a4eb4227dd1ccdf387aa94fd
carlomazzaferro <carlo.mazzaferro@gmail.com>
2022-06-16 11:36:18 +0200

    linting and smoke tests

--- packages/fuse-twap-bot/src/index.ts
+++ packages/fuse-twap-bot/src/index.ts
@@ -1,12 +1,12 @@
-export { default as updateCumulativePrices } from './updateCumulativePrices';
-export { default as tryUpdateCumulativePrices } from './tryUpdateCumulativePrices';
-export { default as setUpSdk } from './setUpSdk';
-export { default as setPriceAndRepeat } from './setPriceAndRepeat';
-import pino from 'pino';
+export { default as updateCumulativePrices } from "./updateCumulativePrices";
+export { default as tryUpdateCumulativePrices } from "./tryUpdateCumulativePrices";
+export { default as setUpSdk } from "./setUpSdk";
+export { default as setPriceAndRepeat } from "./setPriceAndRepeat";
+import pino from "pino";
 
 export const logger = pino({
   transport: {
-    target: 'pino-pretty',
+    target: "pino-pretty",
     options: {
       colorize: true,
       translateTime: true,
