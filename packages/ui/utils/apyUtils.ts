a3d4d8f24902521af299d01137db440fc0e69a8c
pczek <839848+peetzweg@users.noreply.github.com>
2022-06-20 15:21:35 +0200

    removes old code and uses new function to calculated APY

--- packages/ui/utils/apyUtils.ts
+++ packages/ui/utils/apyUtils.ts
@@ -1,20 +1,5 @@
-import { BigNumber, utils } from 'ethers';
+import { BigNumber } from 'ethers';
 
-export const convertMantissaToAPY = (
-  mantissa: BigNumber,
-  blocksPerMin: number,
-  dayRange: number
-) => {
-  return (
-    (Math.pow(Number(utils.formatUnits(mantissa)) * blocksPerMin * 60 * 24 + 1, dayRange) - 1) * 100
-  );
-};
-
-export const convertMantissaToAPR = (mantissa: BigNumber, blocksPerMin: number) => {
-  return Number(utils.formatUnits(mantissa)) * blocksPerMin * 60 * 24 * 365 * 100;
-};
-
-//
 /**
  * Directly taken from the compound.finance docs:
  * https://compound.finance/docs#protocol-math
