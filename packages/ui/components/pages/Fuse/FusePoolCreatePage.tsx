fd2a2644f685a6f0833c5a6b3b90c6a7310f1c73
smartdevcom <smart.dev.cop@gmail.com>
2022-06-21 09:38:11 +1000

    fixed margin left value

--- packages/ui/components/pages/Fuse/FusePoolCreatePage.tsx
+++ packages/ui/components/pages/Fuse/FusePoolCreatePage.tsx
@@ -279,7 +279,7 @@ export const CreatePoolConfiguration = () => {
               formatValue={formatPercentage}
               min={0}
               max={50}
-              ml={{ base: 'auto', md: 8 }}
+              ml="auto"
               mt={{ base: 4, md: 0 }}
             />
           </Flex>
