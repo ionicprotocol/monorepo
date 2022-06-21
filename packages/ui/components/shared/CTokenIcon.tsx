ea7a38d631593fff36e8f4e89ac62a1c21a30a92
pczek <839848+peetzweg@users.noreply.github.com>
2022-06-21 19:14:36 +0200

    moves getting the proper image to backend

--- packages/ui/components/shared/CTokenIcon.tsx
+++ packages/ui/components/shared/CTokenIcon.tsx
@@ -1,55 +1,67 @@
 import { Avatar, AvatarGroup, AvatarGroupProps, AvatarProps } from '@chakra-ui/avatar';
-import { HStack, Skeleton, Text, useColorMode } from '@chakra-ui/react';
+import { SpinnerIcon } from '@chakra-ui/icons';
+import {
+  HStack,
+  Icon,
+  IconProps,
+  Skeleton,
+  Text,
+  useColorMode,
+  useColorModeValue,
+} from '@chakra-ui/react';
 import { motion } from 'framer-motion';
-import React, { useMemo } from 'react';
 
 import { SimpleTooltip } from '@ui/components/shared/SimpleTooltip';
-import { config } from '@ui/config/index';
-import { useRari } from '@ui/context/RariContext';
 import { useTokenData } from '@ui/hooks/useTokenData';
 
+type PlaceholderIconProps = IconProps;
+const PlaceholderIcon = ({ color, ...restOfProps }: PlaceholderIconProps) => {
+  return (
+    <Icon
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={color}
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...restOfProps}
+    >
+      <circle cx="12" cy="12" r="10" />
+      <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
+      <line x1="12" y1="17" x2="12.01" y2="17" />
+    </Icon>
+  );
+};
+
+interface CTokenIconProps extends AvatarProps {
+  address: string;
+  withTooltip?: boolean;
+  withMotion?: boolean;
+}
 export const CTokenIcon = ({
   address,
   withTooltip = true,
   withMotion = true,
   ...avatarProps
-}: {
-  address: string;
-  withTooltip?: boolean;
-  withMotion?: boolean;
-} & Partial<AvatarProps>) => {
-  const { addressIcons } = useRari();
-  const { data: tokenData } = useTokenData(address);
-  const { colorMode } = useColorMode();
-
-  const iconUrl = useMemo(() => {
-    if (tokenData && tokenData.logoURL) {
-      return tokenData?.logoURL;
-    }
-
-    if (address && addressIcons[address.toLowerCase()]) {
-      return (
-        config.iconServerURL +
-        '/token/96x96/' +
-        addressIcons[address.toLowerCase()].toLowerCase() +
-        '.png'
-      );
-    }
-
-    if (colorMode === 'light') {
-      return '/images/help-circle-dark.svg';
-    }
-
-    return '/images/help-circle-light.svg';
-  }, [address, addressIcons, colorMode, tokenData]);
+}: CTokenIconProps) => {
+  const iconColor = useColorModeValue('#333', '#ddd');
+  const { data: tokenData, isLoading } = useTokenData(address);
 
   return (
     <motion.div whileHover={withMotion ? { scale: 1.2 } : undefined}>
-      <SimpleTooltip label={tokenData?.symbol ?? 'Loading...'} isDisabled={!withTooltip}>
+      <SimpleTooltip label={tokenData?.symbol || address} isDisabled={!withTooltip}>
         <Avatar
-          key={address}
-          name={tokenData?.symbol ?? 'Loading...'}
-          src={iconUrl}
+          name={isLoading ? undefined : tokenData?.name ? tokenData.name : address}
+          icon={
+            isLoading ? (
+              <SpinnerIcon boxSize={'85%'} color={iconColor} opacity={0.3} />
+            ) : (
+              <PlaceholderIcon boxSize={'100%'} color={iconColor} />
+            )
+          }
+          src={tokenData?.logoURL}
           {...avatarProps}
         />
       </SimpleTooltip>
