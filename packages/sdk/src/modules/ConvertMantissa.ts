c0c59ed0c2ab11cfb265bb9c2e4b66bda6ef8831
pczek <839848+peetzweg@users.noreply.github.com>
2022-06-21 15:30:00 +0200

    replaces outdated calculation with new one

--- packages/sdk/src/modules/ConvertMantissa.ts
+++ packages/sdk/src/modules/ConvertMantissa.ts
@@ -2,14 +2,17 @@ import { BigNumber, utils } from "ethers";
 
 import { FuseBaseConstructor } from "../types";
 
+const daysPerYear = 365;
 export function withConvertMantissa<TBase extends FuseBaseConstructor>(Base: TBase) {
   return class ConvertMantissa extends Base {
-    convertMantissaToAPY(mantissa: BigNumber, blocksPerMin: number, dayRange = 365) {
-      return (Math.pow(Number(utils.formatUnits(mantissa)) * blocksPerMin * 60 * 24 + 1, dayRange) - 1) * 100;
-    }
-
-    convertMantissaToAPR(mantissa: BigNumber, blocksPerMin: number) {
-      return Number(utils.formatUnits(mantissa)) * blocksPerMin * 60 * 24 * 365 * 100;
-    }
+    /**
+     * Directly taken from the compound.finance docs:
+     * https://compound.finance/docs#protocol-math
+     */
+    ratePerBlockToAPY = (ratePerBlock: BigNumber, blocksPerMin: number) => {
+      const blocksPerDay = blocksPerMin * 60 * 24;
+      const rateAsNumber = Number(utils.formatUnits(ratePerBlock, 18));
+      return (Math.pow(rateAsNumber * blocksPerDay + 1, daysPerYear) - 1) * 100;
+    };
   };
 }
