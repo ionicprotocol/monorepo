67279e74e9a3cac63cd33e04e2a2eea7af0e98f5
carlomazzaferro <carlo.mazzaferro@gmail.com>
2022-06-24 11:10:09 +0200

    retry on net failure of fetch failure

--- packages/fuse-liquidator-bot/index.ts
+++ packages/fuse-liquidator-bot/index.ts
@@ -6,9 +6,17 @@ import { approveTokensToSafeLiquidator, liquidateAndRepeat, logger } from "./src
 
 dotenv.config();
 
-(async function () {
+(async function runBot() {
   const chainId: number = process.env.TARGET_CHAIN_ID ? parseInt(process.env.TARGET_CHAIN_ID) : SupportedChains.ganache;
   const provider = new JsonRpcProvider(process.env.WEB3_HTTP_PROVIDER_URL);
+
+  try {
+    await provider.getNetwork();
+  } catch (e) {
+    logger.error(`Error (${e}) fetching network, timing out and restarting...`);
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    await runBot();
+  }
 
   logger.info(`Starting liquidation bot on chain: ${chainId}`);
   if (process.env.LIQUIDATION_STRATEGY === "") {
