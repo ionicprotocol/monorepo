c534ecf0fcdda5c9876bb58cba02a5be4fe285e2
pczek <839848+peetzweg@users.noreply.github.com>
2022-06-29 17:14:53 +0200

    brings back APY calculation via Lens contract

--- packages/ui/hooks/rewards/useFlywheelRewardsForPool.ts
+++ packages/ui/hooks/rewards/useFlywheelRewardsForPool.ts
@@ -1,3 +1,4 @@
+import LogRocket from 'logrocket';
 import { useQuery } from 'react-query';
 
 import { useRari } from '@ui/context/RariContext';
@@ -13,9 +14,18 @@ export const useFlywheelRewardsForPool = (poolAddress?: string) => {
     ['useFlywheelRewardsForPool', chainId, poolAddress],
     async () => {
       if (!poolAddress) return undefined;
-      return fuse.getFlywheelMarketRewardsByPool(poolAddress, {
-        from: address,
-      });
+      try {
+        // Try with APRs first
+        return await fuse.getFlywheelMarketRewardsByPoolWithAPR(poolAddress, {
+          from: address,
+        });
+      } catch (error) {
+        LogRocket.captureException(new Error(`Unable to get Rewards with APRs for ${poolAddress}`));
+        // Fallback to rewards without APRs
+        return fuse.getFlywheelMarketRewardsByPool(poolAddress, {
+          from: address,
+        });
+      }
     },
     { enabled: !!poolAddress, initialData: [] }
   );
