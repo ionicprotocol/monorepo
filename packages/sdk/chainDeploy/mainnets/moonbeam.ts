50bd74b1491c4197122e3f6fbc30520cc2bb15a7
carlomazzaferro <carlo.mazzaferro@gmail.com>
2022-06-29 09:13:21 +0200

    moonbeam asset support

--- packages/sdk/chainDeploy/mainnets/moonbeam.ts
+++ packages/sdk/chainDeploy/mainnets/moonbeam.ts
@@ -3,9 +3,9 @@ import { ethers } from "ethers";
 import { SupportedChains } from "../../src";
 import { assetSymbols, chainSpecificParams, chainSupportedAssets } from "../../src/chainConfig";
 import { SupportedAsset } from "../../src/types";
-import { ChainDeployConfig, deployUniswapOracle } from "../helpers";
+import { ChainDeployConfig, deployChainlinkOracle, deployUniswapOracle } from "../helpers";
 import { deployDiaOracle } from "../helpers/dia";
-import { ChainDeployFnParams, DiaAsset } from "../helpers/types";
+import { ChainDeployFnParams, ChainlinkAsset, ChainlinkFeedBaseCurrency, DiaAsset } from "../helpers/types";
 import { deployUniswapLpOracle } from "../oracles/uniswapLp";
 
 const assets = chainSupportedAssets[SupportedChains.moonbeam];
@@ -34,18 +34,72 @@ export const deployConfig: ChainDeployConfig = {
   },
 };
 
-const diaAssets: DiaAsset[] = [
+const chainlinkAssets: ChainlinkAsset[] = [
+  //
   {
-    symbol: assetSymbols.ETH,
-    underlying: assets.find((a: SupportedAsset) => a.symbol === assetSymbols.ETH)!.underlying,
-    feed: "0x1f1BAe8D7a2957CeF5ffA0d957cfEDd6828D728f",
-    key: "ETH/USD",
+    symbol: assetSymbols.ATOM,
+    aggregator: "0x4F152D143c97B5e8d2293bc5B2380600f274a5dd",
+    feedBaseCurrency: ChainlinkFeedBaseCurrency.USD,
   },
   {
-    symbol: assetSymbols.USDC,
-    underlying: assets.find((a: SupportedAsset) => a.symbol === assetSymbols.USDC)!.underlying,
+    symbol: assetSymbols.madWBTC,
+    aggregator: "0x8c4425e141979c66423A83bE2ee59135864487Eb",
+    feedBaseCurrency: ChainlinkFeedBaseCurrency.USD,
+  },
+  {
+    symbol: assetSymbols.xcDOT,
+    aggregator: "0x1466b4bD0C4B6B8e1164991909961e0EE6a66d8c",
+    feedBaseCurrency: ChainlinkFeedBaseCurrency.USD,
+  },
+  {
+    symbol: assetSymbols.ETH,
+    aggregator: "0x9ce2388a1696e22F870341C3FC1E89710C7569B5",
+    feedBaseCurrency: ChainlinkFeedBaseCurrency.USD,
+  },
+  {
+    symbol: assetSymbols.BNB,
+    aggregator: "0x0147f2Ad7F1e2Bc51F998CC128a8355d5AE8C32D",
+    feedBaseCurrency: ChainlinkFeedBaseCurrency.USD,
+  },
+  {
+    symbol: assetSymbols.GLMR,
+    aggregator: "0x4497B606be93e773bbA5eaCFCb2ac5E2214220Eb",
+    feedBaseCurrency: ChainlinkFeedBaseCurrency.USD,
+  },
+  {
+    symbol: assetSymbols.LINK,
+    aggregator: "0xd61D7398B7734aBe7C4B143fE57dC666D2fe83aD",
+    feedBaseCurrency: ChainlinkFeedBaseCurrency.USD,
+  },
+  // stables
+  {
+    symbol: assetSymbols.madUSDC,
+    aggregator: "0xA122591F60115D63421f66F752EF9f6e0bc73abC",
+    feedBaseCurrency: ChainlinkFeedBaseCurrency.USD,
+  },
+  {
+    symbol: assetSymbols.multiUSDC,
+    aggregator: "0xA122591F60115D63421f66F752EF9f6e0bc73abC",
+    feedBaseCurrency: ChainlinkFeedBaseCurrency.USD,
+  },
+  {
+    symbol: assetSymbols.FRAX,
+    aggregator: "0x05Ec3Fb5B7CB3bE9D7150FBA1Fb0749407e5Aa8a",
+    feedBaseCurrency: ChainlinkFeedBaseCurrency.USD,
+  },
+  {
+    symbol: assetSymbols.USDT,
+    aggregator: "0xB97Ad0E74fa7d920791E90258A6E2085088b4320",
+    feedBaseCurrency: ChainlinkFeedBaseCurrency.USD,
+  },
+];
+
+const diaAssets: DiaAsset[] = [
+  {
+    symbol: assetSymbols.KSM,
+    underlying: assets.find((a: SupportedAsset) => a.symbol === assetSymbols.KSM)!.underlying,
     feed: "0x1f1BAe8D7a2957CeF5ffA0d957cfEDd6828D728f",
-    key: "USDC/USD",
+    key: "KSM/USD",
   },
   {
     symbol: assetSymbols.FTM,
@@ -59,6 +113,7 @@ export const deploy = async ({ run, ethers, getNamedAccounts, deployments }: Cha
   console.log("no chain specific deployments to run");
   const { deployer } = await getNamedAccounts();
 
+  //// ORACLES
   //// Uniswap Oracle
   await deployUniswapOracle({ run, ethers, getNamedAccounts, deployments, deployConfig });
   ////
@@ -70,6 +125,16 @@ export const deploy = async ({ run, ethers, getNamedAccounts, deployments }: Cha
     diaAssets,
     deployConfig,
     diaNativeFeed: { feed: "0x8ae08CB9161A38CE241BB54816b2CbA549C136Ae", key: "GLMR/USD" },
+  });
+  //// ChainLinkV2 Oracle
+  await deployChainlinkOracle({
+    run,
+    ethers,
+    getNamedAccounts,
+    deployments,
+    deployConfig,
+    assets: assets,
+    chainlinkAssets,
   });
 
   //// Uniswap Lp Oracle
