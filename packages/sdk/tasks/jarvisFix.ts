f95c84593f2147ca9c92cba0ea3b435d098d97f4
pczek <839848+peetzweg@users.noreply.github.com>
2022-07-07 17:22:51 +0200

    makes all necessary steps to upgrade jarvis pool and strategy

--- packages/sdk/tasks/jarvisFix.ts
+++ packages/sdk/tasks/jarvisFix.ts
@@ -1,9 +1,10 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { task, types } from "hardhat/config";
 
 task("jarvis-fix", "deploy new strategy for jarvis 2brl pool")
   .addParam("signer", "Named account to use fo tx", "deployer", types.string)
   .setAction(async (taskArgs, hre) => {
-    let signer;
+    let signer: SignerWithAddress;
     try {
       signer = await hre.ethers.getNamedSigner(taskArgs.signer);
     } catch {
@@ -14,26 +15,59 @@ task("jarvis-fix", "deploy new strategy for jarvis 2brl pool")
     const fuseModule = await import("../tests/utils/fuseSdk");
     const sdk = await fuseModule.getOrCreateFuse();
 
+    const jarvisComptroller = "0x31d76A64Bc8BbEffb601fac5884372DEF910F044";
     const twobrl = "0x1B6E11c5DB9B15DE87714eA9934a6c52371CfEA9";
-    const market = await sdk
+    const dddFlywheelAddress = "0x851Cc0037B6923e60dC81Fa79Ac0799cC983492c";
+    const epxFlywheelAddress = "0xC6431455AeE17a08D6409BdFB18c4bc73a4069E4";
+    const marketAddress = await sdk
       .createComptroller("0x31d76A64Bc8BbEffb601fac5884372DEF910F044", signer)
       .callStatic.cTokensByUnderlying(twobrl);
 
-    console.log({ market });
-    const args = [
+    // Step 1: Deploy Fresh Strategy with marketAddress=rewardsDestination
+    console.log({ marketAddress });
+
+    const strategyArgs = [
       "0x1B6E11c5DB9B15DE87714eA9934a6c52371CfEA9", //_asset,
-      "0x851Cc0037B6923e60dC81Fa79Ac0799cC983492c", //_dddFlywheel,
-      "0xC6431455AeE17a08D6409BdFB18c4bc73a4069E4", //_epxFlywheel,
+      dddFlywheelAddress, //_dddFlywheel,
+      epxFlywheelAddress, //_epxFlywheel,
       "0x8189F0afdBf8fE6a9e13c69bA35528ac6abeB1af", //_lpDepositor,
-      market, //_rewardsDestination,
+      marketAddress, //_rewardsDestination,
       ["0x84c97300a190676a19D1E13115629A11f8482Bd1", "0xaf41054c1487b0e5e2b9250c0332ecbce6ce9d71"], //_rewardTokens
     ];
-    console.log({ args });
+    console.log({ strategyArgs });
 
-    const deploymentTx = await hre.deployments.deploy("DotDotLpERC4626", {
+    const pluginDeployment = await hre.deployments.deploy("DotDotLpERC4626_" + marketAddress, {
+      contract: "DotDotLpERC4626",
       from: signer.address,
-      args: args,
+      args: strategyArgs,
       log: true,
     });
-    console.log(deploymentTx);
+    console.log(`Plugin deployed successfully: ${pluginDeployment.address}`);
+
+    // Step 2: update plugin, use same implementation
+    const cToken = await sdk.createCToken(marketAddress);
+    const currentImplementation = await cToken.callStatic.implementation();
+    const abiCoder = new hre.ethers.utils.AbiCoder();
+    const implementationData = abiCoder.encode(["address"], [pluginDeployment.address]);
+    const upgradeTx = await cToken._setImplementationSafe(currentImplementation, false, implementationData);
+    const upgradeResult = upgradeTx.wait(2);
+    console.log("changed plugin successfully", upgradeResult);
+
+    // Step 3: enable marketAddress on flywheels
+    const dddFlywheel = sdk.createFuseFlywheelCore(dddFlywheelAddress);
+    const epxFlywheel = sdk.createFuseFlywheelCore(epxFlywheelAddress);
+
+    const dddAddTx = await dddFlywheel.addStrategyForRewards(marketAddress);
+    const resultDDD = await dddAddTx.wait(2);
+    console.log("enabled market on dddFWC", resultDDD);
+    const epxAddTx = await epxFlywheel.addStrategyForRewards(marketAddress);
+    const resultEPX = await epxAddTx.wait(2);
+    console.log("enabled market on epxFWC", resultEPX);
+
+    // Step 4: add Flywheels to market
+    await sdk.addFlywheelCoreToComptroller(dddFlywheelAddress, jarvisComptroller, { from: signer.address });
+    console.log("dddFWC added to comptroller");
+    await sdk.addFlywheelCoreToComptroller(epxFlywheelAddress, jarvisComptroller, { from: signer.address });
+    console.log("epxFWC added to comptroller");
+    console.log("DONE");
   });
