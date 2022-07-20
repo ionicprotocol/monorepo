415f305a9f190474221aa6945d56e71042fdd9b0
coolnft <haruto@midascapital.xyz>
2022-07-20 09:15:19 -0400

    config variable name changed.

--- packages/functions/src/config/variables.ts
+++ packages/functions/src/config/variables.ts
@@ -5,7 +5,7 @@ const config = {
   chain: parseInt(process.env.SUPPORT_CHAIN ?? '56', 10),
   rpcUrl: process.env.SUPPORT_RPC_URL ?? 'https://bsc-dataseed1.binance.org/',
   supabaseUrl: process.env.SUPABASE_URL ?? '',
-  supabasePublicKey: process.env.SUPABASE_PUBLIC_KEY ?? '',
+  supabasePublicKey: process.env.SUPABASE_KEY ?? '',
   supabasePluginTableName: process.env.SUPABASE_PLUGIN_TABLE_NAME ?? '',
   supabaseFlywheelTableName: process.env.SUPABASE_FLYWHEEL_TABLE_NAME ?? '',
 };
