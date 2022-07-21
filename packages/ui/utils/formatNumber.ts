4b7d456363ba9cd8b63ed3b55329e2d64128e936
smartdevcom <smart.dev.cop@gmail.com>
2022-07-21 11:30:27 +1000

    fixed toFixedNoRound func

--- packages/ui/utils/formatNumber.ts
+++ packages/ui/utils/formatNumber.ts
@@ -1,31 +1,9 @@
-export const toFixedNoRound = (value: number, len: number) => {
-  let resultStr = '';
-  if (Math.abs(value) < 1.0) {
-    const e = parseInt(value.toString().split('e-')[1]);
-    if (e) {
-      if (e > len) {
-        resultStr = '0';
-      } else {
-        const val = value * Math.pow(10, e - 1);
-        resultStr = '0.' + new Array(e).join('0') + val.toString().substring(2, 3 + len - e);
-      }
-    } else {
-      const value1 = value.toString().split('.')[0];
-      const value2 = value.toString().split('.')[1];
-      if (value2) {
-        resultStr = value1 + '.' + value2.slice(0, len);
-      } else {
-        resultStr = value1;
-      }
-    }
-  } else {
-    let e = parseInt(value.toString().split('+')[1]);
-    if (e > 20) {
-      e -= 20;
-      const val = value / Math.pow(10, e);
-      resultStr = val + new Array(e + 1).join('0');
-    }
-  }
+import { format } from 'mathjs';
 
-  return resultStr;
+export const toFixedNoRound = (value: number, len: number) => {
+  const factor = Math.pow(10, len);
+  const val = Math.floor(value * factor) / factor;
+  const str = format(val, { notation: 'fixed' });
+
+  return str;
 };
