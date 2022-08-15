13b0963f849a4131215c6bf9e5fd8b58433a8f26
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2022-08-15 11:10:24 +0300

    making the flash swap fee an init param

--- packages/sdk/chainDeploy/helpers/liquidators/fuseSafeLiquidator.ts
+++ packages/sdk/chainDeploy/helpers/liquidators/fuseSafeLiquidator.ts
@@ -34,6 +34,7 @@ export const deployFuseSafeLiquidator = async ({
             deployConfig.stableToken ?? constants.AddressZero,
             deployConfig.wBTCToken ?? constants.AddressZero,
             deployConfig.uniswap.pairInitHashCode ?? "0x",
+            deployConfig.uniswap.flashSwapFee
           ],
         },
         onUpgrade: {
