fbc113687a840d870cf196ad7b171c1bb5df2fae
pczek <839848+peetzweg@users.noreply.github.com>
2022-08-17 15:15:21 +0200

    adds new function to save native prices in supabase every 5 minutes

--- packages/functions/src/config/variables.ts
+++ packages/functions/src/config/variables.ts
@@ -5,6 +5,7 @@ const config = {
   supabaseUrl: process.env.SUPABASE_URL ?? '',
   supabasePublicKey: process.env.SUPABASE_KEY ?? '',
   supabasePluginTableName: process.env.SUPABASE_PLUGIN_TABLE_NAME ?? '',
+  supabaseNativePricesTableName: process.env.SUPABASE_NATIVE_PRICES_TABLE_NAME ?? '',
   supabaseFlywheelTableName: process.env.SUPABASE_FLYWHEEL_TABLE_NAME ?? '',
 };
 
--- packages/functions/src/functions/save-native-prices.ts
+++ packages/functions/src/functions/save-native-prices.ts
@@ -1,6 +1,7 @@
+import { bsc, moonbeam, polygon } from '@midas-capital/chains';
 import { Handler } from '@netlify/functions';
 import axios from 'axios';
-import { bsc, polygon, moonbeam } from '@midas-capital/chains';
+import { config, supabase } from '../config';
 
 const NATIVE_ASSETS = {
   [bsc.chainId]: bsc.specificParams.cgId,
@@ -15,9 +16,23 @@ async function getUSDPriceOf(cgIds: string[]): Promise<number[]> {
   return cgIds.map((cgId) => (data[cgId] ? data[cgId].usd : 1));
 }
 
-const handler: Handler = async (event, context) => {
-  const [chainIds, cgIds] = Object.entries(NATIVE_ASSETS);
-  await getUSDPriceOf(cgIds);
+const handler: Handler = async () => {
+  const chainIds = Object.keys(NATIVE_ASSETS);
+  const cgIds = Object.values(NATIVE_ASSETS);
+  const prices = await getUSDPriceOf(cgIds);
+
+  const upserts = prices.map((price, index) => ({
+    chainId: chainIds[index],
+    usd: price,
+  }));
+
+  const { error } = await supabase.from(config.supabaseNativePricesTableName).upsert(upserts);
+  if (error) {
+    throw `Error occurred during saving native prices:  ${error.message}`;
+  } else {
+    console.log(`Successfully updated native prices for: ${chainIds.join(', ')}`);
+  }
+
   return {
     statusCode: 200,
     body: JSON.stringify({ message: 'done' }),
