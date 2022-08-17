64485baa0f3a925220b89f8c3b9592c92d092ed0
smartdevcom <smart.dev.cop@gmail.com>
2022-08-17 12:17:18 +1000

    Fixed style

--- packages/chains/src/common/docs.ts
+++ packages/chains/src/common/docs.ts
@@ -21,7 +21,7 @@ export const pancakeSwapDocs = (token0: string, token1: string, poolName: string
   return `<p><b>How to acquire this token</b><p/><br />
   <p> 1. Head to <a href="https://pancakeswap.finance/add/${token0}/${token1}" target="_blank">Pancakeswap</a>.</p><br />
   <p> 2. Ensure that the tokens are correct, and tap "Add Liquidity".</p>
-  <p><b>NOTE:</b> you might have to convert between tokens and/or have to approve Pancakeswap to spend them. Finally, click on supply.</p>
+  <p><b>NOTE:</b> You might have to convert between tokens and/or have to approve Pancakeswap to spend them. Finally, click on supply.</p>
   <p>You will get back <a href="https://bscscan.com/address/${tokenAddress}" target="_blank">Pancakeswap ${poolName} LP tokens</a> in your wallet.</p><br />
   <p> 3. Come back here and hit "MAX" to deposit them all in this pool.</p>
   `;
@@ -31,7 +31,7 @@ export const quickSwapDocs = (token0: string, token1: string, poolName: string, 
   return `<p><b>How to acquire this token</b><p/><br />
   <p> 1. Head to <a href="https://quickswap.exchange/#/pools">Quickswap</a>.</p><br />
   <p> 2. Input the token addresses -- token 1: ${token0} and  token 2: ${token1}".</p>
-  <p><b>NOTE:</b> you might have to convert between tokens and/or have to approve QuickSwap to spend them.</p><br />
+  <p><b>NOTE:</b> You might have to convert between tokens and/or have to approve QuickSwap to spend them.</p><br />
   <p> 3. Finally, click on supply.</p>
   <p>You will get back <a href="https://polygonscan.com/address/${tokenAddress}">Quickswap ${poolName} LP tokens</a> in your wallet.</p><br />
   <p> 4. Come back here and hit "MAX" to deposit them all in this pool.</p>
@@ -41,7 +41,7 @@ export const quickSwapDocs = (token0: string, token1: string, poolName: string, 
 export const beamSwapDocs = (token0: string, token1: string, poolName: string, tokenAddress: string) => {
   return `<p><b>How to acquire this token</b><p/><br />
   <p> 1. Head to <a href="https://app.beamswap.io/exchange/add/${token0}/${token1}">BeamSwap</a> and supply the desired liquidity pairs.</p>
-  <p><b>NOTE:</b> you might have to convert between tokens and/or have to approve BeamSwap to spend them.</p><br />
+  <p><b>NOTE:</b> You might have to convert between tokens and/or have to approve BeamSwap to spend them.</p><br />
   <p> 2. You will get back <a href="https://moonbeam.moonscan.com/address/${tokenAddress}">BeamSwap ${poolName} LP tokens</a> in your wallet.</p><br />
   <p> 3. Come back here and hit "MAX" to deposit them all in this pool. </p>
   `;
@@ -50,7 +50,7 @@ export const beamSwapDocs = (token0: string, token1: string, poolName: string, t
 export const beamSwapStableDocs = (poolName: string, tokenAddress: string) => {
   return `<p><b>How to acquire this token</b><p/><br />
   <p> 1. Head to <a href="https://app.beamswap.io/exchange/add/multi/${poolName}">BeamSwap</a> and supply the desired liquidity pairs.<p>
-  <p><b>NOTE:</b> you might have to convert between tokens and/or have to approve BeamSwap to spend them.</p><br />
+  <p><b>NOTE:</b> You might have to convert between tokens and/or have to approve BeamSwap to spend them.</p><br />
   <p> 2. You will get back <a href="https://moonbeam.moonscan.com/address/${tokenAddress}">BeamSwap ${poolName} Stable LP tokens</a> in your wallet.</p><br />
   <p> 3. Come back here and hit "MAX" to deposit them all in this pool. </p>
   `;
@@ -78,10 +78,11 @@ export const jarvisDocs = (v: string) => {
 };
 
 export const arrakisDocs = (networkName: string, chainId: number, vaultAddress: string) => {
-  return `<p><b>How to acquire this Arrakis Vault token</b><p/><br /><p> 1. Make sure you are connected to ${networkName} Network on your browser wallet </p><br />
-  <p> 2. Head to the <a href="https://beta.arrakis.finance/vaults/${chainId}/${vaultAddress}/add" target="_blank">Arrakis Finance Vault</a> and
-  deposit the desired amount of token pairs. <b>NOTE:</b> you might have to convert between tokens and/or have to approve Arrakis to spend them. </p><br />
-  <p> 3. Click on "Deposit & Stake". This will credit your wallet with the Arrakis Vault Tokens</p><br />
+  return `<p><b>How to acquire this Arrakis Vault token</b><p/><br /><p> 1. Make sure you are connected to ${networkName} Network on your browser wallet.</p><br />
+  <p> 2. Head to the <a href="https://beta.arrakis.finance/vaults/${chainId}/${vaultAddress}/add" target="_blank">Arrakis Finance Vault</a> and deposit the desired amount of token pairs.</p>
+  <p><b>NOTE:</b> You might have to convert between tokens and/or have to approve Arrakis to spend them. </p><br />
+  <p> 3. Click on "Deposit & Stake".</p>
+  <p>This will credit your wallet with the Arrakis Vault Tokens</p><br />
   <p> 4. Come back back here and hit "MAX" to deposit them all in this pool.</p>
   `;
 };
