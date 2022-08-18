689bf1b1a95e8e3c016b68bd7da061f3599def3f
carlomazzaferro <carlo.mazzaferro@gmail.com>
2022-08-18 18:46:31 +0100

    mimo irm

--- packages/chains/src/polygon/irms.ts
+++ packages/chains/src/polygon/irms.ts
@@ -2,6 +2,6 @@ import { IrmTypes } from "@midas-capital/types";
 
 const baseIrms = [IrmTypes.WhitePaperInterestRateModel, IrmTypes.JumpRateModel];
 
-const irms: IrmTypes[] = [...baseIrms];
+const irms: IrmTypes[] = [...baseIrms, IrmTypes.JumpRateModel_MIMO_002_018_4_04];
 
 export default irms;
