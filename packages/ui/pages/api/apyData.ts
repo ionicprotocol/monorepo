c4ccdad7d6361b428081c8035319b01f7270e903
pczek <839848+peetzweg@users.noreply.github.com>
2022-08-22 11:49:16 +0200

    comment out code to prevent linter errors

--- packages/ui/pages/api/apyData.ts
+++ packages/ui/pages/api/apyData.ts
@@ -33,6 +33,7 @@ interface PluginState extends MarketState {
   pluginAddress: string;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 interface FlywheelState extends MarketState {
   underlyingAddress: string;
   rewardAddress: string;
@@ -65,58 +66,59 @@ const handler = async (req: NextApiRequest, res: NextApiResponse<APYResult>) => 
  * @param query Validated NextApiRequest query
  * @returns
  */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 async function rewardTokenAPY(query: Query): Promise<APYResult> {
   // TODO https://github.com/Midas-Protocol/monorepo/issues/543
   return { error: 'Not yet supported.' };
-  const client = createClient(config.supabaseUrl, config.supabasePublicKey);
-  const dateLimit = new Date();
-  dateLimit.setDate(dateLimit.getDate() - parseInt('7', 10));
-  const { chain, pluginAddress, rewardAddress, underlyingAddress } = query;
+  // const client = createClient(config.supabaseUrl, config.supabasePublicKey);
+  // const dateLimit = new Date();
+  // dateLimit.setDate(dateLimit.getDate() - parseInt('7', 10));
+  // const { chain, pluginAddress, rewardAddress, underlyingAddress } = query;
 
-  const [start, end] = await Promise.all([
-    client
-      .from<FlywheelState>(config.supabaseFlywheelTableName)
-      .select('totalAssets,created_at')
-      .eq('chain', parseInt(chain as string, 10))
-      .eq('pluginAddress', (pluginAddress as string).toLowerCase())
-      .eq('rewardAddress', (rewardAddress as string).toLowerCase())
-      .eq('underlyingAddress', (underlyingAddress as string).toLowerCase())
-      .gte('created_at', dateLimit.toISOString())
-      .order('created_at', { ascending: true })
-      .limit(1),
-    client
-      .from<FlywheelState>(config.supabaseFlywheelTableName)
-      .select('totalAssets,created_at')
-      .eq('chain', parseInt(chain as string, 10))
-      .eq('pluginAddress', (pluginAddress as string).toLowerCase())
-      .eq('rewardAddress', (rewardAddress as string).toLowerCase())
-      .eq('underlyingAddress', (underlyingAddress as string).toLowerCase())
-      .order('created_at', { ascending: false })
-      .limit(1),
-  ]);
+  // const [start, end] = await Promise.all([
+  //   client
+  //     .from<FlywheelState>(config.supabaseFlywheelTableName)
+  //     .select('totalAssets,created_at')
+  //     .eq('chain', parseInt(chain as string, 10))
+  //     .eq('pluginAddress', (pluginAddress as string).toLowerCase())
+  //     .eq('rewardAddress', (rewardAddress as string).toLowerCase())
+  //     .eq('underlyingAddress', (underlyingAddress as string).toLowerCase())
+  //     .gte('created_at', dateLimit.toISOString())
+  //     .order('created_at', { ascending: true })
+  //     .limit(1),
+  //   client
+  //     .from<FlywheelState>(config.supabaseFlywheelTableName)
+  //     .select('totalAssets,created_at')
+  //     .eq('chain', parseInt(chain as string, 10))
+  //     .eq('pluginAddress', (pluginAddress as string).toLowerCase())
+  //     .eq('rewardAddress', (rewardAddress as string).toLowerCase())
+  //     .eq('underlyingAddress', (underlyingAddress as string).toLowerCase())
+  //     .order('created_at', { ascending: false })
+  //     .limit(1),
+  // ]);
 
-  if (
-    start.error ||
-    end.error ||
-    start.data.length === 0 ||
-    end.data.length === 0 ||
-    start.data[0].created_at === end.data[0].created_at ||
-    parseFloat(start.data[0].totalAssets) === 0 ||
-    parseFloat(end.data[0].totalAssets) === 0
-  ) {
-    return { apy: undefined, error: 'Not enough data yet to calculate APY' };
-  }
+  // if (
+  //   start.error ||
+  //   end.error ||
+  //   start.data.length === 0 ||
+  //   end.data.length === 0 ||
+  //   start.data[0].created_at === end.data[0].created_at ||
+  //   parseFloat(start.data[0].totalAssets) === 0 ||
+  //   parseFloat(end.data[0].totalAssets) === 0
+  // ) {
+  //   return { apy: undefined, error: 'Not enough data yet to calculate APY' };
+  // }
 
-  const [startAssets, endAssets] = [
-    parseFloat(start.data[0].totalAssets),
-    parseFloat(end.data[0].totalAssets),
-  ];
+  // const [startAssets, endAssets] = [
+  //   parseFloat(start.data[0].totalAssets),
+  //   parseFloat(end.data[0].totalAssets),
+  // ];
 
-  const date1 = end.data[0].created_at;
-  const date2 = start.data[0].created_at;
-  const dateDelta = new Date(date1).getTime() - new Date(date2).getTime();
-  const millisecondsInADay = 86_400_000;
-  return { apy: (Math.log(endAssets / startAssets) / dateDelta) * millisecondsInADay * 365 };
+  // const date1 = end.data[0].created_at;
+  // const date2 = start.data[0].created_at;
+  // const dateDelta = new Date(date1).getTime() - new Date(date2).getTime();
+  // const millisecondsInADay = 86_400_000;
+  // return { apy: (Math.log(endAssets / startAssets) / dateDelta) * millisecondsInADay * 365 };
 }
 
 async function underlyingTokenAPY(query: Query): Promise<APYResult> {
