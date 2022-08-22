8acfc92e686bd06f4f841ad4bc139cae790abfce
carlomazzaferro <carlo.mazzaferro@gmail.com>
2022-08-22 13:30:11 +0200

    fix irm setting

--- packages/sdk/tasks/irm/admin.ts
+++ packages/sdk/tasks/irm/admin.ts
@@ -9,14 +9,11 @@ export default task("irm:set", "Set new IRM to ctoken")
     // @ts-ignore
     const midasSdkModule = await import("../../tests/utils/midasSdk");
     const sdk = await midasSdkModule.getOrCreateMidas();
-    const pool = await sdk.fetchFusePoolData("3", undefined);
-    console.log(pool.id, pool.name);
-    for (const asset of pool.assets) {
-      const cToken = new ethers.Contract(asset.cToken, sdk.chainDeployment.CErc20Delegate.abi, deployer);
-      const tx = await cToken._setInterestRateModel("0x01BBE11e0212eF4B3DDa71e5F715eed4Db0C84eC");
-      await tx.wait();
-      console.log(`Set IRM of ${await cToken.callStatic.underlying()} to ${_irm}`);
-    }
 
+    const cToken = new ethers.Contract(_ctoken, sdk.chainDeployment.CErc20Delegate.abi, deployer);
     // const interestRateModel = await ethers.getContractAt(_irm, await sdk.irms[_irm].address, deployer);
+
+    const tx = await cToken._setInterestRateModel(sdk.irms[_irm].address);
+    await tx.wait();
+    console.log(`Set IRM of ${await cToken.callStatic.underlying()} to ${_irm}`);
   });
