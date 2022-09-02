6569f6e8a02eae51d0662c2d270845bfe458ed73
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2022-09-02 12:34:26 +0300

    linting

--- packages/sdk/chainDeploy/helpers/getCgPrice.ts
+++ packages/sdk/chainDeploy/helpers/getCgPrice.ts
@@ -7,7 +7,8 @@ export const getCgPrice = async (coingeckoId: string) => {
 
   let usdPrice: number;
 
-  const data = (await axios.get(`https://api.coingecko.com/api/v3/simple/price?vs_currencies=usd&ids=${coingeckoId}`)).data[coingeckoId]
+  const data = (await axios.get(`https://api.coingecko.com/api/v3/simple/price?vs_currencies=usd&ids=${coingeckoId}`))
+    .data[coingeckoId];
   usdPrice = data?.usd as number;
 
   // set 1.0 for undefined token prices in coingecko
