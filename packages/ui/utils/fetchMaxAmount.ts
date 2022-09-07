2a016836d8ce15031ecad714dd799c7bc6db3620
pczek <839848+peetzweg@users.noreply.github.com>
2022-09-07 18:33:27 +0200

    scales by proper decimal number instead of default 18

--- packages/ui/utils/fetchMaxAmount.ts
+++ packages/ui/utils/fetchMaxAmount.ts
@@ -46,7 +46,9 @@ export const fetchMaxAmount = async (
       { from: address }
     );
 
-    maxRedeem = utils.parseUnits(toFixedNoRound(utils.formatUnits(maxRedeem), 7));
+    maxRedeem = utils.parseUnits(
+      toFixedNoRound(utils.formatUnits(maxRedeem, asset.underlyingDecimals), 7)
+    );
 
     if (maxRedeem) {
       return BigNumber.from(maxRedeem);
