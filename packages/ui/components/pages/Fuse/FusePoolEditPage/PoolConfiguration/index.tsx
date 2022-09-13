979f1583973f50e0edfed27b3f0fcaae8a549c21
pczek <839848+peetzweg@users.noreply.github.com>
2022-09-13 14:18:46 +0200

    fix uses of `useExtraPoolInfo`

--- packages/ui/components/pages/Fuse/FusePoolEditPage/PoolConfiguration/index.tsx
+++ packages/ui/components/pages/Fuse/FusePoolEditPage/PoolConfiguration/index.tsx
@@ -22,7 +22,7 @@ import { useQueryClient } from '@tanstack/react-query';
 import { BigNumber, Contract, ContractTransaction, utils } from 'ethers';
 import LogRocket from 'logrocket';
 import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
 
 import { WhitelistInfo } from '@ui/components/pages/Fuse/FusePoolCreatePage/WhitelistInfo';
@@ -59,7 +59,7 @@ const PoolConfiguration = ({
   const errorToast = useErrorToast();
   const successToast = useSuccessToast();
 
-  const data = useExtraPoolInfo(comptrollerAddress);
+  const { data } = useExtraPoolInfo(comptrollerAddress);
 
   const [inputPoolName, setInputPoolName] = useState<string>(poolName);
   const [isEditable, setIsEditable] = useState<boolean>(false);
