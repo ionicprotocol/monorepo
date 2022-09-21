3826c3f5f0760d3a384bfd99ade1b55143bb97a0
smartdevcom <smart.dev.cop@gmail.com>
2022-09-21 13:46:00 +1000

    added keys

--- packages/ui/pages/_document.tsx
+++ packages/ui/pages/_document.tsx
@@ -13,39 +13,50 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="robots" content="index,follow" />
           <meta
             name="description"
             content="Build custom lending and borrowing pools for any group of assets that are isolated from other pools and assets within the protocol. Use any asset as collateral and optimize the capital efficiency of your holdings."
           />
-          <meta property="og:type" content="website" />
-          <meta property="og:site_name" content="Midas Capital" />
-          <meta
-            property="og:image"
-            content="https://d1912tcoux65lj.cloudfront.net/social/preview.png"
-          />
-          <meta
-            property="og:title"
-            content="Midas - Money markets for all. Lending and borrowing pools for any asset."
-          />
-          <meta
-            property="og:description"
-            content="Build custom lending and borrowing pools for any group of assets that are isolated from other pools and assets within the protocol. Use any asset as collateral and optimize the capital efficiency of your holdings."
-          />
-          <meta name="twitter:card" content="summary_large_image" />
+
+          <meta name="twitter:card" content="summary_large_image" key="twcard" />
           <meta
             name="twitter:title"
             content="Midas - Money markets for all. Lending and borrowing pools for any asset."
+            key="twtitle"
           />
           <meta
             name="twitter:description"
             content="Build custom lending and borrowing pools for any group of assets that are isolated from other pools and assets within the protocol. Use any asset as collateral and optimize the capital efficiency of your holdings."
+            key="twdesc"
           />
-          <meta name="twitter:url" content="https://app.midascapital.xyz/" />
+          <meta name="twitter:url" content="https://app.midascapital.xyz/" key="twurl" />
           <meta
             name="twitter:image"
             content="https://d1912tcoux65lj.cloudfront.net/social/preview.png"
+            key="twimage"
           />
+
+          <meta property="og:type" content="website" key="ogtype" />
+          <meta property="og:site_name" content="Midas Capital" key="ogsitename" />
+          <meta
+            property="og:image"
+            content="https://d1912tcoux65lj.cloudfront.net/social/preview.png"
+            key="ogimage"
+          />
+          <meta
+            property="og:title"
+            content="Midas - Money markets for all. Lending and borrowing pools for any asset."
+            key="ogtitle"
+          />
+          <meta
+            property="og:description"
+            content="Build custom lending and borrowing pools for any group of assets that are isolated from other pools and assets within the protocol. Use any asset as collateral and optimize the capital efficiency of your holdings."
+            key="ogdesc"
+          />
+
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link
