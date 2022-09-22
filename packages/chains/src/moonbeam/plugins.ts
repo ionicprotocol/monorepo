e387834f41a6c63f971d4bc805a7f9594cc45a2f
carlomazzaferro <carlo.mazzaferro@gmail.com>
2022-09-22 15:07:05 +0200

    moonbam

--- packages/chains/src/moonbeam/plugins.ts
+++ packages/chains/src/moonbeam/plugins.ts
@@ -1,4 +1,6 @@
-import { DeployedPlugins } from "@midas-capital/types";
+import { assetSymbols, DeployedPlugins, underlying } from "@midas-capital/types";
+
+import assets from "./assets";
 
 const deployedPlugins: DeployedPlugins = {
   "0x0DaFF7aaaE63F1Fc30c1C40816257513D052b649": {
@@ -7,12 +9,12 @@ const deployedPlugins: DeployedPlugins = {
     strategy: "StellaLpERC4626",
     apyDocsUrl: "https://app.stellaswap.com/farm",
     strategyDocsUrl: "https://docs.midascapital.xyz/guides/assets-and-strategies-addresses/moonbeam-coming-soon",
-    underlying: "0xf4C10263f2A4B1f75b8a5FD5328fb61605321639",
+    underlying: underlying(assets, assetSymbols["ATOM-GLMR"]),
     otherParams: [
       "0xF3a5454496E26ac57da879bf3285Fa85DEBF0388",
       "5",
       "0x85Ff07b5F3454143531F36Bd6bEd92654d0681eD",
-      ["0x0E358838ce72d5e61E0018a2ffaC4bEC5F4c88d2", "0xAcc15dC74880C9944775448304B263D191c6077F"],
+      [underlying(assets, assetSymbols.STELLA), underlying(assets, assetSymbols.WGLMR)],
     ],
   },
   "0x46eC3122C73CA62A18FFCFd434cDc1C341Fe96dB": {
@@ -21,12 +23,12 @@ const deployedPlugins: DeployedPlugins = {
     strategy: "StellaLpERC4626",
     apyDocsUrl: "https://app.stellaswap.com/farm",
     strategyDocsUrl: "https://docs.midascapital.xyz/guides/assets-and-strategies-addresses/moonbeam-coming-soon",
-    underlying: "0xa927E1e1E044CA1D9fe1854585003477331fE2Af",
+    underlying: underlying(assets, assetSymbols["WGLMR-xcDOT"]),
     otherParams: [
       "0xF3a5454496E26ac57da879bf3285Fa85DEBF0388",
       "10",
       "0x32Be4b977BaB44e9146Bb414c18911e652C56568",
-      ["0x0E358838ce72d5e61E0018a2ffaC4bEC5F4c88d2", "0xAcc15dC74880C9944775448304B263D191c6077F"]
+      [underlying(assets, assetSymbols.STELLA), underlying(assets, assetSymbols.WGLMR)],
     ],
   },
 };
