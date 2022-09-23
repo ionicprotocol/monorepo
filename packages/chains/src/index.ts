32f090b90de3bc8d1486de53f4c18593f75cbb8a
carlomazzaferro <carlo.mazzaferro@gmail.com>
2022-09-23 12:57:32 +0200

    fix exports

--- packages/chains/src/index.ts
+++ packages/chains/src/index.ts
@@ -1,13 +1,3 @@
-import { ChainConfig } from "@midas-capital/types";
-
-import { default as bsc } from "./bsc";
-import { default as polygon } from "./polygon";
-import { default as moonbeam } from "./moonbeam";
-import { default as neondevnet } from "./neondevnet";
-import { default as chapel } from "./chapel";
-import { default as ganache } from "./ganache";
-import { default as arbitrum } from "./arbitrum";
-
 export { default as bsc } from "./bsc";
 export { default as polygon } from "./polygon";
 export { default as moonbeam } from "./moonbeam";
@@ -16,13 +6,3 @@ export { default as neondevnet } from "./neondevnet";
 export { default as evmos } from "./evmos";
 export { default as chapel } from "./chapel";
 export { default as arbitrum } from "./arbitrum";
-
-export const chainIdToConfig: { [chainId: number]: ChainConfig } = {
-    [bsc.chainId]: bsc,
-    [polygon.chainId]: polygon,
-    [moonbeam.chainId]: moonbeam,
-    [neondevnet.chainId]: neondevnet,
-    [chapel.chainId]: chapel,
-    [ganache.chainId]: ganache,
-    [arbitrum.chainId]: arbitrum,
-  };
