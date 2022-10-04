6715d4ce90478917a843dd03f1b6626f359c63b1
smartdevcom <smart.dev.cop@gmail.com>
2022-10-04 22:11:36 +1000

    dynamic import

--- packages/ui/pages/create-pool/index.tsx
+++ packages/ui/pages/create-pool/index.tsx
@@ -1,9 +1,17 @@
 import { NextPage } from 'next';
+import dynamic from 'next/dynamic';
 import Head from 'next/head';
 import { useEffect } from 'react';
 
-import FusePoolCreate from '@ui/components/pages/Fuse/FusePoolCreatePage';
 import { useMultiMidas } from '@ui/context/MultiMidasContext';
+
+const FusePoolCreate = dynamic(() => import('@ui/components/pages/Fuse/FusePoolCreatePage'), {
+  ssr: false,
+});
+
+export async function getInitialProps() {
+  return {};
+}
 
 const FusePoolCreatePage: NextPage = () => {
   const { setGlobalLoading } = useMultiMidas();
