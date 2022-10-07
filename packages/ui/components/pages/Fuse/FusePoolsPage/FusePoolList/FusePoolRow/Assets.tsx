30ce4e750d907212244a2e57acfe2743ac08c257
smartdevcom <smart.dev.cop@gmail.com>
2022-10-08 00:30:04 +1000

    added +XY, used router when logo click

--- packages/ui/components/pages/Fuse/FusePoolsPage/FusePoolList/FusePoolRow/Assets.tsx
+++ packages/ui/components/pages/Fuse/FusePoolsPage/FusePoolList/FusePoolRow/Assets.tsx
@@ -1,4 +1,4 @@
-import { AvatarGroup, VStack } from '@chakra-ui/react';
+import { AvatarGroup, HStack, Text, VStack } from '@chakra-ui/react';
 import { useMemo } from 'react';
 
 import { CTokenIcon } from '@ui/components/shared/CTokenIcon';
@@ -13,13 +13,16 @@ export const Assets = ({ pool }: { pool: PoolData }) => {
   }, [pool.underlyingSymbols, pool.underlyingTokens]);
 
   return (
-    <VStack alignItems={'flex-start'} px={{ base: 2, lg: 4 }} py={4} width="250px">
+    <VStack alignItems={'flex-start'} px={{ base: 2, lg: 4 }} py={4} width="280px">
       {pool.underlyingTokens.length === 0 ? null : (
-        <AvatarGroup size="sm" max={30}>
-          {tokens.slice(0, 10).map((token, i) => (
-            <CTokenIcon key={i} address={token.address} chainId={pool.chainId} />
-          ))}
-        </AvatarGroup>
+        <HStack spacing={0}>
+          <AvatarGroup size="sm" max={30}>
+            {tokens.slice(0, 10).map((token, i) => (
+              <CTokenIcon key={i} address={token.address} chainId={pool.chainId} />
+            ))}
+          </AvatarGroup>
+          {tokens.length - 10 > 0 && <Text fontWeight="bold">+{tokens.length - 10}</Text>}
+        </HStack>
       )}
     </VStack>
   );
