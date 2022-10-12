56f58dcca8fc8b3d2617a42f7b2b5266f9f70a06
pczek <839848+peetzweg@users.noreply.github.com>
2022-10-12 18:14:55 +0200

    use strategy enum to prevent mispellings

--- packages/chains/src/moonbeam/plugins.ts
+++ packages/chains/src/moonbeam/plugins.ts
@@ -1,4 +1,4 @@
-import { assetSymbols, DeployedPlugins, underlying } from "@midas-capital/types";
+import { assetSymbols, DeployedPlugins, Strategy, underlying } from "@midas-capital/types";
 
 import assets from "./assets";
 
@@ -6,7 +6,7 @@ const deployedPlugins: DeployedPlugins = {
   "0x0DaFF7aaaE63F1Fc30c1C40816257513D052b649": {
     market: "0x85Ff07b5F3454143531F36Bd6bEd92654d0681eD",
     name: "GLMR-ATOM",
-    strategy: "StellaLpERC4626",
+    strategy: Strategy.Stella,
     apyDocsUrl: "https://app.stellaswap.com/farm",
     strategyDocsUrl: "https://docs.midascapital.xyz/guides/assets-and-strategies-addresses/moonbeam-coming-soon",
     underlying: underlying(assets, assetSymbols["ATOM-GLMR"]),
@@ -20,7 +20,7 @@ const deployedPlugins: DeployedPlugins = {
   "0x46eC3122C73CA62A18FFCFd434cDc1C341Fe96dB": {
     market: "0x32Be4b977BaB44e9146Bb414c18911e652C56568",
     name: "GLMR-xc.DOT",
-    strategy: "StellaLpERC4626",
+    strategy: Strategy.Stella,
     apyDocsUrl: "https://app.stellaswap.com/farm",
     strategyDocsUrl: "https://docs.midascapital.xyz/guides/assets-and-strategies-addresses/moonbeam-coming-soon",
     underlying: underlying(assets, assetSymbols["WGLMR-xcDOT"]),
--- packages/types/src/config.ts
+++ packages/types/src/config.ts
@@ -42,10 +42,18 @@ export type InterestRateModelConf = {
   interestRateModelParams?: InterestRateModelParams;
 };
 
+export enum Strategy {
+  Beefy = "BeefyERC4626",
+  Arrakis = "ArrakisERC4626",
+  DotDot = "DotDotLpERC4626",
+  Stella = "StellaLpERC4626",
+  Bomb = "BombERC4626",
+}
+
 export interface PluginData {
   market: string;
   name: string;
-  strategy?: string;
+  strategy?: Strategy;
   apyDocsUrl?: string;
   strategyDocsUrl?: string;
   underlying: string;
