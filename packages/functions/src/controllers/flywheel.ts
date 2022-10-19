ae6eeaefbe8a2761020f16367899e44d503e79a7
pczek <839848+peetzweg@users.noreply.github.com>
2022-10-19 12:10:06 +0200

    minor changes to trigger deployment

--- packages/functions/src/controllers/flywheel.ts
+++ packages/functions/src/controllers/flywheel.ts
@@ -46,13 +46,16 @@ const updateFlywheelData = async (chainId: SupportedChains, rpcUrl: string) => {
           if (error) {
             throw `Error occurred during saving data for flywheel's plugin ${pluginAddress}: ${error.message}`;
           }
-        } catch (err) {
-          await functionsAlert("Saving Flywheel's plugin", JSON.stringify(err));
+        } catch (exception) {
+          await functionsAlert(
+            `Functions.flywheel: Error occurred during saving data for flywheel ${flywheel}`,
+            JSON.stringify(exception)
+          );
         }
       }
     }
   } catch (err) {
-    await functionsAlert("Saving Flywheel's plugin", JSON.stringify(err));
+    await functionsAlert('Functions.flywheel: Generic Error', JSON.stringify(err));
   }
 };
 
