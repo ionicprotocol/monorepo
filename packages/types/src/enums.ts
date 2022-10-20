9e4dbeb19bcf01be1d299677899749029c6c1efd
carlomazzaferro <carlo.mazzaferro@gmail.com>
2022-10-20 13:46:14 +0200

    BNBx and JRM for Transfero

--- packages/types/src/enums.ts
+++ packages/types/src/enums.ts
@@ -51,6 +51,7 @@ export enum OracleTypes {
   DiaStDotPriceOracle = "DiaStDotPriceOracle",
   BalancerLpTokenPriceOracle = "BalancerLpTokenPriceOracle",
   StkBNBPriceOracle = "StkBNBPriceOracle",
+  BNBxPriceOracle = "BNBxPriceOracle",
 }
 
 export enum IrmTypes {
@@ -61,6 +62,7 @@ export enum IrmTypes {
   JumpRateModel_JARVIS_2_004_4_08 = "JumpRateModel_JARVIS_2_004_4_08",
   AdjustableJumpRateModel_PSTAKE_WBNB = "AdjustableJumpRateModel_PSTAKE_WBNB",
   AdjustableJumpRateModel_MIXBYTES_XCDOT = "AdjustableJumpRateModel_MIXBYTES_XCDOT",
+  AdjustableJumpRateModel_TRANSFERO_WBNB = "AdjustableJumpRateModel_TRANSFERO_WBNB",
 }
 
 export enum LiquidationStrategy {
@@ -199,6 +201,9 @@ export enum assetSymbols {
 
   "CAKE-WBNB" = "CAKE-WBNB",
   "stkBNB-WBNB" = "stkBNB-WBNB",
+  BNBx = "BNBx",
+  "epsBNBx-BNB" = "epsBNBx-BNB",
+  "asBNBx-WBNB" = "ApeSwap BNBx-WBNB LP",
 
   DDD = "DDD",
   EPX = "EPX",
