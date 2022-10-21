3e51adfd122a033e72bedc9eeb731cbd92dba71b
smartdevcom <smart.dev.cop@gmail.com>
2022-10-21 12:16:23 +1000

    reject, gas estimation

--- packages/ui/utils/errorHandling.ts
+++ packages/ui/utils/errorHandling.ts
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { CreateToastFnReturn } from '@chakra-ui/react';
 import LogRocket from 'logrocket';
 
-export const handleGenericError = (e: unknown, errorToast?: CreateToastFnReturn) => {
+export const handleGenericError = (e: any, errorToast?: CreateToastFnReturn) => {
   console.error(e);
+  console.error(JSON.stringify(e));
   let message: string;
 
   if (e instanceof Error) {
@@ -14,6 +16,18 @@ export const handleGenericError = (e: unknown, errorToast?: CreateToastFnReturn)
   }
 
   if (errorToast) {
-    errorToast({ description: message });
+    if (e.code === 'ACTION_REJECTED') {
+      errorToast({
+        title: 'Action Rejected!',
+        status: 'warning',
+        description: 'Your transaction has been rejected!',
+      });
+    } else if (e.method === 'estimateGas') {
+      errorToast({
+        description: 'Gas cannot be estimated!',
+      });
+    } else {
+      errorToast({ description: message });
+    }
   }
 };
