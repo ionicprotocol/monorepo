fc53240f762314067e2ab88d90d33f6864828fdc
pczek <839848+peetzweg@users.noreply.github.com>
2022-10-21 15:08:35 +0200

    update step labels

--- packages/ui/constants/index.ts
+++ packages/ui/constants/index.ts
@@ -97,4 +97,4 @@ export const ADMIN_FEE_TOOLTIP =
 export const MIDAS_LOCALSTORAGE_KEYS = 'midas_localstorage_keys';
 export const SHRINK_ASSETS = 10;
 export const MIDAS_T_AND_C_ACCEPTED = 'MidasTandCAccepted';
-export const SUPPLY_STEPS = ['Approve', 'Set Collateral', 'Mint'];
+export const SUPPLY_STEPS = ['Approve', 'Enter Market', 'Mint Market Share'];
