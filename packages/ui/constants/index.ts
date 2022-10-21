b73e6d92832ccd906b522dd2bee7eba83cb26622
pczek <839848+peetzweg@users.noreply.github.com>
2022-10-21 15:43:26 +0200

    change label for collateral factor to 'Loan-to-Value'

--- packages/ui/constants/index.ts
+++ packages/ui/constants/index.ts
@@ -93,7 +93,8 @@ export const COLLATERAL_FACTOR_TOOLTIP =
   'Collateral factor can range from 0-90%, and represents the proportionate increase in liquidity (borrow limit) that an account receives by depositing the asset.';
 export const ADMIN_FEE_TOOLTIP =
   "The fraction of interest generated on a given asset that is routed to the asset's admin address as a fee.";
-
+export const LOAN_TO_VALUE_TOOLTIP =
+  'The Loan to Value (LTV) ratio defines the maximum amount of tokens in the pool that can be borrowed with a specific collateral. It’s expressed in percentage: if in a pool ETH has 75% LTV, for every 1 ETH worth of collateral, borrowers will be able to borrow 0.75 ETH worth of other tokens in the pool.';
 export const MIDAS_LOCALSTORAGE_KEYS = 'midas_localstorage_keys';
 export const SHRINK_ASSETS = 10;
 export const MIDAS_T_AND_C_ACCEPTED = 'MidasTandCAccepted';
