979d982b317fa09d5828f3abce5fabdd2df3e0e5
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2022-11-03 22:34:06 +0200

    fixing the bsc deploy error

--- packages/sdk/deploy/deploy.ts
+++ packages/sdk/deploy/deploy.ts
@@ -517,12 +517,14 @@ const func: DeployFunction = async ({ run, ethers, getNamedAccounts, deployments
     console.log("setAddress DefaultProxyAdmin: ", tx.hash);
   }
 
-  const quoter = await ethers.getContract("Quoter");
-  const quoterAddress = await addressesProvider.callStatic.getAddress("Quoter");
-  if (quoterAddress !== quoter.address) {
-    tx = await addressesProvider.setAddress("Quoter", quoter.address);
-    await tx.wait();
-    console.log("setAddress Quoter: ", tx.hash);
+  const quoter = await ethers.getContractOrNull("Quoter");
+  if (quoter != null) {
+    const quoterAddress = await addressesProvider.callStatic.getAddress("Quoter");
+    if (quoterAddress !== quoter.address) {
+      tx = await addressesProvider.setAddress("Quoter", quoter.address);
+      await tx.wait();
+      console.log("setAddress Quoter: ", tx.hash);
+    }
   }
 
   await configureAddressesProviderStrategies({
