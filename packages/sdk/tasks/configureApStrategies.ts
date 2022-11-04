b14ef108e320a2a9dc734fb59b5bbdb9afa12cd4
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2022-11-04 15:27:12 +0200

    deploy to bsc artifacts

--- packages/sdk/tasks/configureApStrategies.ts
+++ packages/sdk/tasks/configureApStrategies.ts
@@ -2,7 +2,10 @@ import { task } from "hardhat/config";
 
 import { configureAddressesProviderStrategies } from "../chainDeploy/helpers/liquidators/fuseSafeLiquidator";
 
-export default task("config:strategies", "").setAction(async ({}, { ethers, getNamedAccounts, getChainId }) => {
+export default task(
+  "config:strategies",
+  "Configure the redemption and funding strategies in the AddressesProvider for testing purposes"
+).setAction(async ({}, { ethers, getNamedAccounts, getChainId }) => {
   const chainId = await getChainId();
   await configureAddressesProviderStrategies({
     ethers,
