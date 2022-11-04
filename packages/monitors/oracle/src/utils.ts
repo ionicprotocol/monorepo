953a89e69c7b4a9d5f1131a7d78c4baa9920ae70
smartdevcom <smart.dev.cop@gmail.com>
2022-11-04 12:00:53 +1000

    fall back to defiLlama api

--- packages/monitors/oracle/src/utils.ts
+++ packages/monitors/oracle/src/utils.ts
@@ -2,15 +2,31 @@ import { neondevnet } from "@midas-capital/chains";
 import axios from "axios";
 
 export const getCgPrice = async (coingeckoId: string) => {
-  // set neon price hardcoded as 0.05 for now
-  if (coingeckoId === neondevnet.specificParams.cgId) return 0.05;
+  let usdPrice = NaN;
 
-  let usdPrice: number;
+  try {
+    const { data } = await axios.get(
+      `https://api.coingecko.com/api/v3/simple/price?vs_currencies=usd&ids=${coingeckoId}`
+    );
 
-  usdPrice = (await axios.get(`https://api.coingecko.com/api/v3/simple/price?vs_currencies=usd&ids=${coingeckoId}`))
-    .data[coingeckoId].usd as number;
+    if (data[coingeckoId] && data[coingeckoId].usd) {
+      usdPrice = data[coingeckoId].usd;
+    }
+  } catch (e) {
+    const { data } = await axios.get(`https://coins.llama.fi/prices/current/coingecko:${coingeckoId}`);
 
-  usdPrice = usdPrice ? usdPrice : 1.0;
+    if (data.coins[`coingecko:${coingeckoId}`] && data.coins[`coingecko:${coingeckoId}`].price) {
+      usdPrice = data.coins[`coingecko:${coingeckoId}`].price;
+    }
+  }
 
-  return usdPrice;
+  if (usdPrice) {
+    return usdPrice;
+  } else {
+    if (coingeckoId === neondevnet.specificParams.cgId) {
+      return 0.05;
+    } else {
+      return 1;
+    }
+  }
 };
