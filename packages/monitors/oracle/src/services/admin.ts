c628c808b1271f0b44b25462b0b3506eaecb549f
carlomazzaferro <carlo.mazzaferro@gmail.com>
2022-11-16 09:19:18 +0100

    comptroller extension fixes

--- packages/monitors/oracle/src/services/admin.ts
+++ packages/monitors/oracle/src/services/admin.ts
@@ -1,4 +1,6 @@
 import { MidasSdk } from "@midas-capital/sdk";
+import { Comptroller } from "@midas-capital/sdk/dist/cjs/lib/contracts/typechain/Comptroller";
+import { ComptrollerFirstExtension } from "@midas-capital/sdk/dist/cjs/lib/contracts/typechain/ComptrollerFirstExtension";
 import { SupportedAsset } from "@midas-capital/types";
 import { constants, Contract, logger, Signer } from "ethers";
 
@@ -18,48 +20,49 @@ export class AdminService {
     return this;
   }
 
-  async pauseAllPools(pools: Array<Contract>) {
+  async pauseAllPools(pools: Array<Comptroller>) {
     for (const pool of pools) {
       const cTokenAddress = await pool.callStatic.cTokensByUnderlying(this.asset.underlying);
+      const poolExtension = this.sdk.createFirstComptrollerExtension(pool.address, this.sdk.signer);
       const cToken = this.sdk.createCToken(cTokenAddress, this.admin);
-      await this.pauseMarketActivity(pool, cToken);
+      await this.pauseMarketActivity(pool, poolExtension, cToken);
     }
   }
 
-  private async getPauseGuardian(pool: Contract) {
-    return await pool.pauseGuardian();
+  private async getPauseGuardian(pool: ComptrollerFirstExtension) {
+    return await pool.callStatic.pauseGuardian();
   }
-  private async setPauseGuardian(pool: Contract) {
+  private async setPauseGuardian(pool: ComptrollerFirstExtension) {
     const tx = await pool._setPauseGuardian(this.adminAddress);
     await tx.wait();
     logger.info(`Set the pause guardian to ${this.adminAddress}`);
   }
 
-  async pauseMarketActivity(pool: Contract, cToken: Contract) {
-    const pauseGuardian = await this.getPauseGuardian(pool);
+  async pauseMarketActivity(pool: Comptroller, extension: ComptrollerFirstExtension, cToken: Contract) {
+    const pauseGuardian = await this.getPauseGuardian(extension);
     if (pauseGuardian === constants.AddressZero) {
-      await this.setPauseGuardian(pool);
+      await this.setPauseGuardian(extension);
     }
-    await this.pauseMintActivity(pool, cToken);
-    await this.pauseBorrowActivity(pool);
+    await this.pauseMintActivity(pool, extension, cToken);
+    await this.pauseBorrowActivity(pool, extension);
   }
 
-  async pauseMintActivity(pool: Contract, cToken: Contract) {
-    const isPaused: boolean = await pool.mintGuardianPaused(cToken.address);
+  async pauseMintActivity(pool: Comptroller, extension: ComptrollerFirstExtension, cToken: Contract) {
+    const isPaused: boolean = await pool.callStatic.mintGuardianPaused(cToken.address);
     if (!isPaused) {
-      const tx = await pool._setMintPaused(cToken.address, true);
+      const tx = await extension._setMintPaused(cToken.address, true);
       await tx.wait();
       logger.warn(`Market mint pause tx ${tx.hash}`);
     } else {
       logger.info(`Minting already paused`);
     }
   }
-  async pauseBorrowActivity(pool: Contract) {
-    const markets = await pool.getAllMarkets();
+  async pauseBorrowActivity(pool: Comptroller, extension: ComptrollerFirstExtension) {
+    const markets = await pool.callStatic.getAllMarkets();
     for (const market of markets) {
-      const isPaused: boolean = await pool.borrowGuardianPaused(market);
+      const isPaused: boolean = await pool.callStatic.borrowGuardianPaused(market);
       if (!isPaused) {
-        const tx = await pool._setBorrowPaused(market, true);
+        const tx = await extension._setBorrowPaused(market, true);
         await tx.wait();
         logger.warn(`Market borrow pause tx ${tx.hash}`);
       } else {
