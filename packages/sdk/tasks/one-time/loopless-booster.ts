e8e92b3ceb166fc001876e8c3dadba60ef52577e
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2022-11-16 18:40:55 +0200

    loopless booster deploy artifacts; removed the AnkrBNBcPriceOracle artifacts

--- packages/sdk/tasks/one-time/loopless-booster.ts
+++ packages/sdk/tasks/one-time/loopless-booster.ts
@@ -9,7 +9,7 @@ task("loopless-booster", "deploy and a loopless booster for a flywheel")
   .setAction(async ({ flywheelAddress }, { ethers, deployments }) => {
     const deployer = await ethers.getNamedSigner("deployer");
 
-    if (flywheelAddress == "0x34f28ca9B62Af94Ae2F80a9fA2AffEF4D08cB18e") {
+    if (flywheelAddress == "0xUseThisToVerify") {
         const flywheel = (await ethers.getContractAt("MidasFlywheelCore", flywheelAddress, deployer)) as MidasFlywheelCore;
         const currentBoosterAddress = await flywheel.callStatic.flywheelBooster();
 
@@ -22,7 +22,7 @@ task("loopless-booster", "deploy and a loopless booster for a flywheel")
 
         const tx = await flywheel.setBooster(booster.address);
         await tx.wait();
-        console.log(`set the booster with tx ${tx.hash}`);
+        console.log(`set the booster at ${booster.address} to flywheel at ${flywheelAddress} with tx ${tx.hash}`);
       } else {
         throw new Error(`flywheel at ${flywheelAddress} already has a booster ${currentBoosterAddress}`);
       }
