e0a5236cb3b6c653ca4ccfbcec2cd3f8b9ee7c48
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2022-12-13 18:44:41 +0200

    prettier

--- packages/sdk/tasks/one-time/loopless-booster.ts
+++ packages/sdk/tasks/one-time/loopless-booster.ts
@@ -1,11 +1,10 @@
+import { constants } from "ethers";
 import { task, types } from "hardhat/config";
 
-import { MidasFlywheelCore } from "../../lib/contracts/typechain/MidasFlywheelCore";
-import { ComptrollerFirstExtension } from "../../lib/contracts/typechain/ComptrollerFirstExtension";
 import { Comptroller } from "../../lib/contracts/typechain/Comptroller";
+import { ComptrollerFirstExtension } from "../../lib/contracts/typechain/ComptrollerFirstExtension";
 import { FlywheelStaticRewards } from "../../lib/contracts/typechain/FlywheelStaticRewards";
-
-import { constants } from "ethers";
+import { MidasFlywheelCore } from "../../lib/contracts/typechain/MidasFlywheelCore";
 
 task("loopless-booster", "deploy and a loopless booster for a flywheel")
   .addParam("flywheelAddress", "Address of the flywheel to set the booster to", undefined, types.string)
@@ -40,125 +39,114 @@ task("loopless-booster", "deploy and a loopless booster for a flywheel")
     }
   });
 
-task("replace-flywheel-with-upgradable", "")
-  .setAction(async ({}, {ethers, deployments, getChainId}) => {
-    const poolAddress = "0xeB2D3A9D962d89b4A9a34ce2bF6a2650c938e185"; // stDOT Pool
-    const brokenFlywheelAddress = "0x0e7742b50a14Cbc879193f6b2E04EfcDCCC6BE86";
-    const fxcDOTMarketAddress = "0xa9736bA05de1213145F688e4619E5A7e0dcf4C72";
-    const fwstDOTMarketAddress = "0xb3D83F2CAb787adcB99d4c768f1Eb42c8734b563";
+task("replace-flywheel-with-upgradable", "").setAction(async ({}, { ethers, deployments, getChainId }) => {
+  const poolAddress = "0xeB2D3A9D962d89b4A9a34ce2bF6a2650c938e185"; // stDOT Pool
+  const brokenFlywheelAddress = "0x0e7742b50a14Cbc879193f6b2E04EfcDCCC6BE86";
+  const fxcDOTMarketAddress = "0xa9736bA05de1213145F688e4619E5A7e0dcf4C72";
+  const fwstDOTMarketAddress = "0xb3D83F2CAb787adcB99d4c768f1Eb42c8734b563";
 
-    const deployer = await ethers.getNamedSigner("deployer");
-    const chainid = await getChainId();
-    if (chainid == 1284) {
+  const deployer = await ethers.getNamedSigner("deployer");
+  const chainid = await getChainId();
+  if (chainid == 1284) {
+    const asComptrollerExtension = (await ethers.getContractAt(
+      "ComptrollerFirstExtension",
+      poolAddress,
+      deployer
+    )) as ComptrollerFirstExtension;
 
-      const asComptrollerExtension = (await ethers.getContractAt(
-          "ComptrollerFirstExtension",
-          poolAddress,
-          deployer)
-      ) as ComptrollerFirstExtension;
+    const asComptroller = (await ethers.getContractAt("Comptroller", poolAddress, deployer)) as Comptroller;
 
-      const asComptroller = (await ethers.getContractAt(
-          "Comptroller",
-          poolAddress,
-          deployer)
-      ) as Comptroller;
+    const brokenFlywheel = (await ethers.getContractAt(
+      "MidasFlywheelCore",
+      brokenFlywheelAddress,
+      deployer
+    )) as MidasFlywheelCore;
 
-      const brokenFlywheel = (await ethers.getContractAt(
-        "MidasFlywheelCore",
-        brokenFlywheelAddress,
-        deployer
-      )) as MidasFlywheelCore;
+    const rewardToken = await brokenFlywheel.callStatic.rewardToken();
+    const flywheelBooster = await brokenFlywheel.callStatic.flywheelBooster();
+    const oldStaticRewardsAddress = await brokenFlywheel.flywheelRewards();
 
-      const rewardToken = await brokenFlywheel.callStatic.rewardToken();
-      const flywheelBooster = await brokenFlywheel.callStatic.flywheelBooster();
-      const oldStaticRewardsAddress = await brokenFlywheel.flywheelRewards();
+    const oldStaticRewards = (await ethers.getContractAt(
+      "FlywheelStaticRewards",
+      oldStaticRewardsAddress,
+      deployer
+    )) as FlywheelStaticRewards;
 
-      const oldStaticRewards = (await ethers.getContractAt(
-          "FlywheelStaticRewards",
-          oldStaticRewardsAddress,
-          deployer)
-      ) as FlywheelStaticRewards;
+    await oldStaticRewards.setRewardsInfo(fxcDOTMarketAddress, {
+      rewardsPerSecond: 0,
+      rewardsEndTimestamp: 1,
+    });
 
-      await oldStaticRewards.setRewardsInfo(fxcDOTMarketAddress, {
-        rewardsPerSecond: 0,
-        rewardsEndTimestamp: 1,
-      });
+    await oldStaticRewards.setRewardsInfo(fwstDOTMarketAddress, {
+      rewardsPerSecond: 0,
+      rewardsEndTimestamp: 1,
+    });
 
-      await oldStaticRewards.setRewardsInfo(fwstDOTMarketAddress, {
-        rewardsPerSecond: 0,
-        rewardsEndTimestamp: 1,
-      });
-
-      const replacingFlywheel = await deployments.deploy("MidasFlywheel",  {
-        contract: "MidasFlywheel",
-        from: deployer.address,
-        log: true,
-        proxy: {
-          proxyContract: "OpenZeppelinTransparentProxy",
-          execute: {
-            init: {
-              methodName: "initialize",
-              args: [
-                rewardToken,
-                constants.AddressZero,
-                flywheelBooster,
-                deployer.address,
-              ],
-            },
+    const replacingFlywheel = await deployments.deploy("MidasFlywheel", {
+      contract: "MidasFlywheel",
+      from: deployer.address,
+      log: true,
+      proxy: {
+        proxyContract: "OpenZeppelinTransparentProxy",
+        execute: {
+          init: {
+            methodName: "initialize",
+            args: [rewardToken, constants.AddressZero, flywheelBooster, deployer.address],
           },
-          owner: deployer.address,
         },
-      });
+        owner: deployer.address,
+      },
+    });
 
-      // the replacing rewards contract is needed because on creation it makes an infinite approve to the flywheel
-      const replacingRewards = await deployments.deploy("FlywheelStaticRewards", {
-        contract: "FlywheelStaticRewards",
-        from: deployer.address,
-        log: true,
-        args: [
-          replacingFlywheel.address, // flywheel
-          deployer.address, // owner
-          constants.AddressZero, // Authority
-        ],
-      });
+    // the replacing rewards contract is needed because on creation it makes an infinite approve to the flywheel
+    const replacingRewards = await deployments.deploy("FlywheelStaticRewards", {
+      contract: "FlywheelStaticRewards",
+      from: deployer.address,
+      log: true,
+      args: [
+        replacingFlywheel.address, // flywheel
+        deployer.address, // owner
+        constants.AddressZero, // Authority
+      ],
+    });
 
-      // this will transfer all the rewards from the old FlywheelStaticRewards contract
-      // which has an infinite approve only to the old flywheel
-      let tx = await brokenFlywheel.setFlywheelRewards(replacingRewards.address);
-      await tx.wait();
-      console.log("setFlywheelRewards: ", tx.hash);
+    // this will transfer all the rewards from the old FlywheelStaticRewards contract
+    // which has an infinite approve only to the old flywheel
+    let tx = await brokenFlywheel.setFlywheelRewards(replacingRewards.address);
+    await tx.wait();
+    console.log("setFlywheelRewards: ", tx.hash);
 
-      // probably it is better to remove the flywheel because it cannot pull rewards from the new FlywheelStaticRewards
-      // so whatever rewards were accrued, will be reset and cannot be claimed
-      tx = await asComptrollerExtension.addNonAccruingFlywheel(brokenFlywheelAddress);
-      await tx.wait();
-      console.log("addNonAccruingFlywheel: ", tx.hash);
+    // probably it is better to remove the flywheel because it cannot pull rewards from the new FlywheelStaticRewards
+    // so whatever rewards were accrued, will be reset and cannot be claimed
+    tx = await asComptrollerExtension.addNonAccruingFlywheel(brokenFlywheelAddress);
+    await tx.wait();
+    console.log("addNonAccruingFlywheel: ", tx.hash);
 
-      // adding the new flywheel to the pool
-      tx = await asComptroller._addRewardsDistributor(replacingFlywheel.address);
-      await tx.wait();
-      console.log("_addRewardsDistributor: ", tx.hash);
+    // adding the new flywheel to the pool
+    tx = await asComptroller._addRewardsDistributor(replacingFlywheel.address);
+    await tx.wait();
+    console.log("_addRewardsDistributor: ", tx.hash);
 
-      // configuring the same two strategies/markets
-      const newFlywheel = (await ethers.getContractAt(
-        "MidasFlywheelCore",
-        replacingFlywheel.address,
-        deployer
-      )) as MidasFlywheelCore;
+    // configuring the same two strategies/markets
+    const newFlywheel = (await ethers.getContractAt(
+      "MidasFlywheelCore",
+      replacingFlywheel.address,
+      deployer
+    )) as MidasFlywheelCore;
 
-      // the flywheel was initialized with address(0) for the rewards, so set it up
-      tx = await newFlywheel.setFlywheelRewards(replacingRewards.address);
-      await tx.wait();
-      console.log("setFlywheelRewards: ", tx.hash);
+    // the flywheel was initialized with address(0) for the rewards, so set it up
+    tx = await newFlywheel.setFlywheelRewards(replacingRewards.address);
+    await tx.wait();
+    console.log("setFlywheelRewards: ", tx.hash);
 
-      tx = await newFlywheel.addStrategyForRewards(fxcDOTMarketAddress);
-      await tx.wait();
-      console.log("addStrategyForRewards fxcDOT: ", tx.hash);
+    tx = await newFlywheel.addStrategyForRewards(fxcDOTMarketAddress);
+    await tx.wait();
+    console.log("addStrategyForRewards fxcDOT: ", tx.hash);
 
-      tx = await newFlywheel.addStrategyForRewards(fwstDOTMarketAddress);
-      await tx.wait();
-      console.log("addStrategyForRewards fwstDOT: ", tx.hash);
-    } else {
-      console.log(`wrong chain`);
-    }
-  });
+    tx = await newFlywheel.addStrategyForRewards(fwstDOTMarketAddress);
+    await tx.wait();
+    console.log("addStrategyForRewards fwstDOT: ", tx.hash);
+  } else {
+    console.log(`wrong chain`);
+  }
+});
