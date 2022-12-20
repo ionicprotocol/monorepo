34f2916b81d9148648cb4bf782f712f44ef47834
pczek <839848+peetzweg@users.noreply.github.com>
2022-12-20 15:41:47 +0100

    makes named export as it was used every now and than.

--- packages/sdk/src/Artifacts.ts
+++ packages/sdk/src/Artifacts.ts
@@ -30,7 +30,7 @@ const AdjustableJumpRateModel_JARVIS_jEUR: Artifact = AdjustableJumpRateModel;
 const JumpRateModel_MIMO_002_004_4_08: Artifact = JumpRateModel;
 const JumpRateModel_JARVIS_002_004_4_08: Artifact = JumpRateModel;
 
-const ARTIFACTS: Record<IrmTypes | OracleTypes | string, Artifact> = {
+export const ARTIFACTS: Record<IrmTypes | OracleTypes | string, Artifact> = {
   AdjustableJumpRateModel_JARVIS_jBRL,
   AdjustableJumpRateModel_JARVIS_jEUR,
   AdjustableJumpRateModel_MIXBYTES_USDC,
