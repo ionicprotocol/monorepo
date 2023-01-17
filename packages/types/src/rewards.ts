269a2557f14c64f1e5b8f4e51f7d661364b808e7
smartdevcom <smart.dev.cop@gmail.com>
2023-01-17 21:22:34 +1000

    fixed type

--- packages/types/src/rewards.ts
+++ packages/types/src/rewards.ts
@@ -1,6 +1,6 @@
 export interface AbstractReward {
   apy?: number;
-  status?: "active" | "eol" | "paused" | "unknown";
+  status: "active" | "eol" | "paused" | "unknown";
   updated_at: string;
 }
 
