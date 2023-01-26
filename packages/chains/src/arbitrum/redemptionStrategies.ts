b4cf830b17869fc2c29940b0cf396e85faa22b01
carlomazzaferro <carlo.mazzaferro@gmail.com>
2023-01-26 14:22:59 +0100

    redemption strategies for arbi and ftm

--- packages/chains/src/arbitrum/redemptionStrategies.ts
+++ packages/chains/src/arbitrum/redemptionStrategies.ts
@@ -1,7 +1,11 @@
 import { assetSymbols, RedemptionStrategyContract, underlying } from "@midas-capital/types";
 
-import { assets } from "./assets";
+import { assets, USDC } from "./assets";
 
-const redemptionStrategies: { [token: string]: [RedemptionStrategyContract, string] } = {};
+const redemptionStrategies: { [token: string]: [RedemptionStrategyContract, string] } = {
+  [underlying(assets, assetSymbols.saddleFraxBP)]: [RedemptionStrategyContract.SaddleLpTokenLiquidator, USDC],
+  [underlying(assets, assetSymbols.saddleFraxUsdsBP)]: [RedemptionStrategyContract.SaddleLpTokenLiquidator, USDC],
+  [underlying(assets, assetSymbols.saddleFraxUsdtBP)]: [RedemptionStrategyContract.SaddleLpTokenLiquidator, USDC],
+};
 
 export default redemptionStrategies;
