a205b1e5a1ea34721108cf08b93fe5f152697772
carlomazzaferro <carlo.mazzaferro@gmail.com>
2023-01-26 14:33:57 +0100

    fix redemption strat for saddle arbi

--- packages/chains/src/arbitrum/redemptionStrategies.ts
+++ packages/chains/src/arbitrum/redemptionStrategies.ts
@@ -4,8 +4,14 @@ import { assets, USDC } from "./assets";
 
 const redemptionStrategies: { [token: string]: [RedemptionStrategyContract, string] } = {
   [underlying(assets, assetSymbols.saddleFraxBP)]: [RedemptionStrategyContract.SaddleLpTokenLiquidator, USDC],
-  [underlying(assets, assetSymbols.saddleFraxUsdsBP)]: [RedemptionStrategyContract.SaddleLpTokenLiquidator, USDC],
-  [underlying(assets, assetSymbols.saddleFraxUsdtBP)]: [RedemptionStrategyContract.SaddleLpTokenLiquidator, USDC],
+  [underlying(assets, assetSymbols.saddleFraxUsdsBP)]: [
+    RedemptionStrategyContract.SaddleLpTokenLiquidator,
+    underlying(assets, assetSymbols.saddleFraxBP),
+  ],
+  [underlying(assets, assetSymbols.saddleFraxUsdtBP)]: [
+    RedemptionStrategyContract.SaddleLpTokenLiquidator,
+    underlying(assets, assetSymbols.saddleFraxBP),
+  ],
 };
 
 export default redemptionStrategies;
