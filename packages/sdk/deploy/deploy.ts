53bb7497b603d112cbd7c02a85c9f7551ed95848
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2023-01-27 13:15:49 +0200

    deploy ownable spo

--- packages/sdk/deploy/deploy.ts
+++ packages/sdk/deploy/deploy.ts
@@ -435,6 +435,16 @@ const func: DeployFunction = async ({ run, ethers, getNamedAccounts, deployments
     from: deployer,
     args: [],
     log: true,
+    proxy: {
+      execute: {
+        init: {
+          methodName: "initialize",
+          args: [],
+        },
+      },
+      proxyContract: "OpenZeppelinTransparentProxy",
+      owner: deployer,
+    },
     waitConfirmations: 1,
   });
   if (simplePO.transactionHash) await ethers.provider.waitForTransaction(simplePO.transactionHash);
