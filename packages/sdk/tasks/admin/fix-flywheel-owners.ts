5e592326220376901df72cb620cce087a42e2b66
carlomazzaferro <carlo.mazzaferro@gmail.com>
2023-02-01 21:31:17 +0100

    add task for replacing strategy fee recipients

--- packages/sdk/tasks/admin/fix-flywheel-owners.ts
+++ packages/sdk/tasks/admin/fix-flywheel-owners.ts
@@ -1,6 +1,7 @@
 import { providers } from "ethers";
 import { task, types } from "hardhat/config";
 
+import { MidasERC4626 } from "../../typechain/MidasERC4626";
 import { MidasFlywheelCore } from "../../typechain/MidasFlywheelCore";
 
 task("deploy-fwr", "Changes the system admin to a new address")
@@ -32,9 +33,9 @@ task("deploy-fwr", "Changes the system admin to a new address")
     await tx.wait();
   });
 
-export default task("replace-fee-recipient", "Changes the system admin to a new address")
+task("replace-fw-fee-recipient", "Changes the system admin to a new address")
   .addParam("newDeployer", "The address of the new deployer", undefined, types.string)
-  .addParam("fwAddresses", "The addresses of the fw for which to update the fees", undefined, types.string)
+  .addParam("strategies", "The addresses of the strategies for which to update the fees", undefined, types.string)
   .setAction(async ({ newDeployer, fwAddresses }, { ethers }) => {
     let tx: providers.TransactionResponse;
 
@@ -59,6 +60,35 @@ export default task("replace-fee-recipient", "Changes the system admin to a new 
       } else {
         console.log(`fee recipient needs to be replaced from ${feeRecipient} to ${newDeployer}}`);
         tx = await flywheelCore.updateFeeSettings(performanceFee, newDeployer);
+        await tx.wait();
+        console.log("fee recipient updated to the new deployer");
+      }
+    }
+  });
+
+export default task("replace-plugin-fee-recipient", "Changes the system admin to a new address")
+  .addParam("newDeployer", "The address of the new deployer", undefined, types.string)
+  .addParam("strategies", "The addresses of the fw for which to update the fees", undefined, types.string)
+  .setAction(async ({ newDeployer, strategies }, { ethers }) => {
+    let tx: providers.TransactionResponse;
+
+    const deployer = await ethers.getSigner(newDeployer);
+    console.log("current deployer", deployer.address);
+
+    const strategyAddresses = strategies.split(",");
+    for (const strategyAddress of strategyAddresses) {
+      const strategy = (await ethers.getContractAt("MidasERC4626", strategyAddress, deployer)) as MidasERC4626;
+
+      const currentOwner = await strategy.callStatic.owner();
+      console.log(`current owner ${currentOwner} of the strategy at ${strategy.address}`);
+      const feeRecipient = await strategy.callStatic.feeRecipient();
+      const performanceFee = await strategy.callStatic.performanceFee();
+      if (feeRecipient === newDeployer) {
+        console.log("fee recipient already set to the new deployer");
+        continue;
+      } else {
+        console.log(`fee recipient needs to be replaced from ${feeRecipient} to ${newDeployer}}`);
+        tx = await strategy.updateFeeSettings(performanceFee, newDeployer);
         await tx.wait();
         console.log("fee recipient updated to the new deployer");
       }
