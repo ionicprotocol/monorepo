715b1a64cf5d22b0e94899846b34396619ae29b0
carlomazzaferro <carlo.mazzaferro@gmail.com>
2023-02-01 21:18:25 +0100

    redeployed some flywheels

--- packages/sdk/tasks/admin/fix-flywheel-owners.ts
+++ packages/sdk/tasks/admin/fix-flywheel-owners.ts
@@ -1,69 +1,66 @@
 import { providers } from "ethers";
 import { task, types } from "hardhat/config";
 
-import { Auth } from "../../typechain/Auth";
-import { CErc20PluginDelegate } from "../../typechain/CErc20PluginDelegate";
-import { ComptrollerFirstExtension } from "../../typechain/ComptrollerFirstExtension";
-import { FusePoolDirectory } from "../../typechain/FusePoolDirectory";
-import { MidasERC4626 } from "../../typechain/MidasERC4626";
 import { MidasFlywheelCore } from "../../typechain/MidasFlywheelCore";
-import { ReplacingFlywheelDynamicRewards } from "../../typechain/ReplacingFlywheelDynamicRewards";
-import { ReplacingFlywheelStaticRewards } from "../../typechain/ReplacingFlywheelStaticRewards";
-import { Unitroller } from "../../typechain/Unitroller";
 
-export default task("replace-fwr", "Changes the system admin to a new address")
-  .addParam("currentDeployer", "The address of the current deployer", undefined, types.string)
+task("deploy-fwr", "Changes the system admin to a new address")
+  .addParam("signer", "The address of the current deployer", "deployer", types.string)
+  .addParam("flywheel", "The address of the new deployer", undefined, types.string)
+  .addParam("name", "Name of the FW rewarsd", undefined, types.string)
+  .setAction(async ({ signer, flywheel, name }, { ethers, deployments }) => {
+    const deployer = await ethers.getNamedSigner(signer);
+
+    console.log("current deployer", deployer.address);
+    console.log("flywheel", flywheel);
+    console.log("name", name);
+    const rewards = await deployments.deploy(`FlywheelStaticRewards_${name}`, {
+      contract: "FlywheelStaticRewards",
+      from: deployer.address,
+      log: true,
+      args: [
+        flywheel, // flywheel
+        deployer.address, // owner
+        ethers.constants.AddressZero, // Authority
+      ],
+      waitConfirmations: 1,
+    });
+    // @ts-ignore
+    const midasSdkModule = await import("../../tests/utils/midasSdk");
+    const sdk = await midasSdkModule.getOrCreateMidas(deployer);
+
+    const tx = await sdk.setFlywheelRewards(flywheel, rewards.address);
+    await tx.wait();
+  });
+
+export default task("replace-fee-recipient", "Changes the system admin to a new address")
   .addParam("newDeployer", "The address of the new deployer", undefined, types.string)
-  .setAction(async ({ currentDeployer, newDeployer }, { ethers }) => {
+  .addParam("fwAddresses", "The addresses of the fw for which to update the fees", undefined, types.string)
+  .setAction(async ({ newDeployer, fwAddresses }, { ethers }) => {
     let tx: providers.TransactionResponse;
 
-    const deployer = await ethers.getSigner(currentDeployer);
+    const deployer = await ethers.getSigner(newDeployer);
     console.log("current deployer", deployer.address);
 
-    const fusePoolDirectory = (await ethers.getContract("FusePoolDirectory", deployer)) as FusePoolDirectory;
-    const [, pools] = await fusePoolDirectory.callStatic.getActivePools();
-    for (let i = 0; i < pools.length; i++) {
-      const pool = pools[i];
-      console.log("pool name", pool.name);
-      const unitroller = (await ethers.getContractAt("Unitroller", pool.comptroller, deployer)) as Unitroller;
-      const admin = await unitroller.callStatic.admin();
-      console.log("pool admin", admin);
-      console.log("pool comptroller", pool.comptroller);
-
-      const comptrollerAsExtension = (await ethers.getContractAt(
-        "ComptrollerFirstExtension",
-        pool.comptroller,
+    const flywheelAddresses = fwAddresses.split(",");
+    for (const flywheelAddress of flywheelAddresses) {
+      const flywheelCore = (await ethers.getContractAt(
+        "MidasFlywheelCore",
+        flywheelAddress,
         deployer
-      )) as ComptrollerFirstExtension;
-      const flywheels = await comptrollerAsExtension.callStatic.getRewardsDistributors();
-      for (let k = 0; k < flywheels.length; k++) {
-        const flywheelAddress = flywheels[k];
-        {
-          const flywheelCore = (await ethers.getContractAt(
-            "MidasFlywheelCore",
-            flywheelAddress,
-            deployer
-          )) as MidasFlywheelCore;
+      )) as MidasFlywheelCore;
 
-          const currentOwner = await flywheelCore.callStatic.owner();
-          console.log(`current owner ${currentOwner} of the flywheel at ${flywheelCore.address}`);
-          console.log("flywheel rewards address: ", await flywheelCore.callStatic.flywheelRewards());
-          const rewards = (await ethers.getContractAt(
-            "ReplacingFlywheelDynamicRewards",
-            await flywheelCore.callStatic.flywheelRewards(),
-            deployer
-          )) as ReplacingFlywheelDynamicRewards;
-
-          console.log(rewards.address);
-          console.log(`rewards owner: ${await rewards.callStatic.owner()}`);
-          const rewardsOwner = await rewards.callStatic.owner();
-          if (rewardsOwner == currentDeployer) {
-            console.log(`setting rewards owner to ${newDeployer}`);
-            tx = await rewards.setAuthority(newDeployer);
-            await tx.wait();
-            console.log(`rewards.transferOwnership tx mined ${tx.hash}`);
-          }
-        }
+      const currentOwner = await flywheelCore.callStatic.owner();
+      console.log(`current owner ${currentOwner} of the flywheel at ${flywheelCore.address}`);
+      console.log("flywheel rewards address: ", await flywheelCore.callStatic.flywheelRewards());
+      const feeRecipient = await flywheelCore.callStatic.feeRecipient();
+      const performanceFee = await flywheelCore.callStatic.performanceFee();
+      if (feeRecipient === newDeployer) {
+        console.log("fee recipient already set to the new deployer");
+        continue;
+      } else {
+        tx = await flywheelCore.updateFeeSettings(performanceFee, newDeployer);
+        await tx.wait();
+        console.log("fee recipient updated to the new deployer");
       }
     }
   });
