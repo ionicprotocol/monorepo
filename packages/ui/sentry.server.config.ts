6d0846794b4ba3eb5507c7ec5f5fa127e76c07dd
pczek <839848+peetzweg@users.noreply.github.com>
2023-02-10 16:54:16 +0100

    don't init at all if dsn if sentry is disabled.

--- packages/ui/sentry.server.config.ts
+++ packages/ui/sentry.server.config.ts
@@ -1,8 +1,10 @@
 import * as Sentry from '@sentry/nextjs';
 
-const SENTRY_DSN = process.env.IS_SENTRY_ENABLED === 'true' ? process.env.SENTRY_DSN : '';
+const SENTRY_DSN = process.env.IS_SENTRY_ENABLED === 'true' ? process.env.SENTRY_DSN : undefined;
 
-Sentry.init({
-  dsn: SENTRY_DSN,
-  tracesSampleRate: 1.0,
-});
+if (SENTRY_DSN) {
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    tracesSampleRate: 1.0,
+  });
+}
