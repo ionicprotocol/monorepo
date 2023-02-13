665b876743cf04452d90c647bba81965132ff839
pczek <839848+peetzweg@users.noreply.github.com>
2023-02-13 10:12:58 +0100

    set environment for sentry logs

--- packages/ui/sentry.edge.config.ts
+++ packages/ui/sentry.edge.config.ts
@@ -5,6 +5,7 @@ const SENTRY_DSN = process.env.IS_SENTRY_ENABLED === 'true' ? process.env.SENTRY
 if (SENTRY_DSN) {
   Sentry.init({
     dsn: SENTRY_DSN,
+    environment: process.env.NODE_ENV,
     tracesSampleRate: 1.0,
   });
 }
