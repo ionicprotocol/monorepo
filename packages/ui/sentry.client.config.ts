e28b61572a66e248b256cf56d7788d67d221ae88
smartdevcom <smart.dev.cop@gmail.com>
2023-03-02 19:14:47 +1000

    console test

--- packages/ui/sentry.client.config.ts
+++ packages/ui/sentry.client.config.ts
@@ -1,7 +1,16 @@
 import * as Sentry from '@sentry/nextjs';
 
+import { config } from '@ui/config/index';
+
 const SENTRY_DSN = process.env.IS_SENTRY_ENABLED === 'true' ? process.env.SENTRY_DSN : undefined;
-console.warn('client; ', process.env.IS_SENTRY_ENABLED, process.env.SENTRY_DSN, SENTRY_DSN);
+console.warn(config.supabasePublicKey);
+console.warn(
+  'client; ',
+  process.env.SUPABASE_KEY,
+  process.env.IS_SENTRY_ENABLED,
+  process.env.SENTRY_DSN,
+  SENTRY_DSN
+);
 if (SENTRY_DSN) {
   Sentry.init({
     dsn: SENTRY_DSN,
