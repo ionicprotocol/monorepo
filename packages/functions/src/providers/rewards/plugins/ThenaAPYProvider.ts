fc6e87e3d418aaf38884498ad1bb5e008921b5e9
smartdevcom <smart.dev.cop@gmail.com>
2023-04-24 19:37:36 +1000

    fixed apy for thena

--- packages/functions/src/providers/rewards/plugins/ThenaAPYProvider.ts
+++ packages/functions/src/providers/rewards/plugins/ThenaAPYProvider.ts
@@ -1,61 +1,80 @@
 import { Reward, Strategy, ThenaERC4626Plugin } from '@midas-capital/types';
-import axios from 'axios';
 import { functionsAlert } from '../../../alert';
-import { AbstractPluginAPYProvider } from './AbstractPluginAPYProvider';
-import { z } from 'zod';
+import { AbstractPluginAPYProvider, APYProviderInitObject } from './AbstractPluginAPYProvider';
+import { Contract, utils } from 'ethers';
+import { chainIdToConfig } from '@midas-capital/chains';
+import { SupportedChains } from '@midas-capital/types';
+import { MidasSdk } from '@midas-capital/sdk';
+import { JsonRpcProvider } from '@ethersproject/providers';
+import { assetSymbols } from '@midas-capital/types';
 
-const ThenaAPYData = z.object({
-  address: z.string(),
-  gauge: z.object({
-    projectedApr: z.number(),
-  }),
-});
-const ThenaAPYResponse = z.array(ThenaAPYData);
-type ThenaAPYResponse = z.infer<typeof ThenaAPYResponse>;
-
-interface ThenaAPYs {
-  [key: string]: number;
-}
+const VOTER_V3_ADDRESS = '0x3A1D0952809F4948d15EBCe8d345962A282C4fCb';
+const SECONDS_IN_A_DAY = 86400;
+const DAYS_IN_A_YEAR = 365;
 
 class ThenaAPYProvider extends AbstractPluginAPYProvider {
-  static apyEndpoint = 'https://api.thena.fi/api/v1/pools';
-  private thenaAPYs: ThenaAPYs | undefined;
+  private provider: APYProviderInitObject['provider'] | undefined;
+  private chainId: SupportedChains | undefined;
 
-  async init() {
-    const thenaResponse = await axios
-      .get(ThenaAPYProvider.apyEndpoint)
-      .then((response) => response.data)
-      .then((data) => ThenaAPYResponse.parse(data.data));
-
-    this.thenaAPYs = thenaResponse.reduce((acc, cur) => {
-      return {
-        ...acc,
-        [cur.address.toLowerCase()]: cur.gauge.projectedApr,
-      };
-    }, {});
-
-    if (!this.thenaAPYs) {
-      throw `ThenaAPYProvider: unable to format Thena APY data as expected`;
-    }
+  async init(initObj: APYProviderInitObject) {
+    this.provider = initObj.provider;
+    this.chainId = initObj.chainId;
   }
 
   async getApy(pluginAddress: string, pluginData: ThenaERC4626Plugin): Promise<Reward[]> {
     if (pluginData.strategy != Strategy.ThenaERC4626)
       throw `ThenaAPYProvider: Not a Thena Plugin ${pluginAddress}`;
 
-    if (this.thenaAPYs === undefined) {
-      throw 'ThenaAPYProvider: Not initialized';
+    if (this.chainId === undefined) {
+      throw 'ThenaAPYProvider: ChainId Not initialized';
     }
 
-    const apy = this.thenaAPYs[pluginData.underlying.toLowerCase()];
+    const voterV3Contract = new Contract(
+      VOTER_V3_ADDRESS,
+      ['function gauges(address pool) view returns (address)'],
+      this.provider
+    );
+    const gaugeAddress = await voterV3Contract.callStatic.gauges(pluginData.underlying);
 
-    if (apy === undefined) {
-      await functionsAlert(
-        `ThenaAPYProvider: ${pluginData.underlying}`,
-        `Unable to find APY data for underlying ${pluginData.underlying}`
-      );
-      throw `Thena underlying: "${pluginData.underlying}" not included in Thena APY data`;
+    const gaugeV2Contract = new Contract(
+      gaugeAddress,
+      [
+        'function rewardRate() external view returns (uint256)',
+        'function totalSupply() external view returns (uint256)',
+      ],
+      this.provider
+    );
+    const [rewardRateBig, totalSupplyBig] = await Promise.all([
+      gaugeV2Contract.callStatic.rewardRate(),
+      gaugeV2Contract.callStatic.totalSupply(),
+    ]);
+    const rewardRate = Number(utils.formatUnits(rewardRateBig));
+    const totalSupply = Number(utils.formatUnits(totalSupplyBig));
+
+    const config = chainIdToConfig[this.chainId];
+    const theAsset = config.assets.find((asset) => asset.symbol === assetSymbols.THE);
+
+    if (!theAsset) {
+      throw 'ThenaAPYProvider: THE asset is not defined in supported assets list';
     }
+
+    const sdk = new MidasSdk(
+      new JsonRpcProvider(config.specificParams.metadata.rpcUrls.default.http[0]),
+      config
+    );
+    const mpo = sdk.createMasterPriceOracle();
+    const [theUsdPriceBig, lpTokenUsdPriceBig] = await Promise.all([
+      mpo.callStatic.price(theAsset.underlying),
+      mpo.callStatic.price(pluginData.underlying),
+    ]);
+
+    const theUsdPrice = Number(utils.formatUnits(theUsdPriceBig));
+    const lpTokenUsdPrice = Number(utils.formatUnits(lpTokenUsdPriceBig));
+
+    const lpVaule = lpTokenUsdPrice * totalSupply;
+    const totTheInYear = rewardRate * SECONDS_IN_A_DAY * DAYS_IN_A_YEAR;
+    const totUsdDistributedInYear = totTheInYear * theUsdPrice;
+    const apy = totUsdDistributedInYear / lpVaule;
 
     if (apy === 0) {
       await functionsAlert(`ThenaAPYProvider: ${pluginAddress}`, 'APY of Plugin is 0');
@@ -63,7 +82,7 @@ class ThenaAPYProvider extends AbstractPluginAPYProvider {
 
     return [
       {
-        apy: apy / 100,
+        apy: apy,
         plugin: pluginAddress,
         updated_at: new Date().toISOString(),
       },
