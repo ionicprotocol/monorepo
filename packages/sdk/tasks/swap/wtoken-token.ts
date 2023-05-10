d3ef248947995cdfcd41c53486c6b5164d119aa7
smartdevcom <smart.dev.cop@gmail.com>
2023-05-10 19:07:18 +1000

    reset `hardhat-ethers` version

--- packages/sdk/tasks/swap/wtoken-token.ts
+++ packages/sdk/tasks/swap/wtoken-token.ts
@@ -1,4 +1,4 @@
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { BigNumber } from "ethers";
 import { task, types } from "hardhat/config";
 
