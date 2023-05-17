598709649f9620604fe920f80ef6a2e2a323b893
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2023-05-17 16:26:38 +0300

    new liquidators registry

--- packages/sdk/deploy/deploy.ts
+++ packages/sdk/deploy/deploy.ts
@@ -12,6 +12,7 @@ import {
 import { AddressesProvider } from "../typechain/AddressesProvider";
 import { FuseFeeDistributor } from "../typechain/FuseFeeDistributor";
 import { LiquidatorsRegistry } from "../typechain/LiquidatorsRegistry";
+import { LeveredPositionFactory } from "../typechain/LeveredPositionFactory";
 
 const func: DeployFunction = async ({ run, ethers, getNamedAccounts, deployments, getChainId }): Promise<void> => {
   console.log("RPC URL: ", ethers.provider.connection.url);
@@ -630,6 +631,14 @@ const func: DeployFunction = async ({ run, ethers, getNamedAccounts, deployments
   });
   if (lpfDep.transactionHash) await ethers.provider.waitForTransaction(lpfDep.transactionHash);
   console.log("LeveredPositionFactory: ", lpfDep.address);
+
+  const leveredPositionFactory = (await ethers.getContract("LeveredPositionFactory", deployer)) as LeveredPositionFactory;
+  const currentLiquidatorsRegistry = await leveredPositionFactory.callStatic.liquidatorsRegistry();
+  if (currentLiquidatorsRegistry.toLowerCase() != liquidatorsRegistry.address.toLowerCase()) {
+    tx = await leveredPositionFactory.setLiquidatorsRegistry(liquidatorsRegistry.address);
+    await tx.wait();
+    console.log("updated the liquidators registry: ", tx.hash);
+  }
   ////
 
   /// EXTERNAL ADDRESSES
