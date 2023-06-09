4ed7ca9b5e1e71da0616b69c0a01cb6142f3f78f
smartdevcom <smart.dev.cop@gmail.com>
2023-06-09 21:42:10 +1000

    set empty string

--- packages/sdk/src/modules/FusePools.ts
+++ packages/sdk/src/modules/FusePools.ts
@@ -89,10 +89,10 @@ export function withFusePools<TBase extends MidasBaseConstructor>(Base: TBase) {
         );
 
         if (_asset) {
-          asset.underlyingSymbol = _asset.symbol;
+          asset.underlyingSymbol = _asset.symbol ?? "";
           asset.logoUrl = _asset.symbol
             ? "https://d1912tcoux65lj.cloudfront.net/token/96x96/" + _asset.symbol.toLowerCase() + ".png"
-            : undefined;
+            : "";
           asset.originalSymbol = _asset.originalSymbol ? _asset.originalSymbol : undefined;
         }
 
