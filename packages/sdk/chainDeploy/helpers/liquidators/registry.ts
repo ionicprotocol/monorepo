d705f4e7de1134957906a71295e676bb9555f60e
carlomazzaferro <carlo.mazzaferro@gmail.com>
2023-07-05 08:15:40 +0200

    pair strategies match

--- packages/sdk/chainDeploy/helpers/liquidators/registry.ts
+++ packages/sdk/chainDeploy/helpers/liquidators/registry.ts
@@ -17,24 +17,10 @@ export const configureLiquidatorsRegistry = async ({
     deployer
   )) as ILiquidatorsRegistry;
 
-  let currentStrategies: string[] = [];
-  let currentInputTokens: string[] = [];
-  let currentOutputTokens: string[] = [];
-
   const strategies: string[] = [];
   const inputTokens: string[] = [];
   const outputTokens: string[] = [];
 
-  const strategyMapping: { [inputToken: string]: { [outputToken: string]: string } } = {};
-
-  [currentStrategies, currentInputTokens, currentOutputTokens] =
-    await liquidatorsRegistry.callStatic.getAllPairsStrategies();
-
-  currentInputTokens.map((inputToken, index) => {
-    strategyMapping[inputToken] = { [currentOutputTokens[index]]: currentStrategies[index] };
-  });
-
-  let update = false;
   for (const inputToken in chainIdToConfig[chainId].redemptionStrategies) {
     const [redemptionStrategyType, outputToken] = chainIdToConfig[chainId].redemptionStrategies[inputToken];
     const redemptionStrategy = await ethers.getContract(redemptionStrategyType, deployer);
@@ -42,17 +28,9 @@ export const configureLiquidatorsRegistry = async ({
     strategies.push(redemptionStrategy.address);
     inputTokens.push(inputToken);
     outputTokens.push(outputToken);
-
-    const existingStrategy =
-      strategyMapping[inputToken] ?? strategyMapping[inputToken][outputToken]
-        ? strategyMapping[inputToken][outputToken]
-        : null;
-    if (existingStrategy != redemptionStrategy.address || existingStrategy == null || existingStrategy == undefined) {
-      update = true;
-    }
   }
-
-  if (update) {
+  const match = await liquidatorsRegistry.callStatic.pairsStrategiesMatch(strategies, inputTokens, outputTokens);
+  if (match) {
     const tx = await liquidatorsRegistry._resetRedemptionStrategies(strategies, inputTokens, outputTokens);
     console.log("waiting for tx ", tx.hash);
     await tx.wait();
