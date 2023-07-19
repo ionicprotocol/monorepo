ce9176622a1c7a09bde8883bacda7806592b6015
carlomazzaferro <carlo.mazzaferro@gmail.com>
2023-07-19 17:20:31 +0200

    remove unused

--- packages/chains/src/bsc/assets.ts
+++ packages/chains/src/bsc/assets.ts
@@ -36,29 +36,17 @@ const FRAX = "0x29cED01C447166958605519F10DcF8b0255fB379";
 const threeEPS = "0xaF4dE8E872131AE328Ce21D909C74705d3Aaf452";
 const maiThreeEPS = "0x80D00D2c8d920a9253c3D65BA901250a55011b37";
 const twoBRL = "0x1B6E11c5DB9B15DE87714eA9934a6c52371CfEA9";
-const threeBRL = "0x27b5Fc5333246F63280dA8e3e533512EfA747c13";
-const val3EPS = "0x5b5bD8913D766D005859CE002533D4838B0Ebbb5";
-const valdai3EPS = "0x8087a94FFE6bcF08DC4b4EBB3d28B4Ed75a792aC";
-const epsBUSD_jCHF = "0x5887cEa5e2bb7dD36F0C06Da47A8Df918c289A29";
-const BOMB = "0x522348779DCb2911539e76A1042aA922F9C47Ee3";
-const xBOMB = "0xAf16cB45B8149DA403AF41C63AbFEBFbcd16264b";
 const THE = "0xF4C8E32EaDEC4BFe97E0F595AdD0f4450a863a11";
 const RDNT = "0xf7DE7E8A6bd59ED41a4b5fe50278b3B7f31384dF";
 
 const stkBNB_WBNB = "0xaA2527ff1893e0D40d4a454623d362B79E8bb7F1";
 const stkBNB = "0xc2E9d07F66A89c44062459A47a0D2Dc038E4fb16";
-const asBNBx_WBNB = "0xB88F211EC9ecfc2931Ae1DE53ea28Da76B9Ed37A";
 const asANKR_ankrBNB = "0x653D51dbB4CC8B9Bcd884BB0c0795b4BE672AA4c";
 const epsBNBx_BNB = "0x5c73804FeDd39f3388E03F4aa1fE06a1C0e60c8e";
 const BNBx = "0x1bdd3Cf7F79cfB8EdbB955f20ad99211551BA275";
 const jBRL = "0x316622977073BBC3dF32E7d2A9B3c77596a0a603";
-const jCHF = "0x7c869b5A294b1314E985283d01C702B62224a05f";
 const jEUR = "0x23b8683Ff98F9E4781552DFE6f12Aa32814924e8";
-const jGBP = "0x048E9b1ddF9EBbb224812372280e94Ccac443f9e";
-const jMXN = "0x47b19Af93d0bC33805269Af02B5CA953Aa145127";
 const BRZ = "0x71be881e9C5d4465B3FfF61e89c6f3651E69B5bb";
-const BRZw = "0x5b1a9850f55d9282a7C4Bf23A2a21B050e3Beb2f";
-const BTCB_BOMB = "0x84392649eb0bC1c1532F2180E58Bae4E1dAbd8D6";
 const WBNB_BUSD = "0x58F876857a02D6762E0101bb5C46A8c1ED44Dc16";
 const WBNB_DAI = "0xc7c3cCCE4FA25700fD5574DA7E200ae28BBd36A3";
 const WBNB_USDC = "0xd99c7F6C65857AC913a8f880A4cb84032AB2FC5b";
@@ -277,61 +265,12 @@ const assets: SupportedAsset[] = [
     extraDocs: ellipsisDocs("0x68354c6E8Bbd020F9dE81EAf57ea5424ba9ef322", "mai3EPS", maiThreeEPS)
   },
   {
-    symbol: assetSymbols.val3EPS,
-    underlying: val3EPS,
-    name: "Ellipsis.finance val3EPS (BUSD/USDC/USDT)",
-    decimals: 18,
-    oracle: OracleTypes.CurveLpTokenPriceOracleNoRegistry,
-    extraDocs: ellipsisDocs("0x19EC9e3F7B21dd27598E7ad5aAe7dC0Db00A806d", "val3EPS", val3EPS)
-  },
-  {
-    symbol: assetSymbols.valdai3EPS,
-    underlying: valdai3EPS,
-    name: "Ellipsis.finance valdai3EPS (DAI, val3EPS)",
-    decimals: 18,
-    oracle: OracleTypes.CurveLpTokenPriceOracleNoRegistry,
-    extraDocs: ellipsisDocs("0x245e8bb5427822FB8fd6cE062d8dd853FbcfABF5", "valdai3EPS", valdai3EPS)
-  },
-  {
     symbol: assetSymbols["2brl"],
     underlying: twoBRL,
     name: "Ellipsis.finance 2BRL (BRZ, jBRL)",
     decimals: 18,
     oracle: OracleTypes.CurveLpTokenPriceOracleNoRegistry,
     extraDocs: ellipsisDocs("0xad51e40D8f255dba1Ad08501D6B1a6ACb7C188f3", "2brl", twoBRL)
-  },
-  {
-    symbol: assetSymbols["3brl"],
-    underlying: threeBRL,
-    name: "Ellipsis.finance 3BRL (jBRL, BRZ, BRZ (Wormhole))",
-    decimals: 18,
-    oracle: OracleTypes.CurveLpTokenPriceOracleNoRegistry,
-    extraDocs: ellipsisDocs("0x43719DfFf12B04C71F7A589cdc7F54a01da07D7a", "3brl", threeBRL)
-  },
-  {
-    symbol: assetSymbols["JCHF-BUSD"],
-    underlying: epsBUSD_jCHF,
-    name: "Ellipsis.finance JCHF-BUSD",
-    decimals: 18,
-    oracle: OracleTypes.CurveV2LpTokenPriceOracleNoRegistry,
-    extraDocs: ellipsisDocs("0xBcA6E25937B0F7E0FD8130076b6B218F595E32e2", "eps BUSD jCHF", epsBUSD_jCHF)
-  },
-  // Bomb
-  {
-    symbol: assetSymbols.BOMB,
-    underlying: BOMB,
-    name: "BOMB",
-    decimals: 18,
-    oracle: OracleTypes.UniswapTwapPriceOracleV2,
-    extraDocs: defaultDocs("https://bscscan.com", BOMB),
-    disabled: true
-  },
-  {
-    symbol: assetSymbols.xBOMB,
-    underlying: xBOMB,
-    name: "xBOMB",
-    decimals: 18,
-    disabled: true
   },
   {
     symbol: assetSymbols.ankrBNB,
@@ -364,22 +303,6 @@ const assets: SupportedAsset[] = [
     decimals: 18,
     oracle: OracleTypes.CurveLpTokenPriceOracleNoRegistry,
     extraDocs: ellipsisDocs("0xFD4afeAc39DA03a05f61844095A75c4fB7D766DA", "BNBx/BNB", epsBNBx_BNB)
-  },
-  {
-    symbol: assetSymbols["asBNBx-WBNB"],
-    underlying: asBNBx_WBNB,
-    name: "BNBx-BNB ApeSwap LP",
-    decimals: 18,
-    oracle: OracleTypes.UniswapLpTokenPriceOracle,
-    extraDocs: apeSwapDocs(WBNB, BNBx, "BNBx-WBNB", asBNBx_WBNB)
-  },
-  {
-    symbol: assetSymbols["BTCB-BOMB"],
-    underlying: BTCB_BOMB,
-    name: "BOMB-BTC PCS LP",
-    decimals: 18,
-    oracle: OracleTypes.UniswapLpTokenPriceOracle,
-    extraDocs: pancakeSwapDocs(BTCB, BOMB, "BOMB-BTC", BTCB_BOMB)
   },
   {
     symbol: assetSymbols["stkBNB-WBNB"],
@@ -415,33 +338,9 @@ const assets: SupportedAsset[] = [
     extraDocs: jarvisDocs("v1")
   },
   {
-    symbol: assetSymbols.JCHF,
-    underlying: jCHF,
-    name: "Jarvis Synthetic Swiss Franc",
-    decimals: 18,
-    oracle: OracleTypes.ChainlinkPriceOracleV2,
-    extraDocs: jarvisDocs("v1")
-  },
-  {
     symbol: assetSymbols.JEUR,
     underlying: jEUR,
     name: "Jarvis Synthetic Euro",
-    decimals: 18,
-    oracle: OracleTypes.ChainlinkPriceOracleV2,
-    extraDocs: jarvisDocs("v1")
-  },
-  {
-    symbol: assetSymbols.JGBP,
-    underlying: jGBP,
-    name: "Jarvis Synthetic British Pound",
-    decimals: 18,
-    oracle: OracleTypes.ChainlinkPriceOracleV2,
-    extraDocs: jarvisDocs("v1")
-  },
-  {
-    symbol: assetSymbols.JMXN,
-    underlying: jMXN,
-    name: "Jarvis Synthetic Mexican Peso",
     decimals: 18,
     oracle: OracleTypes.ChainlinkPriceOracleV2,
     extraDocs: jarvisDocs("v1")
@@ -454,15 +353,6 @@ const assets: SupportedAsset[] = [
     oracle: OracleTypes.ChainlinkPriceOracleV2,
     extraDocs: `<p><b>How to acquire this token</b><p/><br />
     <p>You can acquire BRZ tokens at <a href="https://www.brztoken.io" target="_blank" style="color: #BCAC83; cursor="pointer">https://www.brztoken.io</> or other centralised exchanges</p>`
-  },
-  {
-    symbol: assetSymbols.BRZw,
-    underlying: BRZw,
-    name: "BRZ Token (Wormhole)",
-    decimals: 4,
-    oracle: OracleTypes.ChainlinkPriceOracleV2,
-    extraDocs: `<p><b>How to acquire this token</b><p/><br />
-    <p>This is the Wormhole-bridged version of BRZ. To get it, you can bridge BRZ from Solana to BSC using the <a href="https://www.portalbridge.com/#/transfer" target="_blank" style="color: #BCAC83; cursor="pointer">Official Bridge</></p>`
   },
   {
     symbol: assetSymbols["WBNB-BUSD"],
