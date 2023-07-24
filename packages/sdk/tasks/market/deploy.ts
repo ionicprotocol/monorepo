3b45695057a146ef15ba7a884bb6d18ed39cef82
carlomazzaferro <carlo.mazzaferro@gmail.com>
2023-07-24 14:25:55 +0200

    deploy market task

--- packages/sdk/tasks/market/deploy.ts
+++ packages/sdk/tasks/market/deploy.ts
@@ -1,8 +1,10 @@
+import { MarketConfig } from "@ionicprotocol/types";
 import { task, types } from "hardhat/config";
 
 task("market:deploy", "deploy market")
   .addParam("signer", "Named account to use for tx", "deployer", types.string)
-  .addParam("cf", "Collateral factor", "800000000000000000", types.string)
+  .addParam("cf", "Collateral factor", "80", types.string)
+  .addParam("underlying", "Collateral factor", "80", types.string)
   .addParam("comptroller", "Comptroller address", undefined, types.string)
   .setAction(async (taskArgs, { ethers }) => {
     const signer = await ethers.getNamedSigner(taskArgs.signer);
@@ -10,23 +12,57 @@ task("market:deploy", "deploy market")
     const sdk = await ionicSdkModule.getOrCreateIonic(signer);
     const comptroller = sdk.createComptroller(taskArgs.comptroller, signer);
 
-    const constructorData = "";
-    const becomeImplData = "";
+    const abiCoder = new ethers.utils.AbiCoder();
 
-    const collateralFactor = ethers.BigNumber.from(taskArgs.cf);
+    const delegateType = 1;
+    const implementationData = "0x00";
+
+    const config: MarketConfig = {
+      underlying: taskArgs.underlying,
+      comptroller: comptroller.address,
+      adminFee: 0,
+      collateralFactor: parseInt(taskArgs.cf),
+      interestRateModel: sdk.chainDeployment.JumpRateModel.address,
+      reserveFactor: 0,
+      bypassPriceFeedCheck: true,
+      feeDistributor: sdk.chainDeployment.FeeDistributor.address,
+      symbol: "fUSDR-1",
+      name: "Pearl Farm USDR"
+    };
+
+    const reserveFactorBN = ethers.utils.parseUnits((config.reserveFactor / 100).toString());
+    const adminFeeBN = ethers.utils.parseUnits((config.adminFee / 100).toString());
+    const collateralFactorBN = ethers.utils.parseUnits((config.collateralFactor / 100).toString());
+
+    const deployArgs = [
+      config.underlying,
+      config.comptroller,
+      config.feeDistributor,
+      config.interestRateModel,
+      config.name,
+      config.symbol,
+      implementationData,
+      reserveFactorBN,
+      adminFeeBN
+    ];
+
+    const constructorData = abiCoder.encode(
+      ["address", "address", "address", "address", "string", "string", "bytes", "uint256", "uint256"],
+      deployArgs
+    );
+
     // Test Transaction
-    const errorCode = await comptroller.callStatic._deployMarket(1, constructorData, becomeImplData, collateralFactor);
+    const errorCode = await comptroller.callStatic._deployMarket(
+      delegateType,
+      constructorData,
+      implementationData,
+      collateralFactorBN
+    );
     if (errorCode.toNumber() !== 0) {
       throw `Unable to _deployMarket: ${sdk.COMPTROLLER_ERROR_CODES[errorCode.toNumber()]}`;
     }
     // Make actual Transaction
-    const tx = await comptroller._deployMarket(1, constructorData, becomeImplData, collateralFactor, {
-      // suggested: 394700457
-      // max:         7920027
-      gasLimit: 20020027,
-      maxFeePerGas: ethers.utils.parseUnits("240", "gwei"),
-      maxPriorityFeePerGas: ethers.utils.parseUnits("50", "gwei")
-    });
+    const tx = await comptroller._deployMarket(1, constructorData, implementationData, collateralFactorBN);
     console.log("tx", tx.hash, tx.nonce);
 
     // Recreate Address of Deployed Market
