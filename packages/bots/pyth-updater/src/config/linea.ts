904197b44d1b5f8646b58013a1af4a87cf68956e
carlomazzaferro <carlo.mazzaferro@gmail.com>
2023-07-31 10:35:21 +0200

    increase min deviation

--- packages/bots/pyth-updater/src/config/linea.ts
+++ packages/bots/pyth-updater/src/config/linea.ts
@@ -9,20 +9,20 @@ export const pythConfig: PythAssetConfig[] = [
     priceId: '0x5bc91f13e412c07599167bae86f07543f076a638962b8d6017ec19dab4a82814',
     configRefreshRateInSeconds: 3600,
     validTimePeriodSeconds: 86400, // 24 hours
-    deviationThresholdBps: 100, // 1%
+    deviationThresholdBps: 100, // 2%
   },
   {
     // BNB/USD
     priceId: '0x2f95862b045670cd22bee3114c39763a4a08beeb663b145d283c31d7d1101c4f',
     configRefreshRateInSeconds: 3600,
     validTimePeriodSeconds: 86400, // 24 hours
-    deviationThresholdBps: 100, // 1%
+    deviationThresholdBps: 250, // 2%
   },
   {
     // MATIC/USD
     priceId: '0x5de33a9112c2b700b8d30b8a3402c103578ccfa2765696471cc672bd5cf6ac52',
     configRefreshRateInSeconds: 3600,
     validTimePeriodSeconds: 86400, // 24 hours
-    deviationThresholdBps: 100, // 1%
+    deviationThresholdBps: 250, // 2%
   },
 ];
