c94cfdec087d1163e1c97cea5101b6febb0016d0
carlomazzaferro <carlo.mazzaferro@gmail.com>
2023-08-01 09:40:01 +0200

    remove unsued

--- packages/chains/src/arbitrum/redemptionStrategies.ts
+++ packages/chains/src/arbitrum/redemptionStrategies.ts
@@ -1,23 +1,8 @@
 import { assetSymbols, RedemptionStrategy, RedemptionStrategyContract, underlying } from "@ionicprotocol/types";
 
-import { assets, USDC } from "./assets";
+import { assets } from "./assets";
 
 const redemptionStrategies: RedemptionStrategy[] = [
-  {
-    inputToken: underlying(assets, assetSymbols.saddleFraxBP),
-    strategy: RedemptionStrategyContract.SaddleLpTokenLiquidator,
-    outputToken: USDC
-  },
-  {
-    inputToken: underlying(assets, assetSymbols.saddleFraxUsdsBP),
-    strategy: RedemptionStrategyContract.SaddleLpTokenLiquidator,
-    outputToken: underlying(assets, assetSymbols.saddleFraxBP)
-  },
-  {
-    inputToken: underlying(assets, assetSymbols.saddleFraxUsdtBP),
-    strategy: RedemptionStrategyContract.SaddleLpTokenLiquidator,
-    outputToken: underlying(assets, assetSymbols.saddleFraxBP)
-  },
   {
     inputToken: underlying(assets, assetSymbols.OHM),
     strategy: RedemptionStrategyContract.BalancerSwapLiquidator,
