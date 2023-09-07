04934106901fe73bebbed1c44d11bc6998fa1b99
carlomazzaferro <carlo.mazzaferro@gmail.com>
2023-09-07 08:59:44 +0200

    Deploy all tangible / pearl plugins

--- packages/chains/src/polygon/plugins.ts
+++ packages/chains/src/polygon/plugins.ts
@@ -13,6 +13,66 @@ const deployedPlugins: DeployedPlugins = {
     otherParams: ["0x2C6d11e756986f1537a2c71c851e9a1F7A0008b2", "0"],
     icon: "https://d1912tcoux65lj.cloudfront.net/plugin/beefy.png"
   },
+  // BeefyERC4626_sAMM-USDC-USDR_0x83DF24fE1B1eBF38048B91ffc4a8De0bAa88b891.json
+  "0xf634F01D04dA89914d03E914897eA068b3886714": {
+    market: "0x83DF24fE1B1eBF38048B91ffc4a8De0bAa88b891",
+    name: "Beefy USDC-USDR sLP vault Vault",
+    strategy: Strategy.Beefy,
+    apyDocsUrl: "https://app.beefy.finance/vault/pearl-usdc-usdrv3",
+    underlying: underlying(assets, assetSymbols["sAMM-USDC/USDR"]),
+    otherParams: ["0xD74B5df80347cE9c81b91864DF6a50FfAfE44aa5", "0"],
+    icon: "https://d1912tcoux65lj.cloudfront.net/plugin/beefy.png"
+  },
+  // BeefyERC4626_vAMM-MATIC-USDR_0xfacEdA4f9731797102f040380aD5e234c92d1942.json
+  "0x046eBfaC5da8c603db95421861e536e3e16a86Be": {
+    market: "0xfacEdA4f9731797102f040380aD5e234c92d1942",
+    name: "Beefy MATIC-USDR vLP vault Vault",
+    strategy: Strategy.Beefy,
+    apyDocsUrl: "https://app.beefy.finance/vault/pearl-matic-usdrv3",
+    underlying: underlying(assets, assetSymbols["vAMM-MATIC/USDR"]),
+    otherParams: ["0x3325a25A2608EA723cB5D72E27af65AACFfb810e", "0"],
+    icon: "https://d1912tcoux65lj.cloudfront.net/plugin/beefy.png"
+  },
+  // BeefyERC4626_vAMM-wUSDR-USDR_0x06F61E22ef144f1cC4550D40ffbF681CB1C3aCAF.json
+  "0x76B30142Ab37fd78bbD5214D742d9916eAA68249": {
+    market: "0x06F61E22ef144f1cC4550D40ffbF681CB1C3aCAF",
+    name: "Beefy WUSDR-USDR vLP vault Vault",
+    strategy: Strategy.Beefy,
+    apyDocsUrl: "https://app.beefy.finance/vault/pearl-wusdr-usdrv3",
+    underlying: underlying(assets, assetSymbols["vAMM-wUSDR/USDR"]),
+    otherParams: ["0x67FEe70c32097c53B5aeAaCdbee7544eaE2194f8", "0"],
+    icon: "https://d1912tcoux65lj.cloudfront.net/plugin/beefy.png"
+  },
+  // BeefyERC4626_vAMM-WETH-USDR_0x343D9a8D2Bc6A62390aEc764bb5b900C4B039127.json
+  "0x06D929F4433825C9099D73C9F5512aD965A81c40": {
+    market: "0x343D9a8D2Bc6A62390aEc764bb5b900C4B039127",
+    name: "Beefy WETH-USDR vLP vault Vault",
+    strategy: Strategy.Beefy,
+    apyDocsUrl: "https://app.beefy.finance/vault/pearl-usdrv3-weth",
+    underlying: underlying(assets, assetSymbols["vAMM-WETH/USDR"]),
+    otherParams: ["0xbfe9584228BBcB21EAA64a878c28c5cBFd0F6304", "0"],
+    icon: "https://d1912tcoux65lj.cloudfront.net/plugin/beefy.png"
+  },
+  // BeefyERC4626_vAMM-WBTC-USDR_0xffc8c8d747E52fAfbf973c64Bab10d38A6902c46.json
+  "0x7d2eAc8C5241DB0447Aa5509E0399B8A0c41f6D5": {
+    market: "0xffc8c8d747E52fAfbf973c64Bab10d38A6902c46",
+    name: "Beefy WBTC-USDR vLP vault Vault",
+    strategy: Strategy.Beefy,
+    apyDocsUrl: "https://app.beefy.finance/vault/pearl-wbtc-usdrv3",
+    underlying: underlying(assets, assetSymbols["vAMM-WBTC/USDR"]),
+    otherParams: ["0x40c03Cd0B5D1571608D09e6279B4C63660e431F1", "0"],
+    icon: "https://d1912tcoux65lj.cloudfront.net/plugin/beefy.png"
+  },
+  // BeefyERC4626_vAMM-TNGBL-USDR_0x2E870Aeee3D9d1eA29Ec93d2c0A99A4e0D5EB697.json
+  "0xD1Bf0045a8f81f93B40b51C3Ccae2516e199e525": {
+    market: "0x2E870Aeee3D9d1eA29Ec93d2c0A99A4e0D5EB697",
+    name: "Beefy WBTC-USDR vLP vault Vault",
+    strategy: Strategy.Beefy,
+    apyDocsUrl: "https://app.beefy.finance/vault/pearl-usdrv3-tngbl",
+    underlying: underlying(assets, assetSymbols["vAMM-TNGBL/USDR"]),
+    otherParams: ["0x614055aEC4B4248Ab64F976A9060e945D6095ff8", "0"],
+    icon: "https://d1912tcoux65lj.cloudfront.net/plugin/beefy.png"
+  },
   // BeefyERC4626_aUSDC-CASH-N_0x1D2A7078a404ab970f951d5A6dbECD9e24838FB6.json
   "0x3838a561597F9c0BEb722978F7dbDD2fbdF9dEd0": {
     market: "0x1D2A7078a404ab970f951d5A6dbECD9e24838FB6",
@@ -50,7 +110,7 @@ const deployedPlugins: DeployedPlugins = {
     strategy: Strategy.Beefy,
     apyDocsUrl: "https://app.beefy.finance/vault/retro-gamma-wmatic-maticx",
     underlying: underlying(assets, assetSymbols["aWBTC_WETH_N"]),
-    otherParams: ["0xf552a67A82908E6C7F4382b812218d665e058C0B", "0"],
+    otherParams: ["0xec74671f95F0942358016da627b912143100DAF2", "0"],
     icon: "https://d1912tcoux65lj.cloudfront.net/plugin/beefy.png"
   }
 };
