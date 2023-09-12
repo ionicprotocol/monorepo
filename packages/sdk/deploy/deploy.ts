4494e5b45d6f418f2697d6327475013ebd441a2d
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2023-09-12 08:03:24 +0300

    fix in the deploy script

--- packages/sdk/deploy/deploy.ts
+++ packages/sdk/deploy/deploy.ts
@@ -752,9 +752,9 @@ const func: DeployFunction = async ({ run, ethers, getNamedAccounts, deployments
     const leveredPosFactoryAr = await authoritiesRegistry.callStatic.leveredPositionsFactory();
     if (leveredPosFactoryAr.toLowerCase() != leveredPositionFactory.address.toLowerCase()) {
       // set the address in the AR
-      tx = await authoritiesRegistry.reinitialize(authoritiesRegistry.address);
+      tx = await authoritiesRegistry.reinitialize(leveredPositionFactory.address);
       await tx.wait();
-      console.log(`configured the auth registry in the FFD`);
+      console.log(`configured the levered positions factory in the auth registry`);
     }
     ////
   }
