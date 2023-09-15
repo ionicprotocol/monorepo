dae6b6aa6e190ee499b7bdcd4f4bfa5d1fbf35ee
smartdevcom <smart.dev.cop@gmail.com>
2023-09-15 17:57:04 +1000

    added token names

--- packages/ui/components/pages/LeveragePage/ActivePools/CollateralAsset.tsx
+++ packages/ui/components/pages/LeveragePage/ActivePools/CollateralAsset.tsx
@@ -1,7 +1,13 @@
+import { HStack, Text } from '@chakra-ui/react';
 import type { NewPosition } from '@ionicprotocol/types';
 
 import { TokenIcon } from '@ui/components/shared/TokenIcon';
 
 export const CollateralAsset = ({ position }: { position: NewPosition }) => {
-  return <TokenIcon address={position.collateral.underlyingToken} chainId={position.chainId} />;
+  return (
+    <HStack>
+      <TokenIcon address={position.collateral.underlyingToken} chainId={position.chainId} />
+      <Text>{position.collateral.symbol}</Text>
+    </HStack>
+  );
 };
