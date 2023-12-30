b64b234a433a9947a7dd1ed5759d6c8b2f972fd6
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2023-12-30 22:04:06 +0200

    addresses fixes

--- packages/chains/src/mode/addresses.ts
+++ packages/chains/src/mode/addresses.ts
@@ -5,14 +5,14 @@ import { assets } from "./assets";
 
 const chainAddresses: ChainAddresses = {
   PAIR_INIT_HASH: "",
-  STABLE_TOKEN: "",
+  STABLE_TOKEN: ethers.constants.AddressZero,
   UNISWAP_V2_ROUTER: ethers.constants.AddressZero,
   UNISWAP_V2_FACTORY: ethers.constants.AddressZero,
-  UNISWAP_V3: { FACTORY: "", PAIR_INIT_HASH: "", QUOTER_V2: "" },
-  UNISWAP_V3_ROUTER: "",
-  W_BTC_TOKEN: "",
+  UNISWAP_V3: { FACTORY: ethers.constants.AddressZero, PAIR_INIT_HASH: "", QUOTER_V2: ethers.constants.AddressZero },
+  UNISWAP_V3_ROUTER: ethers.constants.AddressZero,
+  W_BTC_TOKEN: ethers.constants.AddressZero,
   W_TOKEN: underlying(assets, assetSymbols.WETH),
-  W_TOKEN_USD_CHAINLINK_PRICE_FEED: ""
+  W_TOKEN_USD_CHAINLINK_PRICE_FEED: ethers.constants.AddressZero
 };
 
 export default chainAddresses;
