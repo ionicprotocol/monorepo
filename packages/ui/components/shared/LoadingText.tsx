a6205abf436638ca0a5eeee53f3b4331b26b9963
k.zlatarov <k.zlatarov@dcvx.io>
2024-01-24 12:42:14 +0200

    Remove old files

--- packages/ui/components/shared/LoadingText.tsx
+++
@@ -1,6 +0,0 @@
-import type { TextProps } from '@chakra-ui/react';
-import { Text } from '@chakra-ui/react';
-
-export const LoadingText = ({ ...props }: TextProps) => {
-  return <Text {...props}>Loading</Text>;
-};
