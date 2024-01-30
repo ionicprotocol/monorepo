b626cb1e28d7d8561ef829b92dee5c06dc184785
Rahul Sethuram <rahul.eth@icloud.com>
2024-01-30 15:30:34 +0400

    fix: link

--- packages/ui/app/_components/Navbar.tsx
+++ packages/ui/app/_components/Navbar.tsx
@@ -18,7 +18,7 @@ export default function Navbar() {
   return (
     <nav className="fixed z-50 flex items-center justify-between w-full py-4 pt-8 px-[4%] text-lg text-white/50 transition-all duration-300 ease-linear -translate-x-1/2 bg-transparent backdrop-blur-sm font-inter top-0 left-1/2 rounded-xl">
       <Link
-        href={'/market'}
+        href={'/'}
         className={`flex items-center  pr-10  `}
       >
         <img
