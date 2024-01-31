83eab74cf3e898227c753835b0afa954637e9a4f
Rahul Sethuram <rahul.eth@icloud.com>
2024-01-31 11:47:15 +0400

    fix: mode default

--- packages/bots/liquidator/src/config.ts
+++ packages/bots/liquidator/src/config.ts
@@ -4,8 +4,8 @@ doetenv.config();
 const config = {
   environment: process.env.NODE_ENV ?? "development",
   logLevel: process.env.LOG_LEVEL ?? "info",
-  chainId: parseInt(process.env.TARGET_CHAIN_ID ?? "56", 10),
-  rpcUrl: process.env.WEB3_HTTP_PROVIDER_URL ?? "https://bsc-dataseed1.binance.org/",
+  chainId: parseInt(process.env.TARGET_CHAIN_ID ?? "34443", 10),
+  rpcUrl: process.env.WEB3_HTTP_PROVIDER_URL ?? "https://mainnet.mode.network/",
   adminPrivateKey: process.env.ETHEREUM_ADMIN_PRIVATE_KEY ?? "",
   adminAccount: process.env.ETHEREUM_ADMIN_ACCOUNT ?? "",
   excludedComptrollers: process.env.EXCLUDED_COMPTROLLERS ? process.env.EXCLUDED_COMPTROLLERS.split(",") : [],
