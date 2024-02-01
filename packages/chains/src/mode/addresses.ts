91256a7c57070e8436414f8042e991c98bac641c
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2024-02-01 09:26:17 +0200

    kim uni v2 config

--- packages/chains/src/mode/addresses.ts
+++ packages/chains/src/mode/addresses.ts
@@ -4,10 +4,10 @@ import { ethers } from "ethers";
 import { assets } from "./assets";
 
 const chainAddresses: ChainAddresses = {
-  PAIR_INIT_HASH: "",
+  PAIR_INIT_HASH: "", // TODO is this used anywhere?
   STABLE_TOKEN: underlying(assets, assetSymbols.USDC),
-  UNISWAP_V2_ROUTER: ethers.constants.AddressZero,
-  UNISWAP_V2_FACTORY: ethers.constants.AddressZero,
+  UNISWAP_V2_ROUTER: "0x5D61c537393cf21893BE619E36fC94cd73C77DD3",
+  UNISWAP_V2_FACTORY: "0xc02155946dd8c89d3d3238a6c8a64d04e2cd4500",
   UNISWAP_V3: {
     FACTORY: "0xC33Ce0058004d44E7e1F366E5797A578fDF38584",
     PAIR_INIT_HASH: "0x3e03ddab0aa29c12c46cd283f9cf8c6800eb7ea3c6530a382474bac82333f2e0",
