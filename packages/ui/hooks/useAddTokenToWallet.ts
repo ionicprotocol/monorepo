35eb1e7028afc897e9d9921412ee1c0ab13648b8
k.zlatarov <k.zlatarov@dcvx.io>
2024-02-08 09:11:39 +0200

    Comment out unused file to prevent TS error

--- packages/ui/hooks/useAddTokenToWallet.ts
+++ packages/ui/hooks/useAddTokenToWallet.ts
@@ -1,50 +1,50 @@
-import { useCallback } from 'react';
-import type { Address } from 'viem';
+// import { useCallback } from 'react';
+// import type { Address } from 'viem';
 
-export const useAddTokenToWallet = ({
-  underlyingAddress,
-  underlyingSymbol,
-  underlyingDecimals,
-  logoUrl
-}: {
-  logoUrl?: string;
-  underlyingAddress: string;
-  underlyingDecimals: number;
-  underlyingSymbol: string;
-}) =>
-  useCallback(async () => {
-    const ethereum = window.ethereum;
+// export const useAddTokenToWallet = ({
+//   underlyingAddress,
+//   underlyingSymbol,
+//   underlyingDecimals,
+//   logoUrl
+// }: {
+//   logoUrl?: string;
+//   underlyingAddress: string;
+//   underlyingDecimals: number;
+//   underlyingSymbol: string;
+// }) =>
+//   useCallback(async () => {
+//     const ethereum = window.ethereum;
 
-    if (!ethereum || !ethereum.request) {
-      return false;
-    }
+//     if (!ethereum || !ethereum.request) {
+//       return false;
+//     }
 
-    try {
-      const added = await ethereum.request({
-        method: 'wallet_watchAsset',
-        params: [
-          {
-            options: {
-              address: underlyingAddress,
-              decimals: underlyingDecimals,
-              image: logoUrl,
-              symbol: underlyingSymbol
-            },
-            type: 'ERC20'
-          } as {
-            options: {
-              address: Address;
-              decimals: number;
-              image?: string;
-              symbol: string;
-            };
-            type: 'ERC20';
-          }
-        ]
-      });
+//     try {
+//       const added = await ethereum.request({
+//         method: 'wallet_watchAsset',
+//         params: [
+//           {
+//             options: {
+//               address: underlyingAddress,
+//               decimals: underlyingDecimals,
+//               image: logoUrl,
+//               symbol: underlyingSymbol
+//             },
+//             type: 'ERC20'
+//           } as {
+//             options: {
+//               address: Address;
+//               decimals: number;
+//               image?: string;
+//               symbol: string;
+//             };
+//             type: 'ERC20';
+//           }
+//         ]
+//       });
 
-      return added;
-    } catch (error) {
-      return false;
-    }
-  }, [underlyingAddress, underlyingSymbol, underlyingDecimals, logoUrl]);
+//       return added;
+//     } catch (error) {
+//       return false;
+//     }
+//   }, [underlyingAddress, underlyingSymbol, underlyingDecimals, logoUrl]);
