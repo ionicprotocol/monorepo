26e63657e5dcdef41c4f2fafab9275ad96328dd1
k.zlatarov <k.zlatarov@dcvx.io>
2024-02-08 16:10:06 +0200

    Remove color change of button text

--- packages/ui/app/_components/ConnectButton.tsx
+++ packages/ui/app/_components/ConnectButton.tsx
@@ -1,23 +1,7 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 type ConnectButtonProps = JSX.IntrinsicElements['w3m-button'];
 
 export default function ConnectButton(props: ConnectButtonProps) {
-  useEffect(() => {
-    const connectButtonCSSOvverides = document.createElement('style');
-
-    connectButtonCSSOvverides.innerHTML = `
-      :root {
-        --wui-color-inverse-100: #0a0a0aff;
-      }
-    `;
-
-    document.body.appendChild(connectButtonCSSOvverides);
-
-    return () => {
-      document.body.removeChild(connectButtonCSSOvverides);
-    };
-  }, []);
-
   return <w3m-button {...props} />;
 }
