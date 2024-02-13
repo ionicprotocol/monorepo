6ee32aab6a6726488aa7ab7b42f82453b1d8e30b
k.zlatarov <k.zlatarov@dcvx.io>
2024-02-13 10:57:19 +0200

    Hide unused stuff from points page

--- packages/ui/app/points/page.tsx
+++ packages/ui/app/points/page.tsx
@@ -3,16 +3,19 @@
 
 import Link from 'next/link';
 import { useMemo } from 'react';
+import { useChainId } from 'wagmi';
 
 import FlatMap from '../_components/points_comp/FlatMap';
 import PercentMeter from '../_components/points_comp/PercentMeter';
 import ReferralLeaderboard from '../_components/points_comp/ReferralLeaderboard';
 import ResultHandler from '../_components/ResultHandler';
 
+import { useFusePoolData } from '@ui/hooks/useFusePoolData';
 import {
   usePointsForBorrow,
   usePointsForSupply
 } from '@ui/hooks/usePointsQueries';
+import millify from 'millify';
 
 export default function Points() {
   const leaderboardData = [
@@ -35,6 +38,12 @@ export default function Points() {
       vaultSupply: 426
     }
   ];
+  const chainId = useChainId();
+  const { data: marketData, isLoading: isLoadingMarketData } = useFusePoolData(
+    '0',
+    chainId
+  );
+  console.log(marketData);
   const { data: supplyPoints, isLoading: isLoadingSupplyPoints } =
     usePointsForSupply();
   const { data: borrowPoints, isLoading: isLoadingBorrowPoints } =
@@ -156,59 +165,75 @@ export default function Points() {
           </ResultHandler>
         </div>
 
-        <FlatMap rewardsData={[summedSupplyPoints, summedBorrowPoints]} />
-        <div
-          className={`w-full gap-x-1 grid  grid-cols-5  py-4 text-[10px] text-white/40 font-semibold text-center  `}
+        <ResultHandler
+          center
+          isLoading={
+            isLoadingMarketData ||
+            isLoadingSupplyPoints ||
+            isLoadingBorrowPoints
+          }
         >
-          <h3 className={` `}>STRATEGY</h3>
-          <h3 className={` `}>AMOUNT</h3>
-          <h3 className={` `}>VAULT SUPPLY</h3>
-          <h3 className={` `}>POINTS</h3>
-          <h3 className={` `}>PERCENTAGE EARNINGS</h3>
-        </div>
-        <div
-          className={`w-full hover:bg-graylite transition-all duration-200 ease-linear bg-grayUnselect rounded-xl mb-3 px-2  gap-x-1 grid  grid-cols-5  py-5 text-xs text-white/80 font-semibold text-center items-center `}
-        >
-          <div className={`  flex gap-2 items-center justify-center  `}>
-            <span
-              className="h-4 w-4 rounded-full"
-              style={{ backgroundColor: `#3bff89` }}
-            />
-            <span className={` `}>Supply</span>
-          </div>
-          <span className={``}>5</span>
-          <span className={``}>5</span>
-          <span className={``}>{summedSupplyPoints}</span>
-          <PercentMeter
-            color="#3bff89"
-            percent={parseFloat(
-              ((summedSupplyPoints / totalPoints) * 100).toFixed(1)
-            )}
-          />
-        </div>
-        <div
-          className={`w-full hover:bg-graylite transition-all duration-200 ease-linear bg-grayUnselect rounded-xl mb-3 px-2  gap-x-1 grid  grid-cols-5  py-5 text-xs text-white/80 font-semibold text-center items-center `}
-        >
-          <div className={`  flex gap-2 items-center justify-center  `}>
-            <span
-              className="h-4 w-4 rounded-full"
-              style={{ backgroundColor: `#f3fa96` }}
-            />
-            <span className={` `}>Borrow</span>
-          </div>
-          <span className={``}>5</span>
-          <span className={``}>5</span>
-          <span className={``}>{summedBorrowPoints}</span>
-          <PercentMeter
-            color="#f3fa96"
-            percent={parseFloat(
-              ((summedBorrowPoints / totalPoints) * 100).toFixed(1)
-            )}
-          />
-        </div>
+          <>
+            <FlatMap rewardsData={[summedSupplyPoints, summedBorrowPoints]} />
+
+            <div
+              className={`w-full gap-x-1 grid  grid-cols-5  py-4 text-[10px] text-white/40 font-semibold text-center  `}
+            >
+              <h3 className={` `}>STRATEGY</h3>
+              <h3 className={` `}>AMOUNT</h3>
+              <h3 className={` `}>VAULT SUPPLY</h3>
+              <h3 className={` `}>POINTS</h3>
+              <h3 className={` `}>PERCENTAGE EARNINGS</h3>
+            </div>
+            <div
+              className={`w-full hover:bg-graylite transition-all duration-200 ease-linear bg-grayUnselect rounded-xl mb-3 px-2  gap-x-1 grid  grid-cols-5  py-5 text-xs text-white/80 font-semibold text-center items-center `}
+            >
+              <div className={`  flex gap-2 items-center justify-center  `}>
+                <span
+                  className="h-4 w-4 rounded-full"
+                  style={{ backgroundColor: `#3bff89` }}
+                />
+                <span className={` `}>Supply</span>
+              </div>
+              <span className={``}>5</span>
+              <span className={``}>
+                ${millify(marketData?.totalSupplyBalanceFiat ?? 0)}
+              </span>
+              <span className={``}>{summedSupplyPoints}</span>
+              <PercentMeter
+                color="#3bff89"
+                percent={parseFloat(
+                  ((summedSupplyPoints / totalPoints) * 100).toFixed(1)
+                )}
+              />
+            </div>
+            <div
+              className={`w-full hover:bg-graylite transition-all duration-200 ease-linear bg-grayUnselect rounded-xl mb-3 px-2  gap-x-1 grid  grid-cols-5  py-5 text-xs text-white/80 font-semibold text-center items-center `}
+            >
+              <div className={`  flex gap-2 items-center justify-center  `}>
+                <span
+                  className="h-4 w-4 rounded-full"
+                  style={{ backgroundColor: `#f3fa96` }}
+                />
+                <span className={` `}>Borrow</span>
+              </div>
+              <span className={``}>5</span>
+              <span className={``}>
+                ${millify(marketData?.totalBorrowBalanceFiat ?? 0)}
+              </span>
+              <span className={``}>{summedBorrowPoints}</span>
+              <PercentMeter
+                color="#f3fa96"
+                percent={parseFloat(
+                  ((summedBorrowPoints / totalPoints) * 100).toFixed(1)
+                )}
+              />
+            </div>
+          </>
+        </ResultHandler>
       </div>
 
-      <div className="w-[70%] flex flex-col items-start py-4 mt-3 justify-start bg-grayone h-min px-[3%] rounded-xl">
+      {/* <div className="w-[70%] flex flex-col items-start py-4 mt-3 justify-start bg-grayone h-min px-[3%] rounded-xl">
         <h1 className={`font-semibold text-xl `}>Your Top Referrals</h1>
         <div
           className={` w-full flex items-center justify-between text-[10px] my-2 text-white/50`}
@@ -236,8 +261,8 @@ export default function Points() {
               vaultSupply={val.vaultSupply}
             />
           ))}
-      </div>
-      <div className="w-[70%] flex flex-col items-start py-4 mt-3 justify-start bg-grayone h-min px-[3%] rounded-xl">
+      </div> */}
+      {/* <div className="w-[70%] flex flex-col items-start py-4 mt-3 justify-start bg-grayone h-min px-[3%] rounded-xl">
         <h1 className={`font-semibold text-xl `}>Global Leaderboard </h1>
         <div
           className={` w-full flex items-center justify-between text-[10px] my-2 text-white/50`}
@@ -265,7 +290,7 @@ export default function Points() {
               vaultSupply={val.vaultSupply}
             />
           ))}
-      </div>
+      </div> */}
     </main>
   );
 }
