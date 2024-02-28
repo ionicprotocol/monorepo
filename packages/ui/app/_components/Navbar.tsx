a6773b827ff6788035f29e66f6e3a2e9dcaaca79
Rahul Sethuram <rahul.eth@icloud.com>
2024-02-28 08:24:54 -0700

    feat: remove

--- packages/ui/app/_components/Navbar.tsx
+++ packages/ui/app/_components/Navbar.tsx
@@ -4,7 +4,7 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React, { useState } from 'react';
-import '@gasbot/widget/style.css';
+// import '@gasbot/widget/style.css';
 
 import ConnectButton from './ConnectButton';
 
