ad4522575efde5df7e5631b2118afbe05dab3e89
Rahul Sethuram <rahul.eth@icloud.com>
2024-03-29 15:08:29 +0400

    feat: update liquidator

--- packages/sdk/src/IonicSdk/index.ts
+++ packages/sdk/src/IonicSdk/index.ts
@@ -142,9 +142,7 @@ export class IonicBase {
       ) as PoolLensSecondary,
       IonicLiquidator: new Contract(
         // remove this once IonicLiquidator is deployed
-        this.chainId == 34443
-          ? this.chainDeployment.IonicUniV3Liquidator.address
-          : this.chainDeployment.IonicLiquidator.address,
+        this.chainDeployment.IonicLiquidator.address,
         IonicLiquidatorArtifact.abi,
         this.provider
       ) as ILiquidator,
