3e3bfdfc46c6e3e835c85896d9fb73cbd5a472b6
k.zlatarov <k.zlatarov@dcvx.io>
2024-04-10 10:58:37 +0300

    Add base of leverage

--- packages/ui/app/_components/Leverage.tsx
+++ packages/ui/app/_components/Leverage.tsx
@@ -8,6 +8,7 @@ import Amount from './popup/Amount';
 
 import { useMaxSupplyAmount } from '@ui/hooks/useMaxSupplyAmount';
 import type { MarketData, PoolData } from '@ui/types/TokensDataMap';
+import Range from './Range';
 
 export type LeverageProps = {
   marketData: PoolData;
@@ -39,11 +40,8 @@ export default function Leverage({ marketData }: LeverageProps) {
       ).toString(),
     [borrowToFundingRatio, currentLeverage, fundingAmount]
   );
-  const { data: maxSupplyAmount } = useMaxSupplyAmount(
-    selectedFundingAsset,
-    marketData.comptroller,
-    chainId
-  );
+  const { data: maxSupplyAmount, isLoading: isLoadingMaxSupplyAmount } =
+    useMaxSupplyAmount(selectedFundingAsset, marketData.comptroller, chainId);
 
   return (
     <div>
@@ -65,6 +63,7 @@ export default function Leverage({ marketData }: LeverageProps) {
         amount={fundingAmount}
         availableAssets={marketData.assets}
         handleInput={(val?: string) => setFundingAmount(val)}
+        isLoading={isLoadingMaxSupplyAmount}
         mainText="Funding"
         max={formatUnits(
           maxSupplyAmount?.bigNumber ?? '0',
@@ -74,6 +73,89 @@ export default function Leverage({ marketData }: LeverageProps) {
         setSelectedAsset={(asset: MarketData) => setSelectedFundingAsset(asset)}
         symbol={selectedFundingAsset.underlyingSymbol}
       />
+
+      <div className="flex items-center text-center text-white/50 mt-2">
+        <div className="mr-6 text-sm">
+          LEVERAGE
+          <div className="text-lg font-bold">{currentLeverage.toFixed(1)}</div>
+        </div>
+
+        <div className="w-full">
+          <div className="relative h-[20px] mb-2 text-xs md:text-sm">
+            {['1x', '2x', '3x', '4x', '5x', '6x', '7x', '8x', '9x', '10x'].map(
+              (label, i) => (
+                <span
+                  className={`absolute top-0 cursor-pointer translate-x-[-50%] ${
+                    currentLeverage === i + 1 && 'text-accent'
+                  }`}
+                  key={`label-${label}`}
+                  onClick={() => setCurrentLeverage(i + 1)}
+                  style={{ left: `${(i / 9) * 100}%` }}
+                >
+                  {label}
+                </span>
+              )
+            )}
+          </div>
+
+          <Range
+            currentValue={currentLeverage}
+            max={10}
+            min={1}
+            setCurrentValue={(val: number) => setCurrentLeverage(val)}
+            step={1}
+          />
+
+          <div className="flex justify-between pt-2 text-white/50 text-xs">
+            <span>{'<'} Repay</span>
+
+            <span>Borrow {'>'}</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="separator" />
+
+      <div
+        className={`flex w-full items-center justify-between mb-2 text-xs text-sm text-white/50 `}
+      >
+        <span className={``}>POSITION VALUE</span>
+        <span className={`font-bold pl-2 text-white`}>$0.00</span>
+      </div>
+
+      <div
+        className={`flex w-full items-center justify-between mb-2 text-xs text-sm text-white/50 `}
+      >
+        <span className={``}>NET APR</span>
+        <span className={`font-bold pl-2 text-white`}>$0.00</span>
+      </div>
+
+      <div
+        className={`flex w-full items-center justify-between mb-2 text-xs text-sm text-white/50 `}
+      >
+        <span className={``}>ANNUAL YIELD</span>
+        <span className={`font-bold pl-2 text-white`}>$0.00</span>
+      </div>
+
+      <div
+        className={`flex w-full items-center justify-between mb-2 text-xs text-sm text-white/50 `}
+      >
+        <span className={``}>COLLATERAL APR</span>
+        <span className={`font-bold pl-2 text-white`}>$0.00</span>
+      </div>
+
+      <div
+        className={`flex w-full items-center justify-between mb-2 text-xs text-sm text-white/50 `}
+      >
+        <span className={``}>HEALTH RATIO</span>
+        <span className={`font-bold pl-2 text-white`}>$0.00</span>
+      </div>
+
+      <div className="separator" />
+
+      <div className="text-center">
+        <button className="btn-green">OPEN POSITION</button>
+      </div>
     </div>
   );
 }
