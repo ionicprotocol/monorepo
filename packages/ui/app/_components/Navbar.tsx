f87b68282d9030a125829908ef9d5862935b0782
Rahul Sethuram <rahul.eth@icloud.com>
2024-04-26 17:26:49 +0400

    chore: remove new

--- packages/ui/app/_components/Navbar.tsx
+++ packages/ui/app/_components/Navbar.tsx
@@ -65,9 +65,6 @@ export default function Navbar() {
             className="relative mb-2 md:mb-0"
             href={'/points'}
           >
-            <span className="absolute px-[5px] top-[90%] right-[50%] translate-x-1/2 bg-accent rounded-lg text-xxs text-darkone whitespace-nowrap	">
-              NEW!
-            </span>
             <p
               className={`${
                 pathname == '/points' ? 'text-accent' : null
@@ -80,9 +77,6 @@ export default function Navbar() {
             className="relative mb-2 md:mb-0"
             href={'/dashboard'}
           >
-            <span className="absolute px-[5px] top-[90%] right-[50%] translate-x-1/2 bg-accent rounded-lg text-xxs text-darkone whitespace-nowrap	">
-              NEW!
-            </span>
             <p
               className={`${
                 pathname == '/dashboard' ? 'text-accent' : null
