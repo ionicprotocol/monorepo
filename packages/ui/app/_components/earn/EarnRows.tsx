ffe749ea56c18d647bca1b49857d40829f4e4ae8
shikhar360 <sssonishikhar360@gmail.com>
2024-04-26 13:32:48 +0530

    earn tab completed

--- packages/ui/app/_components/earn/EarnRows.tsx
+++ packages/ui/app/_components/earn/EarnRows.tsx
@@ -1,25 +1,88 @@
 'use client';
+
+import Link from "next/link";
+
 interface IEarnRow {
-  apr: string; 
-  asset: string; //name of the asset
-  assetImg: string; //or we can ommit this by naming the image name same to the asset name
-  network: string;
-  protocol: string;
-  protocolImg: string;
-  tvl: string;
+  apr?: number | string;
+  asset?: string[]; //name of the asset in uppercase array
+  assetImg?: string; //or we can ommit this by naming the image name same to the asset name
+  network?: string;
+  protocol?: string;
+  protocolImg?: string;
+  tvl?: number;
 }
-export default function EarnRows() {
+
+export default function EarnRows({
+  asset = ['ionUSDC', 'ionUSDT'],
+  protocol = 'Balancer',
+  network = 'MODE',
+  apr = 5,
+  tvl = 1200
+}: IEarnRow) {
+  // const ;
   return (
-    <div className=" w-full hover:bg-graylite transition-all duration-200 ease-linear bg-grayUnselect rounded-xl mb-3 px-2  gap-x-1 lg:grid  grid-cols-13  py-4 text-sm text-white lg:text-center items-center relative">
-      <p className="col-span-3">
-        <span>USDC</span>
+    <div className=" w-full hover:bg-graylite transition-all duration-200 ease-linear bg-grayUnselect rounded-xl my-3 px-2 gap-x-1 md:grid  grid-cols-13  py-4 text-sm text-white lg:text-center items-center relative flex flex-col ">
+      <div className="col-span-3 w-full flex justify-between md:justify-center  gap-x-2 ">
+        <span className="text-white/40 text-xs font-semibold md:hidden">
+          ASSET
+        </span>
+        <div className={` flex ml-auto md:ml-0 `}>
+          {asset.map((coin: any, idx: number) => (
+            <img
+              src={`/img/logo/${coin}.png`}
+              alt="logos"
+              key={idx}
+              className={` w-5 h-5  top-0 left-0 ${
+                idx !== 0 && ' -translate-x-1'
+              } `}
+            />
+          ))}
+        </div>
+        <div>
+          {asset.map((val: any, idx: number) => (
+            <span className="text-center">
+              {idx !== 0 && '/'}
+              {val}
+            </span>
+          ))}
+        </div>
+        {/* <OverlayingAssetImg /> */}
+      </div>
+      <p className="col-span-2 w-full flex justify-between md:justify-center gap-x-2 ">
+        <span className="text-white/40 text-xs font-semibold md:hidden">
+          PROTOCOL
+        </span>
+        {protocol}
       </p>
-      <h1 className="col-span-2"> Protocol</h1>
-      <h1 className="col-span-1"> Network</h1>
-      <h1 className="col-span-1"> dskfhjkapr</h1>
-      <h1 className="col-span-1"> tvl</h1>
+      <h1 className="col-span-1 w-full flex justify-between md:justify-center gap-x-2">
+        <span className="text-white/40 text-xs font-semibold md:hidden">
+          NETWORK
+        </span>
+        <img
+          src={`/img/logo/${network}.png`}
+          alt="logos"
+          className={` w-5 h-5  md:mx-auto ml-auto top-0 left-0 `}
+        />
+      </h1>
+      <h1 className="col-span-1 w-full flex justify-between md:justify-center gap-x-2">
+        <span className="text-white/40 text-xs font-semibold md:hidden">
+          APR
+        </span>
+        {apr}%
+      </h1>
+      <h1 className="col-span-1 w-full flex justify-between md:justify-center gap-x-2">
+        <span className="text-white/40 text-xs font-semibold md:hidden">
+          TVL
+        </span>
+        ${+tvl.toFixed(4)}
+      </h1>
       <h1 className="col-span-3"> </h1>
-      <h1 className="col-span-2"> Deposite button</h1>
+      <Link className="col-span-2 w-max bg-accent text-darkone rounded-xl py-2 px-6 font-semibold cursor-pointer mx-auto"
+      href={'https://app.steer.finance/vault/0x17694615caba46ef765a3673fa488e04332b522a'}
+      target="_blank"
+      >
+        DEPOSIT
+      </Link>
     </div>
   );
 }
