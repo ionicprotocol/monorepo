5f5a971f7a2544289429c950f13d7ae689d7f33c
shikhar360 <sssonishikhar360@gmail.com>
2024-05-15 21:39:35 +0530

    added confetti + improved ui

--- packages/ui/app/claim/page.tsx
+++ packages/ui/app/claim/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from 'react';
+import Confetti from 'react-confetti';
 import { formatEther } from 'viem';
 import { useAccount, useSignMessage } from 'wagmi';
 
@@ -88,7 +89,7 @@ export default function Claim() {
     >
       <div className={`flex w-full  transition-all duration-500 ease-out `}>
         <div className="min-w-full flex items-center justify-between  md:px-8 px-2 py-4 ">
-          <div className="md:text-5xl text-lg md:m-8 m-2 tracking-wider md:gap-y-3 gap-y-1 flex flex-col md:leading-10 leading-6 ">
+          <div className="md:text-5xl text-lg md:m-8 m-2 tracking-wide md:gap-y-3 gap-y-1 flex flex-col md:leading-10 leading-6 ">
             <p>Welcome to the </p> <p>$ION Airdrop </p>
             <button
               className={`md:w-52 w-max  bg-accent text-darkone rounded-lg py-2 px-6  cursor-pointer text-sm md:mt-4 mt-2`}
@@ -113,41 +114,57 @@ export default function Claim() {
         <div
           className={`w-full bg-black/40 backdrop-blur-md z-50 flex items-center justify-center min-h-screen fixed top-0 left-0`}
         >
+          {eligibility === true && popup === true && !claimed && (
+            <Confetti
+              gravity={0.06}
+              height={1080}
+              width={1420}
+              wind={0.01}
+            />
+          )}
           <div
-            className={`md:w-[30%] w-[70%] bg-grayone py-4 px-2 rounded-xl  flex flex-col items-center justify-center min-h-[20vh] `}
+            className={`md:w-[30%] w-[70%] bg-grayone py-4 px-2 rounded-xl  flex flex-col items-center justify-center min-h-[20vh] relative`}
           >
+            <img
+              alt="close"
+              className={`absolute top-4 right-4 h-5 w-5 cursor-pointer z-20 opacity-70`}
+              onClick={() => setPopup(false)}
+              src="/img/assets/close.png"
+            />
             <ResultHandler isLoading={loading}>
               {eligibility === null && (
                 <div className="w-full px-2 mt-2 relative  items-center justify-center gap-x-2  cursor-pointer ">
-                  <p className="w-full text-lg">Check Eligibility</p>
+                  <p className="w-full tracking-wide mb-4">Check Eligibility</p>
 
-                  <div className={`bg-accent w-max my-2 rounded-xl text-black`}>
+                  <div
+                    className={`bg-accent w-max my-2 rounded-xl overflow-hidden text-black`}
+                  >
                     <ConnectButton />
                   </div>
-                  <button
-                    className={`md:w-52 w-max  bg-accent text-darkone rounded-lg py-2 px-6  cursor-pointer text-sm mb-4 font-semibold`}
-                    onClick={() => checkEligibility()}
-                  >
-                    Check
-                  </button>
 
-                  <p className=" text-sm text-white/60 mb-3 ">
+                  <p className=" text-sm text-white/60 mt-3 mb-5 ">
                     $ION airdrop will be send out to the confirmed eligible
                     wallet addresses by the Ionic Team. Once you sign in with
                     your wallet, no further actions needs to be taken
                   </p>
+                  <button
+                    className={` w-full  bg-accent text-darkone rounded-lg py-2 px-6  cursor-pointer text-sm  tracking-wide `}
+                    onClick={() => checkEligibility()}
+                  >
+                    Check
+                  </button>
                 </div>
               )}
 
               {eligibility && eligibility === true ? (
-                <div className="flex flex-col my-auto items-center justify-center">
+                <div className="flex flex-col my-auto items-center justify-center relative px-2">
                   <img
                     alt={`Image `}
                     className="md:w-6 w-6 mb-2 "
                     key={'id'}
                     src={'/img/success.png'}
                   />
-                  <span className="text-center pb-6">
+                  <span className="text-center pb-6 font-semibold">
                     Congratulations, you are eligible for the airdrop! Your
                     allocation:
                   </span>
@@ -157,7 +174,7 @@ export default function Claim() {
                     ).toLocaleString()}{' '}
                     $ION
                   </span>
-                  <span className="text-center pb-6">
+                  <span className="text-center text-white/60 text-sm pb-4">
                     The first tranche of your $ION airdrop allocation (
                     {Math.floor(
                       Number(
@@ -172,7 +189,7 @@ export default function Claim() {
                     months. Details on vesting and vesting reduction will follow
                     soon.
                   </span>
-                  <span className="text-center pb-12">
+                  <span className="text-center pb-5">
                     Press the button below to sign a message and prove ownership
                     of your address. After that, no further actions needed.
                   </span>
@@ -200,16 +217,9 @@ export default function Claim() {
                 disabled={claimed}
                 onClick={() => claimTokens()}
               >
-                {claimed ? 'Claimed' : 'Claim Tokens'}
+                {claimed ? 'Claimed' : 'Sign with the Wallet'}
               </button>
-            ) : (
-              <button
-                className={`mt-auto w-full bg-accent text-darkone rounded-lg py-2 px-6  cursor-pointer text-sm `}
-                onClick={() => setPopup(false)}
-              >
-                Back to Claim
-              </button>
-            )}
+            ) : null}
           </div>
         </div>
       )}
