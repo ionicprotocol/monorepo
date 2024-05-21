4884ec142490ba04e3504bcebe30335724ba5bde
amish777 <141118879+amish777@users.noreply.github.com>
2024-05-21 13:49:58 +0530

    Update mode.ts

--- packages/bots/pyth-updater/src/config/mode.ts
+++ packages/bots/pyth-updater/src/config/mode.ts
@@ -5,13 +5,6 @@ import { pythConfig as commonPythConfig } from './common';
 export const pythConfig: PythAssetConfig[] = [
   ...commonPythConfig,
   {
-    // price feed for WETH
-    priceId: '0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace',
-    configRefreshRateInSeconds: 3600,
-    validTimePeriodSeconds: 86400, // 24 hrs
-    deviationThresholdBps: 100, // 1%
-  },
-  {
     // price feed for USDC
     priceId: '0xeaa020c61cc479712813461ce153894a96a6c00b21ed0cfc2798d1f9a9e9c94a',
     configRefreshRateInSeconds: 3600,
@@ -25,13 +18,20 @@ export const pythConfig: PythAssetConfig[] = [
     validTimePeriodSeconds: 86400, // 24 hrs
     deviationThresholdBps: 100, // 1%
   },
-  // {
-  //   // price feed for WBTC
-  //   priceId: '0xc9d8b075a5c69303365ae23633d4e085199bf5c520a3b90fed1322a0342ffc33',
-  //   configRefreshRateInSeconds: 3600,
-  //   validTimePeriodSeconds: 86400, // 24 hrs
-  //   deviationThresholdBps: 100, // 1%
-  // },
+  {
+    // price feed for WBTC
+    priceId: '0xc9d8b075a5c69303365ae23633d4e085199bf5c520a3b90fed1322a0342ffc33',
+    configRefreshRateInSeconds: 3600,
+    validTimePeriodSeconds: 86400, // 24 hrs
+    deviationThresholdBps: 100, // 1%
+  },
+  {
+    // price feed for MODE
+    priceId: '0x6665073f5bc307b97e68654ff11f3d8875abd6181855814d23ab01b8085c0906',
+    configRefreshRateInSeconds: 3600,
+    validTimePeriodSeconds: 86400, // 24 hrs
+    deviationThresholdBps: 100, // 1%
+  }
   // {
   //   // price feed for UNI
   //   priceId: '0x78d185a741d07edb3412b09008b7c5cfb9bbbd7d568bf00ba737b456ba171501',
