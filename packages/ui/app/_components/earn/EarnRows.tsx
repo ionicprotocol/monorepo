206dc2cc2d72c64505c3b7194b1731bfd4719f3f
Rahul Sethuram <rahul.eth@icloud.com>
2024-05-21 17:58:49 +0400

    feat: new earn pool

--- packages/ui/app/_components/earn/EarnRows.tsx
+++ packages/ui/app/_components/earn/EarnRows.tsx
@@ -2,98 +2,103 @@
 
 import Link from 'next/link';
 
-interface IEarnRow {
+export type EarnRow = {
   apr?: number | string;
-  asset?: string[]; //name of the asset in uppercase array
-  assetImg?: string; //or we can ommit this by naming the image name same to the asset name
+  asset: string[]; //name of the asset in uppercase array
+  getApr: () => Promise<number>;
+  getTvl: () => Promise<number>;
+  link: string;
   network?: string;
   protocol?: string;
-  protocolImg?: string;
-  tvl?: number;
-}
+  tvl: number;
+};
 
-export default function EarnRows({
-  asset = ['ionUSDC', 'ionUSDT'],
-  apr = 5,
-  network = 'MODE',
-  protocol = 'Steer',
-  tvl = 1200
-}: IEarnRow) {
-  // const ;
+type EarnRowsParams = {
+  rows: EarnRow[];
+};
+export default function EarnRows({ rows }: EarnRowsParams) {
   return (
-    <div className=" w-full hover:bg-graylite transition-all duration-200 ease-linear bg-grayUnselect rounded-xl my-3 px-2 gap-x-1 md:grid  grid-cols-13  py-4 text-sm text-white lg:text-center items-center relative flex flex-col ">
-      <div className="col-span-3 w-full flex justify-between md:justify-center  gap-x-2 ">
-        <span className="text-white/40 text-xs font-semibold md:hidden">
-          ASSET
-        </span>
-        <div className={` flex ml-auto md:ml-0 `}>
-          {asset.map((coin: string, idx: number) => (
+    <>
+      {rows.map(({ apr, asset, network, protocol, tvl, link }, idx) => (
+        <div
+          className=" w-full hover:bg-graylite transition-all duration-200 ease-linear bg-grayUnselect rounded-xl my-3 px-2 gap-x-1 md:grid  grid-cols-13  py-4 text-sm text-white lg:text-center items-center relative flex flex-col "
+          key={idx}
+        >
+          <div className="col-span-3 w-full flex justify-between md:justify-center  gap-x-2 ">
+            <span className="text-white/40 text-xs font-semibold md:hidden">
+              ASSET
+            </span>
+            <div className={` flex ml-auto md:ml-0 `}>
+              {asset.map((coin: string, idx: number) => (
+                <img
+                  alt="logos"
+                  className={` w-5 h-5  top-0 left-0 ${
+                    idx !== 0 && ' -translate-x-1'
+                  } `}
+                  key={idx}
+                  src={`/img/symbols/32/color/${coin}.png`}
+                />
+              ))}
+            </div>
+            <div>
+              {asset.map((val: string, idx: number) => (
+                <span
+                  className="text-center"
+                  key={idx}
+                >
+                  {idx !== 0 && '/'}
+                  {val}
+                </span>
+              ))}
+            </div>
+            {/* <OverlayingAssetImg /> */}
+          </div>
+          <p className="col-span-2 w-full flex justify-between md:justify-center gap-x-2 ">
+            <span className="text-white/40 text-xs font-semibold md:hidden">
+              PROTOCOL
+            </span>
+            {protocol}
+          </p>
+          <div className="col-span-1 w-full flex justify-between md:justify-center gap-x-2">
+            <span className="text-white/40 text-xs font-semibold md:hidden">
+              NETWORK
+            </span>
             <img
               alt="logos"
-              className={` w-5 h-5  top-0 left-0 ${
-                idx !== 0 && ' -translate-x-1'
-              } `}
-              key={idx}
-              src={`/img/logo/${coin}.png`}
+              className={` w-5 h-5  md:mx-auto ml-auto top-0 left-0 `}
+              src={`/img/logo/${network}.png`}
             />
-          ))}
-        </div>
-        <div>
-          {asset.map((val: string, idx: number) => (
-            <span
-              className="text-center"
-              key={idx}
-            >
-              {idx !== 0 && '/'}
-              {val}
+          </div>
+          <div className="col-span-1 w-full flex justify-between md:justify-center gap-x-2">
+            <span className="text-white/40 text-xs font-semibold md:hidden">
+              APR
             </span>
-          ))}
+            {apr && Number(apr) > 0 ? apr : '-'}%
+          </div>
+          <div className="col-span-1 w-full flex justify-between md:justify-center gap-x-2">
+            <span className="text-white/40 text-xs font-semibold md:hidden">
+              TVL
+            </span>
+            $
+            {tvl > 0
+              ? tvl.toLocaleString(undefined, { maximumFractionDigits: 2 })
+              : '-'}
+          </div>
+          <div className="col-span-3"> </div>
+          <Link
+            className="col-span-2 w-max bg-accent text-darkone rounded-xl py-2 px-6 font-semibold cursor-pointer mx-auto flex items-center justify-center gap-2"
+            href={link}
+            target="_blank"
+          >
+            <span>DEPOSIT</span>
+            <img
+              alt="external-link"
+              className={`w-3 h-3`}
+              src="https://img.icons8.com/material-outlined/24/external-link.png"
+            />
+          </Link>
         </div>
-        {/* <OverlayingAssetImg /> */}
-      </div>
-      <p className="col-span-2 w-full flex justify-between md:justify-center gap-x-2 ">
-        <span className="text-white/40 text-xs font-semibold md:hidden">
-          PROTOCOL
-        </span>
-        {protocol}
-      </p>
-      <div className="col-span-1 w-full flex justify-between md:justify-center gap-x-2">
-        <span className="text-white/40 text-xs font-semibold md:hidden">
-          NETWORK
-        </span>
-        <img
-          alt="logos"
-          className={` w-5 h-5  md:mx-auto ml-auto top-0 left-0 `}
-          src={`/img/logo/${network}.png`}
-        />
-      </div>
-      <div className="col-span-1 w-full flex justify-between md:justify-center gap-x-2">
-        <span className="text-white/40 text-xs font-semibold md:hidden">
-          APR
-        </span>
-        {apr}%
-      </div>
-      <div className="col-span-1 w-full flex justify-between md:justify-center gap-x-2">
-        <span className="text-white/40 text-xs font-semibold md:hidden">
-          TVL
-        </span>
-        ${+tvl.toFixed(4)}
-      </div>
-      <div className="col-span-3"> </div>
-      <Link
-        className="col-span-2 w-max bg-accent text-darkone rounded-xl py-2 px-6 font-semibold cursor-pointer mx-auto flex items-center justify-center gap-2"
-        href={
-          'https://app.steer.finance/vault/0x17694615caba46ef765a3673fa488e04332b522a/34443'
-        }
-        target="_blank"
-      >
-        <span>DEPOSIT</span>
-        <img
-          alt="external-link"
-          className={`w-3 h-3`}
-          src="https://img.icons8.com/material-outlined/24/external-link.png"
-        />
-      </Link>
-    </div>
+      ))}
+    </>
   );
 }
--- packages/ui/app/earn/page.tsx
+++ packages/ui/app/earn/page.tsx
@@ -2,38 +2,70 @@
 
 import { useEffect, useState } from 'react';
 
-import EarnRows from '../_components/earn/EarnRows';
+import EarnRows, { EarnRow } from '../_components/earn/EarnRows';
 
-export default function Lend() {
-  const [apr, setApr] = useState<number>(0);
-  const [tvl, setTvl] = useState<number>(0);
+export default function Earn() {
+  const [rows, setRows] = useState<EarnRow[]>([
+    {
+      apr: 0,
+      asset: ['ION', 'WETH'],
+      getApr: () => Promise.resolve(0),
+      getTvl: () => Promise.resolve(0),
+      link: 'https://velodrome.finance/deposit?token0=0x18470019bF0E94611f15852F7e93cf5D65BC34CA&token1=0x4200000000000000000000000000000000000006&type=-1',
+      network: 'mode',
+      protocol: 'Velodrome',
+      tvl: 0
+    },
+    {
+      apr: 0,
+      asset: ['ionUSDC', 'ionUSDT'],
+      getApr: async () => {
+        try {
+          const response = await fetch(
+            'https://api.steer.finance/pool/fee-apr?address=0x17694615caba46ef765a3673fa488e04332b522a&chain=34443&interval=604800'
+          );
+          if (!response.ok) {
+            throw new Error(`HTTP error: Status ${response.status}`);
+          }
+          const val = await response.json();
+          return val?.apr ?? 0;
+        } catch (err) {
+          console.error(err);
+        }
+      },
+      getTvl: async () => {
+        try {
+          const response = await fetch(
+            'https://api.steer.finance/pool/lp/value?chain=34443&address=0x17694615caba46ef765a3673fa488e04332b522a'
+          );
+          if (!response.ok) {
+            throw new Error(`HTTP error: Status ${response.status}`);
+          }
+          const val = await response.json();
+          return val?.tvl ?? 0;
+        } catch (err) {
+          console.error(err);
+        }
+      },
+      link: 'https://app.steer.finance/vault/0x17694615caba46ef765a3673fa488e04332b522a/34443',
+      network: 'mode',
+      protocol: 'Steer',
+      tvl: 0
+    }
+  ]);
 
   useEffect(() => {
-    async function getApr() {
-      try {
-        const response = await fetch(
-          'https://api.steer.finance/pool/fee-apr?address=0x17694615caba46ef765a3673fa488e04332b522a&chain=34443&interval=604800'
-        );
-        if (!response.ok) {
-          throw new Error(`HTTP error: Status ${response.status}`);
-        }
-        const val = await response.json();
-        setApr(val?.apr);
-
-        const tvlresponse = await fetch(
-          'https://api.steer.finance/pool/lp/value?chain=34443&address=0x17694615caba46ef765a3673fa488e04332b522a'
-        );
-        if (!tvlresponse.ok) {
-          throw new Error(`HTTP error: Status ${tvlresponse.status}`);
-        }
-        const tvlval = await tvlresponse.json();
-        setTvl(tvlval?.tvl);
-      } catch (err) {
-        // console.log(err);
-      }
-    }
-    getApr();
-  }, []);
+    const populateVals = async () => {
+      await Promise.all(
+        rows.map(async (row) => {
+          row.apr = await row.getApr();
+          row.tvl = await row.getTvl();
+        })
+      );
+      setRows([...rows]);
+    };
+    populateVals();
+  }, [rows]);
   return (
     <>
       <h1 className="mb-4 text-center text-white/80 ">
@@ -54,10 +86,7 @@ export default function Lend() {
         </div>
 
         {/* this will get mapped out in future with the possible api data structure mentioned below */}
-        <EarnRows
-          apr={apr}
-          tvl={tvl}
-        />
+        <EarnRows rows={rows} />
       </div>
     </>
   );
