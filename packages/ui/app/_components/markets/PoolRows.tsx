9db760ca38e43a843b2c80d0f49527014cd6fc80
Rahul Sethuram <rahul.eth@icloud.com>
2024-06-03 18:23:31 +0200

    fix: turtle link

--- packages/ui/app/_components/markets/PoolRows.tsx
+++ packages/ui/app/_components/markets/PoolRows.tsx
@@ -345,9 +345,18 @@ const PoolRows = ({
           >
             + POINTS <i className="popover-hint">i</i>
           </span>
-          <span className="text-darkone bg-accent rounded-lg w-20 ml-1 lg:ml-0 text-center mt-1">
-            + TURTLE <i className="popover-hint">i</i>
-          </span>
+          <a
+            className="text-darkone bg-accent rounded-lg w-20 ml-1 lg:ml-0 text-center mt-1 flex items-center justify-center gap-1"
+            href="https://turtle.club/dashboard/?ref=IONIC"
+            target="_blank"
+          >
+            + TURTLE{' '}
+            <img
+              alt="external-link"
+              className={`w-3 h-3`}
+              src="https://img.icons8.com/material-outlined/24/external-link.png"
+            />
+          </a>
           <div
             className={`popover absolute w-[170px] top-full p-2 mt-1 border ${
               chainColors(dropdownSelectedChain).border
@@ -523,9 +532,18 @@ const PoolRows = ({
           >
             + POINTS <i className="popover-hint">i</i>
           </span>
-          <span className="text-darkone bg-accent rounded-lg w-20 ml-1 lg:ml-0 text-center mt-1">
-            + TURTLE <i className="popover-hint">i</i>
-          </span>
+          <a
+            className="text-darkone bg-accent rounded-lg w-20 ml-1 lg:ml-0 text-center mt-1 flex items-center justify-center gap-1"
+            href="https://turtle.club/dashboard/?ref=IONIC"
+            target="_blank"
+          >
+            + TURTLE{' '}
+            <img
+              alt="external-link"
+              className={`w-3 h-3`}
+              src="https://img.icons8.com/material-outlined/24/external-link.png"
+            />
+          </a>
           <div
             className={`popover absolute w-[170px] top-full p-2 mt-1 border ${
               chainColors(dropdownSelectedChain).border
