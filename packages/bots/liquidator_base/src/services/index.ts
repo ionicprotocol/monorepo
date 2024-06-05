5cda00def073c457a8374d71f98568b26c98e412
amish kohli <amishkohli@192.168.1.4>
2024-06-05 22:35:46 +0530

    fixed base bot

--- packages/bots/liquidator_base/src/services/index.ts
+++
@@ -1,3 +0,0 @@
-export { DiscordService } from "./discord";
-export { Liquidator } from "./liquidator";
-export { EmailService } from "./email";
--- packages/bots/liquidator_base/src/utils/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as setUpSdk } from "./setUpSdk";
-export { fetchGasLimitForTransaction } from "./utils";
-export { default as approveTokensToSafeLiquidator } from "./approveTokensToSafeLiquidator";
