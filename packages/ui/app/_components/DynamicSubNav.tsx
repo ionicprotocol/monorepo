a96b88681c56c47673aa49b3b8b9f7fd857acbb5
Rahul Sethuram <rahul.eth@icloud.com>
2024-06-06 16:38:51 +0400

    fix: banner

--- packages/ui/app/_components/DynamicSubNav.tsx
+++ packages/ui/app/_components/DynamicSubNav.tsx
@@ -16,9 +16,8 @@ function DynamicSubNav() {
         +chainId === base.id ? 'bg-blue-600 text-white' : 'bg-lime text-darkone'
       } absolute w-full top-full left-0 text-center p-2 text-sm font-medium`}
     >
-      Hello, {+chainId === base.id ? 'Base' : 'Mode'}! Ionic is open for lending
-      and borrowing! Supply assets to earn Ionic points. Borrow to earn
-      multiplied points!
+      Season 2 is LIVE - New multipliers, new ways to earn points! See your
+      Season 1 $ION eligibility on the Claim page (Open till 6th of June).
     </div>
   );
 }
