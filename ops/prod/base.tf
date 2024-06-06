c3d3bce9315474f9a9688bd7e4dee5f1cc3463b5
amish kohli <amishkohli@192.168.1.4>
2024-06-06 11:47:12 +0530

    fixed base bot

--- ops/prod/base.tf
+++ ops/prod/base.tf
@@ -6,7 +6,7 @@ locals {
 
 module "base_mainnet_liquidation_rpc_0" {
   source              = "../modules/lambda"
-  ecr_repository_name = local.liquidator_base_ecr_repository_name
+  ecr_repository_name = local.liquidator_ecr_repository_name
   docker_image_tag    = var.bots_image_tag
   container_family    = "liquidator-base-rpc-0"
   environment         = "mainnet"
