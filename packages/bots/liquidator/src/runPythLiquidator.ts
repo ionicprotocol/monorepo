cc5abd5c916969d91ad758fb63ee8c9ef98dfd92
Jourdan Dunkley <chasejamaica@yahoo.com>
2024-06-05 19:26:26 -0500

    lint

--- packages/bots/liquidator/src/runPythLiquidator.ts
+++ packages/bots/liquidator/src/runPythLiquidator.ts
@@ -1,10 +1,12 @@
 import { JsonRpcProvider } from "@ethersproject/providers";
-import { BotType } from "@ionicprotocol/sdk/dist/cjs/src/modules/liquidation/utils";
-import { PythLiquidatablePool } from "@ionicprotocol/sdk/dist/cjs/src/modules/liquidation/utils";
 import { Client, OpportunityParams } from "@pythnetwork/express-relay-evm-js";
-import { encodeAbiParameters, encodeFunctionData } from "viem";
+
+import { BotType, PythLiquidatablePool } from "@ionicprotocol/sdk/dist/cjs/src/modules/liquidation/utils";
 import IonicLiquidatorABI from "../../../sdk/artifacts/IonicLiquidator.sol/IonicLiquidator.json";
+
+import { encodeAbiParameters, encodeFunctionData } from "viem";
 import config from "./config";
+
 import { logger } from "./logger";
 import { Liquidator } from "./services";
 import { setUpSdk } from "./utils";
