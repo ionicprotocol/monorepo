cfd6aa72c6c40291f48cf21e047ff971ebc594fc
amish kohli <amishkohli@192.168.1.4>
2024-06-07 19:26:02 +0530

    build scheduler changed for testing

--- packages/functions/src/functions/asset-total-apy-history-base.ts
+++ packages/functions/src/functions/asset-total-apy-history-base.ts
@@ -1,4 +1,4 @@
 import { SupportedChains } from '@ionicprotocol/types';
-import { createAssetTotalApyHandler } from '../controllers/asset-total-apy-history';
+import { createAssetTotalApyHandler } from '../controllers/asset-total-history-apy';
 
 export const handler = createAssetTotalApyHandler(SupportedChains.base);
