cdc43ec5f47d3e94c246a763f6d7b1d03f135093
Rahul Sethuram <rahul.eth@icloud.com>
2024-07-01 12:25:25 +0400

    fix: build

--- packages/security/src/oracle/constants/index.ts
+++ packages/security/src/oracle/constants/index.ts
@@ -1,14 +1,3 @@
-import { arbitrum, bsc, polygon } from "@ionicprotocol/chains";
+export const chainLinkOracleAssetMappings = {};
 
-import { uniswapV3OracleAssetMappings as uniswapV3OracleAssetMappingsArbitrum } from "./arbitrum";
-import { chainLinkOracleAssetMappings as chainLinkOracleAssetMappingsBsc } from "./bsc";
-import { chainLinkOracleAssetMappings as chainLinkOracleAssetMappingsPolygon } from "./polygon";
-
-export const chainLinkOracleAssetMappings = {
-  [bsc.chainId]: chainLinkOracleAssetMappingsBsc,
-  [polygon.chainId]: chainLinkOracleAssetMappingsPolygon,
-};
-
-export const uniswapV3OracleAssetMappings = {
-  [arbitrum.chainId]: uniswapV3OracleAssetMappingsArbitrum,
-};
+export const uniswapV3OracleAssetMappings = {};
