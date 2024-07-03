11e6f87b795c3c7cdf7d9650656469245598a650
Rahul Sethuram <rahul.eth@icloud.com>
2024-07-03 11:20:55 +0400

    fix: build

--- packages/functions/src/data/flywheels.ts
+++ packages/functions/src/data/flywheels.ts
@@ -1,14 +1,4 @@
 import { SupportedChains } from '@ionicprotocol/types';
 
-export const bscFlywheels = [
-  '0xD09b27B5EA296A901D2113c374A28f02Cd46010D',
-  '0x851Cc0037B6923e60dC81Fa79Ac0799cC983492c', // jarvis, 2brl, DDD Flywheel
-  '0xC6431455AeE17a08D6409BdFB18c4bc73a4069E4', // jarvis, 2brl, EPX Flywheel
-];
-export const polygonFlywheels = [];
-
 type ChainToFlywheels = Partial<Record<SupportedChains, string[]>>;
-export const flywheelsOfChain: ChainToFlywheels = {
-  [SupportedChains.bsc]: bscFlywheels,
-  [SupportedChains.polygon]: polygonFlywheels,
-};
+export const flywheelsOfChain: ChainToFlywheels = {};
--- packages/functions/src/data/rpcs.ts
+++ packages/functions/src/data/rpcs.ts
@@ -3,7 +3,7 @@ import { SupportedChains } from '@ionicprotocol/types';
 type RpcUrls = Partial<Record<SupportedChains, string>>;
 
 export const rpcUrls: RpcUrls = {
-  [SupportedChains.bsc]: 'https://bsc-dataseed1.binance.org/',
-  [SupportedChains.polygon]: 'https://poly-rpc.gateway.pokt.network',
   [SupportedChains.mode]: 'https://mainnet.mode.network',
+  [SupportedChains.base]: 'https://mainnet.base.org',
+  [SupportedChains.optimism]: 'https://mainnet.optimism.io',
 };
