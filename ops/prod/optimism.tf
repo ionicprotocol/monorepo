3c92404bd7cb7441cf8e307a31622a3f1c0c2ae0
amish kohli <amishkohli@192.168.1.2>
2024-07-04 16:46:58 +0530

    optimism addition in liquidator bot

--- ops/prod/optimism.tf
+++ ops/prod/optimism.tf
@@ -1,6 +1,6 @@
 locals {
-  base_mainnet_rpc_0    = "https://mainnet.optimism.io"
-  base_mainnet_chain_id = "10"
+  optimism_mainnet_rpc_0    = "https://mainnet.optimism.io"
+  optimism_mainnet_chain_id = "10"
 }
 
 
@@ -8,12 +8,12 @@ module "optimism_mainnet_liquidation_rpc_0" {
   source              = "../modules/lambda"
   ecr_repository_name = local.liquidator_ecr_repository_name
   docker_image_tag    = var.bots_image_tag
-  container_family    = "liquidator-base-rpc-0"
+  container_family    = "liquidator-optimism-rpc-0"
   environment         = "mainnet"
-  chain_id            = local.base_mainnet_chain_id
+  chain_id            = local.optimism_mainnet_chain_id
   container_env_vars = merge(
     local.liquidation_variables,
-    { WEB3_HTTP_PROVIDER_URL = local.base_mainnet_rpc_0 }
+    { WEB3_HTTP_PROVIDER_URL = local.optimism_mainnet_rpc_0 }
   )
   schedule_expression = "rate(5 minutes)"
   timeout             = 700
