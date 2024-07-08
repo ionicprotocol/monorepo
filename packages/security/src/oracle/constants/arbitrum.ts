f8ce3f8e3bea4f0272afcb475ad17396b621ff42
Rahul Sethuram <rahul.eth@icloud.com>
2024-07-08 17:02:40 +0400

    feat: build

--- packages/security/src/oracle/constants/arbitrum.ts
+++ packages/security/src/oracle/constants/arbitrum.ts
@@ -1,9 +1,6 @@
-import { arbitrum } from "@ionicprotocol/chains";
-import { assetFilter, assetSymbols } from "@ionicprotocol/types";
-import Decimal from "decimal.js";
+import { assetSymbols } from "@ionicprotocol/types";
 
 import { ChainLinkAssetConfig } from "../scorers/chainlink/types";
-import { UniswapV3AssetConfig } from "../scorers/uniswapV3/types";
 
 export const chainLinkOracleAssetMappings: ChainLinkAssetConfig = {
   defaultValidatorNumber: 10,
@@ -28,24 +25,6 @@ export const chainLinkOracleAssetMappings: ChainLinkAssetConfig = {
     "DPX / USD": assetSymbols.DPX,
   },
 };
-
-const GMX = assetFilter(arbitrum.assets, assetSymbols.GMX);
-
-export const uniswapV3OracleAssetMappings: UniswapV3AssetConfig[] = [
-  {
-    token: {
-      address: GMX.underlying,
-      symbol: GMX.symbol,
-      decimals: GMX.decimals,
-    },
-    targetPriceImpact: new Decimal(20),
-    baseToken: arbitrum.chainAddresses.W_TOKEN,
-    fee: 10000,
-    cardinality: 10,
-    attackBlocks: 2,
-    inverted: false,
-  },
-];
 
 // 100 = 0.01%
 // 500 = 0.05%
