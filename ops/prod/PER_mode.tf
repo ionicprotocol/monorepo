06b90d879b6f6b6fd63c86d4a9ceec126c78c82c
amish kohli <amishkohli@192.168.1.3>
2024-07-11 17:47:37 +0530

    log groups permissions

--- ops/prod/PER_mode.tf
+++ ops/prod/PER_mode.tf
@@ -1,6 +1,7 @@
 # CloudWatch Log Group
 resource "aws_cloudwatch_log_group" "ecs_log_group" {
-  name              = "/ecs/${var.cluster_name}"
+  name              = "/ecs/${var.task_definition_family}"
+  retention_in_days = 7  # Adjust retention as needed
 }
 
 # ECS Cluster
@@ -24,12 +25,13 @@ resource "aws_ecs_task_definition" "perbotTaskDefinition" {
       name      = var.container_name
       image     = "058264122535.dkr.ecr.us-east-1.amazonaws.com/liquidator-pyth:${var.bots_image_tag}"
       essential = true
+
       logConfiguration = {
         logDriver = "awslogs"
         options = {
-          awslogs-group         = "/ecs/${var.cluster_name}"
+          awslogs-group         = "/ecs/${var.task_definition_family}"
           awslogs-region        = "us-east-1"
-          awslogs-stream-prefix = var.container_name
+          awslogs-stream-prefix = "ecs"
         }
       }
     }
@@ -41,20 +43,21 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   name = "ecs-task-execution-role"
   
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Effect    = "Allow"
+        Effect    = "Allow",
         Principal = {
           Service = "ecs-tasks.amazonaws.com"
-        }
+        },
         Action    = "sts:AssumeRole"
       }
     ]
   })
   
   managed_policy_arns = [
-    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
+    "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"  # Added CloudWatch Logs permissions
   ]
 }
 
