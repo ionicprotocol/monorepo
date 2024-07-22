49c432e893f49d5abd6a8b3580fe03fd43c5bc47
amish kohli <amishkohli@192.168.1.5>
2024-07-22 14:29:02 +0530

    fixing region for per bot

--- ops/PER_prod/PER_mode.tf
+++ ops/PER_prod/PER_mode.tf
@@ -24,7 +24,7 @@ resource "aws_ecs_task_definition" "perbotTaskDefinition" {
         logDriver = "awslogs"
         options = {
           "awslogs-group"         = "/ecs/${var.container_name}"
-          "awslogs-region"        = "us-east-1"
+          "awslogs-region"        = "eu-central-1"
           "awslogs-stream-prefix" = "ecs"
         }
       }
