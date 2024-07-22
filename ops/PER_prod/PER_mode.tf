b75f9847b84fe1bce6e05dddf4324db9e3e44b78
amish kohli <amishkohli@192.168.1.5>
2024-07-22 14:16:08 +0530

    fixing region for per bot

--- ops/PER_prod/PER_mode.tf
+++ ops/PER_prod/PER_mode.tf
@@ -33,7 +33,31 @@ resource "aws_ecs_task_definition" "perbotTaskDefinition" {
 }
 
 
+# IAM Role for ECS task execution
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs-task-execution-role-1"
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action    = "sts:AssumeRole"
+      }
+    ]
+  })
+  
+  # Example: Attach policies to the role
+  # Replace with actual policies as needed
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
+    "arn:aws:iam::aws:policy/AWSOpsWorksCloudWatchLogs"
 
+  ]
+}
 
 # ECS Service
 resource "aws_ecs_service" "my_ecs_service" {
