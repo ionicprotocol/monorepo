0aace43092fab56149f151303c4f2a3acb914ca6
Rahul Sethuram <rahul.eth@icloud.com>
2024-07-25 14:55:19 +0400

    fix: more testing

--- packages/sdk/tests-integration/rewards-testing.ts
+++ packages/sdk/tests-integration/rewards-testing.ts
@@ -16,11 +16,18 @@ const run = async () => {
   const publicClient = createPublicClient({ transport: http(), chain: base });
 
   const sdk = new IonicSdk(publicClient as any, walletClient, chainIdToConfig[base.id]);
-  const rewards = await sdk.getFlywheelRewardsInfoForMarket(
-    "0x327410E4D3A32EF37712e77fCB005e5327F082De",
-    "0x3d9669de9e3e98db41a1cbf6dc23446109945e3c"
+  const rewards = await sdk.getFlywheelClaimableRewardsForPool(
+    "0x05c9C6417F246600f8f5f49fcA9Ee991bfF73D13",
+    "0x1155b614971f16758C92c4890eD338C9e3ede6b7"
   );
-  console.log("rewards: ", rewards);
+  console.log("getFlywheelClaimableRewardsForPool: ", rewards);
+
+  const markets = await sdk.getFlywheelClaimableRewardsByMarkets(
+    "0x05c9C6417F246600f8f5f49fcA9Ee991bfF73D13",
+    ["0x9c2A4f9c5471fd36bE3BBd8437A33935107215A1", "0x3D9669DE9E3E98DB41A1CbF6dC23446109945E3C"],
+    "0x1155b614971f16758C92c4890eD338C9e3ede6b7"
+  );
+  console.log("getFlywheelClaimableRewardsForPool: ", markets);
 };
 
 run()
