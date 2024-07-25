e25bc4b7456925488e7ee7ec5cff8624f55973df
Rahul Sethuram <rahul.eth@icloud.com>
2024-07-25 14:36:58 +0400

    fix: base

--- packages/sdk/tests-integration/rewards-testing.ts
+++ packages/sdk/tests-integration/rewards-testing.ts
@@ -1,7 +1,7 @@
 import { chainIdToConfig } from "@ionicprotocol/chains";
 import { config as dotenvConfig } from "dotenv";
 import { createPublicClient, createWalletClient, http } from "viem";
-import { mode } from "viem/chains";
+import { base } from "viem/chains";
 
 import { IonicSdk } from "../src";
 
@@ -9,14 +9,17 @@ dotenvConfig();
 
 const run = async () => {
   const walletClient = createWalletClient({
-    chain: mode,
+    chain: base,
     transport: http()
   });
 
-  const publicClient = createPublicClient({ transport: http(), chain: mode });
+  const publicClient = createPublicClient({ transport: http(), chain: base });
 
-  const sdk = new IonicSdk(publicClient, walletClient, chainIdToConfig[mode.id]);
-  const rewards = await sdk.getFlywheelRewardsInfoForMarket("0x0", "0x0");
+  const sdk = new IonicSdk(publicClient as any, walletClient, chainIdToConfig[base.id]);
+  const rewards = await sdk.getFlywheelRewardsInfoForMarket(
+    "0x327410E4D3A32EF37712e77fCB005e5327F082De",
+    "0x3d9669de9e3e98db41a1cbf6dc23446109945e3c"
+  );
   console.log("rewards: ", rewards);
 };
 
