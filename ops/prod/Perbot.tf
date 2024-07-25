3bed98f900e2698a41b4d15d8f4d372029a40db5
amish kohli <amishkohli@192.168.1.9>
2024-07-25 17:01:42 +0530

    testing discord notification

--- ops/prod/Perbot.tf
+++ ops/prod/Perbot.tf
@@ -48,7 +48,11 @@ resource "aws_ecs_task_definition" "perbotTaskDefinition1" {
         {
           name  = "ETHEREUM_ADMIN_PRIVATE_KEY"
           value = "${var.ethereum_admin_private_key}"
-        }  
+        },
+        {
+          name  = "PER_DISCORD_WEBHOOK_URL"
+          value = "${var.per_discord_webhook_url}"
+        } 
       ] 
     }
   ])
