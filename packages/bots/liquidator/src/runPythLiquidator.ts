41dacc6f6db14b883ccc38ea9cf12271db3a1971
amish kohli <amishkohli@192.168.1.3>
2024-07-31 17:10:39 +0530

    min profit logic

--- packages/bots/liquidator/src/runPythLiquidator.ts
+++ packages/bots/liquidator/src/runPythLiquidator.ts
@@ -1,120 +1,149 @@
 import { Client, OpportunityParams } from "@pythnetwork/express-relay-evm-js";
-import { BotType, ionicLiquidatorAbi, PythLiquidatablePool } from "@ionicprotocol/sdk";
+import { BotType, ionicUniV3LiquidatorAbi, PythLiquidatablePool } from "@ionicprotocol/sdk";
 import { createPublicClient, createWalletClient, encodeAbiParameters, encodeFunctionData, Hex, http } from "viem";
 import { mode } from "viem/chains";
 import { privateKeyToAccount } from "viem/accounts";
 
-import { sendDiscordNotification } from "./services/PERdiscord"; // Import the new module
+import { sendDiscordNotification } from "./services/PERdiscord";
 import config from "./config";
 import { logger } from "./logger";
 import { Liquidator } from "./services";
 import { setUpSdk } from "./utils";
 
+// Ensure BigInt.prototype.toJSON is correctly implemented
 (BigInt.prototype as any).toJSON = function () {
   return this.toString();
 };
 
+
+
+// Function to calculate total token value in ETH
+async function calculateTotalValueInEth(
+    _tokenAddress: string,
+    tokenAmount: bigint
+): Promise<bigint> {
+    // Placeholder: Replace with the actual token price fetching logic
+    const tokenPriceInEth = BigInt(1e18); // Example: 1 ETH per token
+    // Calculate total value in ETH
+    const totalValueInEth = tokenPriceInEth * tokenAmount / BigInt(1e18);
+    return totalValueInEth;
+}
+
 (async function () {
-  const chainName: string = config.chainName;
-  const account = privateKeyToAccount(config.adminPrivateKey as Hex);
+    const chainName: string = config.chainName;
+    const account = privateKeyToAccount(config.adminPrivateKey as Hex);
 
-  const publicClient = createPublicClient({
-    chain: mode,
-    transport: http(config.rpcUrl),
-  });
+    const publicClient = createPublicClient({
+        chain: mode,
+        transport: http(config.rpcUrl),
+    });
 
-  const walletClient = createWalletClient({
-    account,
-    chain: mode,
-    transport: http(config.rpcUrl),
-  });
+    const walletClient = createWalletClient({
+        account,
+        chain: mode,
+        transport: http(config.rpcUrl),
+    });
 
-  const ionicSdk = setUpSdk(config.chainId, publicClient, walletClient);
-  const ionicLiquidator = ionicSdk.contracts.IonicLiquidator.address as `0x${string}`;
+    const ionicSdk = setUpSdk(mode.id, publicClient, walletClient);
+    const ionicLiquidator = ionicSdk.contracts.IonicLiquidator.address as `0x${string}`;
 
-  logger.info(`Config for bot: ${JSON.stringify({ ...ionicSdk.chainLiquidationConfig, ...config })}`);
+    logger.info(`Config for bot: ${JSON.stringify({ ...ionicSdk.chainLiquidationConfig, ...config })}`);
+    const liquidator = new Liquidator(ionicSdk);
+    const liquidatablePools = await liquidator.fetchLiquidations<PythLiquidatablePool>(BotType.Pyth);
 
-  const liquidator = new Liquidator(ionicSdk);
-  const liquidatablePools = await liquidator.fetchLiquidations<PythLiquidatablePool>(BotType.Pyth);
+    logger.info(`Found ${liquidatablePools.length} pools with liquidations to process`);
+    const client: Client = new Client({ baseUrl: config.expressRelayEndpoint });
 
-  logger.info(`Found ${liquidatablePools.length} pools with liquidations to process`);
-  const client: Client = new Client({ baseUrl: config.expressRelayEndpoint });
+    for (const liquidatablePool of liquidatablePools) {
+        logger.info(`Liquidating pool: ${liquidatablePool.comptroller} -- ${liquidatablePool.liquidations.length} liquidations found`);
 
-  for (const liquidatablePool of liquidatablePools) {
-    logger.info(
-      `Liquidating pool: ${liquidatablePool.comptroller} -- ${liquidatablePool.liquidations.length} liquidations found`
-    );
+        for (const liquidation of liquidatablePool.liquidations) {
+            logger.info(`Borrower Address: ${liquidation.args[0]}`);
+            logger.info(`Repay Amount: ${liquidation.args[1].toString()}`);
+            logger.info(`cErc20 Address: ${liquidation.args[2]}`);
+            logger.info(`cToken Collateral Address: ${liquidation.args[3]}`);
+            logger.info(`Minimum Output Amount: ${liquidation.args[4].toString()}`);
 
-    for (const liquidation of liquidatablePool.liquidations) {
-      // Log each argument of the current liquidation
-      logger.info(`Borrower Address: ${liquidation.args[0]}`);
-      logger.info(`Repay Amount: ${liquidation.args[1].toString()}`);
-      logger.info(`cErc20 Address: ${liquidation.args[2]}`);
-      logger.info(`cToken Collateral Address: ${liquidation.args[3]}`);
-      logger.info(`Minimum Output Amount: ${liquidation.args[4].toString()}`);
+            if (liquidation.buyTokenUnderlying && liquidation.sellTokenUnderlying) {
+                logger.info(`Buying Token (Underlying): ${liquidation.buyTokenUnderlying}, Amount: ${liquidation.buyTokenAmount.toString()}`);
+                logger.info(`Selling Token (Underlying): ${liquidation.sellTokenUnderlying}, Amount: ${liquidation.sellTokenAmount.toString()}`);
 
-      // Log additional details about tokens involved in the liquidation, if applicable
-      if (liquidation.buyTokenUnderlying && liquidation.sellTokenUnderlying) {
-        logger.info(
-          `Buying Token (Underlying): ${
-            liquidation.buyTokenUnderlying
-          }, Amount: ${liquidation.buyTokenAmount.toString()}`
-        );
-        logger.info(
-          `Selling Token (Underlying): ${
-            liquidation.sellTokenUnderlying
-          }, Amount: ${liquidation.sellTokenAmount.toString()}`
-        );
-      }
+                // Calculate total value in ETH for buy and sell tokens
+                const totalBuyValueInEth = await calculateTotalValueInEth(
+                    liquidation.buyTokenUnderlying as `0x${string}`,
+                    BigInt(liquidation.buyTokenAmount.toString())
+                );
+                const totalSellValueInEth = await calculateTotalValueInEth(
+                    liquidation.sellTokenUnderlying as `0x${string}`,
+                    BigInt(liquidation.sellTokenAmount.toString())
+                );
+                const potentialProfit = totalBuyValueInEth - totalSellValueInEth;
 
-      const calldata = encodeFunctionData({
-        abi: ionicLiquidatorAbi,
-        functionName: "safeLiquidate",
-        args: [liquidation.args[0], liquidation.args[1], liquidation.args[2], liquidation.args[3], liquidation.args[4]],
-      });
+                logger.info(`Potential Profit in ETH: ${potentialProfit}`);
 
-      const permissionkeyPayload = encodeAbiParameters([{ type: "address", name: "borrower" }], [liquidation.args[0]]);
-      const permissionKey = encodeAbiParameters(
-        [
-          { type: "address", name: "contract" },
-          { type: "bytes", name: "vaultId" },
-        ],
-        [ionicLiquidator, permissionkeyPayload]
-      );
+                const gasPrice: bigint = BigInt(await publicClient.getGasPrice());
+                logger.info(`Gas Price: ${gasPrice}`);
+                const expectedGasFee: bigint = gasPrice * BigInt(750000); // Use actual gas estimate if available
 
-      const opportunity: OpportunityParams = {
-        chainId: chainName,
-        targetContract: ionicLiquidator,
-        targetCalldata: calldata as `0x${string}`,
-        permissionKey: permissionKey as `0x${string}`,
-        targetCallValue: BigInt(0),
-        buyTokens: [
-          {
-            token: liquidation.buyTokenUnderlying as `0x${string}`,
-            amount: BigInt(liquidation.buyTokenAmount.toString()),
-          },
-        ],
-        sellTokens: [
-          {
-            token: liquidation.sellTokenUnderlying as `0x${string}`,
-            amount: BigInt(liquidation.sellTokenAmount.toString()),
-          },
-        ],
-      };
+                const minProfitAmountEth: bigint = expectedGasFee + BigInt(ionicSdk.chainLiquidationConfig.MINIMUM_PROFIT_NATIVE);
+                logger.info(`Min Profit Amount in ETH: ${minProfitAmountEth}`);
 
-      try {
-        await client.submitOpportunity(opportunity);
-        console.log("Opportunity submitted successfully: ", opportunity);
+                if (potentialProfit < minProfitAmountEth) {
+                    logger.info(`Potential profit of ${potentialProfit} is less than the minimum required profit of ${minProfitAmountEth}, skipping liquidation.`);
+                    continue;
+                }
 
-        // Send a Discord notification
-        await sendDiscordNotification(opportunity);
-      } catch (error) {
-        console.error("Failed to submit opportunity:", {
-          error,
-          opportunity: JSON.stringify(opportunity, null, 2),
-          blockNumber: await publicClient.getBlockNumber(),
-        });
-      }
+                const calldata = encodeFunctionData({
+                    abi: ionicUniV3LiquidatorAbi,
+                    functionName: "safeLiquidate",
+                    args: [liquidation.args[0], liquidation.args[1], liquidation.args[2], liquidation.args[3], liquidation.args[4]],
+                });
+                logger.info(`Calldata: ${calldata}`);
+
+                const permissionkeyPayload = encodeAbiParameters([{ type: "address", name: "borrower" }], [liquidation.args[0]]);
+                const permissionKey = encodeAbiParameters(
+                    [
+                        { type: "address", name: "contract" },
+                        { type: "bytes", name: "vaultId" },
+                    ],
+                    [ionicLiquidator, permissionkeyPayload]
+                );
+                logger.info(`Permission Key: ${permissionKey}`);
+
+                const opportunity: OpportunityParams = {
+                    chainId: chainName,
+                    targetContract: ionicLiquidator,
+                    targetCalldata: calldata as `0x${string}`,
+                    permissionKey: permissionKey as `0x${string}`,
+                    targetCallValue: BigInt(0),
+                    buyTokens: [
+                        {
+                            token: liquidation.buyTokenUnderlying as `0x${string}`,
+                            amount: BigInt(liquidation.buyTokenAmount.toString()),
+                        },
+                    ],
+                    sellTokens: [
+                        {
+                            token: liquidation.sellTokenUnderlying as `0x${string}`,
+                            amount: BigInt(liquidation.sellTokenAmount.toString()),
+                        },
+                    ],
+                };
+                logger.info("Opportunity:", JSON.stringify(opportunity, null, 2));
+
+                try {
+                    await client.submitOpportunity(opportunity);
+                    console.info("Opportunity submitted successfully: ", opportunity);
+
+                    await sendDiscordNotification(opportunity);
+                } catch (error) {
+                    console.error("Failed to submit opportunity:", {
+                        error,
+                        opportunity: JSON.stringify(opportunity, null, 2),
+                        blockNumber: await publicClient.getBlockNumber(),
+                    });
+                }
+            }
+        }
     }
-  }
 })();
