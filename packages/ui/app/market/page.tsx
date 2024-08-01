d0be4ee99a699559f61598c710bfbc2e2269a649
Rahul Sethuram <rahul.eth@icloud.com>
2024-08-01 14:03:18 +0400

    fix: build

--- packages/ui/app/market/page.tsx
+++ packages/ui/app/market/page.tsx
@@ -109,7 +109,6 @@ export default function Market() {
     chainId: dropdownSelectedChain,
     poolId: selectedPool
   });
-  console.log('rewards: ', rewards);
 
   // const networkOptionstest = [
   //   {
