d0777c72feb02771b9038686046bd5adaa4e66dd
shikhar360 <sssonishikhar360@gmail.com>
2024-08-22 18:21:46 +0530

    added tvl and other assets

--- packages/ui/app/_components/earn/EarnRows.tsx
+++ packages/ui/app/_components/earn/EarnRows.tsx
@@ -2,150 +2,176 @@
 
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
+// import { useEffect, useState } from 'react';
+// import { formatEther, formatUnits } from 'viem';
 import { mode } from 'viem/chains';
+// import { useReadContract } from 'wagmi';
 
+// import { BaseContractABI } from '@ui/constants/baselp';
 import { pools } from '@ui/constants/index';
+import { useTvl } from '@ui/hooks/useTvl';
 
 export type EarnRow = {
-  apr?: number | string;
+  apr: number;
   asset: string[]; //name of the asset in uppercase array
-  getApr: () => Promise<number>;
-  getTvl: () => Promise<number>;
+  getApr?: () => Promise<number>;
+  getTvl?: () => Promise<number>;
   link: string;
-  network?: string;
-  protocol?: string;
+  network: string;
+  poolChain: number;
+  protocol: string;
   tvl: number;
+  tvlpool?: string;
 };
 
-type EarnRowsParams = {
-  rows: EarnRow[];
-};
-export default function EarnRows({ rows }: EarnRowsParams) {
+// type EarnRowsParams = {
+//   rows: EarnRow[];
+// };
+export default function EarnRows({
+  apr,
+  asset,
+  network,
+  protocol,
+  tvlpool,
+  tvl,
+  link,
+  poolChain
+  // getTvl,
+  // getApr,
+}: EarnRow) {
+  // const [tvlValue, setTvlValue] = useState();
   const searchParams = useSearchParams();
   const querychain = searchParams.get('chain');
   const chain = querychain ?? mode.id;
+  const { data: tvlval } = useTvl({
+    poolAddress: tvlpool as `0x${string}`,
+    poolChainId: poolChain,
+    assets: asset
+  });
+  // console.log(tvlval);
+
+  const totaltvl = tvlval?.reduce((acc, tvl) => acc + Number(tvl), 0) ?? 0;
   return (
     <>
-      {rows.map(({ apr, asset, network, protocol, tvl, link }, idx) => (
-        <div
-          className=" w-full hover:bg-graylite transition-all duration-200 ease-linear bg-grayUnselect rounded-xl mb-3 md:px-3 px-3 gap-x-1 md:grid grid-cols-13  py-4 text-sm text-white md:text-center items-center relative flex flex-col "
-          key={idx}
-        >
-          <div className="col-span-3 w-full flex justify-between md:justify-center  gap-x-2 mb-1.5 ">
-            <span className="text-white/40 font-semibold text-[11px] text-center md:hidden">
-              ASSET
-            </span>
-            <div className={` flex ml-auto md:ml-0 `}>
-              {asset.map((coin: string, idx: number) => (
-                <img
-                  alt="logos"
-                  className={` w-5 h-5  top-0 left-0 ${
-                    idx !== 0 && ' -translate-x-1'
-                  } `}
-                  key={idx}
-                  src={`/img/symbols/32/color/${coin}.png`}
-                />
-              ))}
-            </div>
-            <div>
-              {asset.map((val: string, idx: number) => (
-                <span
-                  className="text-center"
-                  key={idx}
-                >
-                  {idx !== 0 && '/'}
-                  {val}
-                </span>
-              ))}
-            </div>
-            {/* <OverlayingAssetImg /> */}
-          </div>
-          <p className="col-span-2 w-full flex justify-between md:justify-center gap-x-2 mb-1.5 ">
-            <span className="text-white/40 font-semibold text-[11px] text-center md:hidden">
-              PROTOCOL
-            </span>
-            {protocol}
-          </p>
-          <div className="col-span-2 w-full flex justify-between md:justify-center gap-x-2 mb-1.5">
-            <span className="text-white/40 font-semibold text-[11px] text-center md:hidden">
-              NETWORK
-            </span>
-            <img
-              alt="logos"
-              className={` w-5 h-5  md:mx-auto ml-auto top-0 left-0 `}
-              src={`/img/logo/${network}.png`}
-            />
-          </div>
-          <div className="popover-container relative flex md:flex-col items-center w-full  cursor-pointer mb-1.5 col-span-2">
-            <div className=" w-max flex  md:justify-center gap-x-2">
-              <span className="text-white/40 font-semibold text-[11px] text-center md:hidden">
-                APR
-              </span>
-              {apr && Number(apr) > 0 ? apr : '-'}%
-            </div>
-            <span
-              className={`${pools[+chain].text} ${pools[+chain].bg} rounded-md w-max md:text-[10px] text-[8px] md:mb-1 py-[1px] md:px-2.5 px-1 ml-auto md:ml-0 text-center`}
-            >
-              + POINTS <i className="popover-hint">i</i>
-            </span>
-            <a
-              className={`${pools[+chain].text} bg-accent rounded-md w-max md:text-[10px] text-[8px] md:mb-1 py-[1px] md:px-2.5 px-1 text-center ml-1 md:ml-0`}
-              href="https://turtle.club/dashboard/?ref=IONIC"
-              target="_blank"
-            >
-              + TURTLE{' '}
+      <div className=" w-full hover:bg-graylite transition-all duration-200 ease-linear bg-grayUnselect rounded-xl mb-3 md:px-3 px-3 gap-x-1 md:grid grid-cols-13  py-4 text-sm text-white md:text-center items-center relative flex flex-col ">
+        <div className="col-span-3 w-full flex justify-between md:justify-center  gap-x-2 mb-1.5 ">
+          <span className="text-white/40 font-semibold text-[11px] text-center md:hidden">
+            ASSET
+          </span>
+          <div className={` flex ml-auto md:ml-0 `}>
+            {asset.map((coin: string, idx: number) => (
               <img
-                alt="external-link"
-                className={`w-3 h-3 inline-block`}
-                src="https://img.icons8.com/material-outlined/24/external-link.png"
+                alt="logos"
+                className={` w-5 h-5  top-0 left-0 ${
+                  idx !== 0 && ' -translate-x-1'
+                } `}
+                key={idx}
+                src={`/img/symbols/32/color/${coin}.png`}
               />
-            </a>
-            <div
-              className={`font-bold popover absolute w-[160px] top-full p-2 mt-1 border border-mode rounded-lg text-xs z-30 opacity-0 invisible bg-grayUnselect transition-all whitespace-nowrap`}
-            >
-              Base APR: {apr && Number(apr) > 0 ? apr : '-'}%
-              <div className="flex pt-4">
-                <img
-                  alt=""
-                  className="size-4 rounded mr-1"
-                  src="/img/ionic-sq.png"
-                />{' '}
-                + 3x Ionic Points
-              </div>
-              <div className="flex">
-                <img
-                  alt=""
-                  className="size-4 rounded mr-1"
-                  src="/images/turtle-ionic.png"
-                />{' '}
-                + Turtle Ionic Points
-              </div>
-            </div>
+            ))}
           </div>
-          <div className="col-span-1 w-full flex justify-between md:justify-center gap-x-2 mb-2 ">
-            <span className="text-white/40 text-xs font-semibold md:hidden">
-              TVL
+          <div>
+            {asset.map((val: string, idx: number) => (
+              <span
+                className="text-center"
+                key={idx}
+              >
+                {idx !== 0 && '/'}
+                {val}
+              </span>
+            ))}
+          </div>
+          {/* <OverlayingAssetImg /> */}
+        </div>
+        <p className="col-span-2 w-full flex justify-between md:justify-center gap-x-2 mb-1.5 ">
+          <span className="text-white/40 font-semibold text-[11px] text-center md:hidden">
+            PROTOCOL
+          </span>
+          {protocol}
+        </p>
+        <div className="col-span-2 w-full flex justify-between md:justify-center gap-x-2 mb-1.5">
+          <span className="text-white/40 font-semibold text-[11px] text-center md:hidden">
+            NETWORK
+          </span>
+          <img
+            alt="logos"
+            className={` w-5 h-5  md:mx-auto ml-auto top-0 left-0 `}
+            src={`/img/logo/${network}.png`}
+          />
+        </div>
+        <div className="popover-container relative flex md:flex-col items-center w-full  cursor-pointer mb-1.5 col-span-2">
+          <div className=" w-max flex  md:justify-center gap-x-2">
+            <span className="text-white/40 font-semibold text-[11px] text-center md:hidden">
+              APR
             </span>
-            $
-            {tvl > 0
-              ? tvl.toLocaleString(undefined, { maximumFractionDigits: 2 })
-              : '-'}
+            {apr && Number(apr) > 0 ? apr : '∞'}%
           </div>
-          <div className="col-span-1"> </div>
-          <Link
-            className="col-span-2 w-full text-xs bg-accent text-darkone rounded-md py-1.5 px-3 font-semibold cursor-pointer mx-auto flex items-center justify-center gap-1.5"
-            href={link}
+          <span
+            className={`${pools[+chain].text} ${pools[+chain].bg} rounded-md w-max md:text-[10px] text-[8px] md:mb-1 py-[1px] md:px-2.5 px-1 ml-auto md:ml-0 text-center`}
+          >
+            + POINTS <i className="popover-hint">i</i>
+          </span>
+          <a
+            className={`${pools[+chain].text} bg-accent rounded-md w-max md:text-[10px] text-[8px] md:mb-1 py-[1px] md:px-2.5 px-1 text-center ml-1 md:ml-0`}
+            href="https://turtle.club/dashboard/?ref=IONIC"
             target="_blank"
           >
-            <span>DEPOSIT</span>
+            + TURTLE{' '}
             <img
               alt="external-link"
-              className={`w-3 h-3`}
+              className={`w-3 h-3 inline-block`}
               src="https://img.icons8.com/material-outlined/24/external-link.png"
             />
-          </Link>
+          </a>
+          <div
+            className={`font-bold popover absolute w-[160px] top-full p-2 mt-1 border border-mode rounded-lg text-xs z-30 opacity-0 invisible bg-grayUnselect transition-all whitespace-nowrap`}
+          >
+            Base APR: {apr && Number(apr) > 0 ? apr : '-'}%
+            <div className="flex pt-4">
+              <img
+                alt=""
+                className="size-4 rounded mr-1"
+                src="/img/ionic-sq.png"
+              />{' '}
+              + 3x Ionic Points
+            </div>
+            <div className="flex">
+              <img
+                alt=""
+                className="size-4 rounded mr-1"
+                src="/images/turtle-ionic.png"
+              />{' '}
+              + Turtle Ionic Points
+            </div>
+          </div>
         </div>
-      ))}
+        <div className="col-span-1 w-full flex justify-between md:justify-center gap-x-2 mb-2 ">
+          <span className="text-white/40 text-xs font-semibold md:hidden">
+            TVL
+          </span>
+          $
+          {tvl > 0
+            ? tvl.toLocaleString(undefined, { maximumFractionDigits: 2 })
+            : totaltvl === 0
+              ? '-'
+              : totaltvl.toLocaleString(undefined, {
+                  maximumFractionDigits: 2
+                })}
+        </div>
+        <div className="col-span-1"> </div>
+        <Link
+          className="col-span-2 w-full text-xs bg-accent text-darkone rounded-md py-1.5 px-3 font-semibold cursor-pointer mx-auto flex items-center justify-center gap-1.5"
+          href={link}
+          target="_blank"
+        >
+          <span>DEPOSIT</span>
+          <img
+            alt="external-link"
+            className={`w-3 h-3`}
+            src="https://img.icons8.com/material-outlined/24/external-link.png"
+          />
+        </Link>
+      </div>
     </>
   );
 }
--- packages/ui/app/earn/page.tsx
+++ packages/ui/app/earn/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import { base, mode } from 'viem/chains';
 
 import EarnRows, { type EarnRow } from '../_components/earn/EarnRows';
 
@@ -12,8 +13,34 @@ const earnOpps: EarnRow[] = [
     getTvl: () => Promise.resolve(0),
     link: 'https://velodrome.finance/deposit?token0=0x18470019bF0E94611f15852F7e93cf5D65BC34CA&token1=0x4200000000000000000000000000000000000006&type=-1',
     network: 'mode',
+    poolChain: mode.id,
     protocol: 'Velodrome',
-    tvl: 0
+    tvl: 0,
+    tvlpool: '0xC6A394952c097004F83d2dfB61715d245A38735a'
+  },
+  {
+    apr: 0,
+    asset: ['ION', 'WETH'],
+    getApr: () => Promise.resolve(0),
+    getTvl: () => Promise.resolve(0),
+    link: '-',
+    network: 'base',
+    poolChain: base.id,
+    protocol: 'Aerodrome Finance',
+    tvl: 0,
+    tvlpool: '0x0FAc819628a7F612AbAc1CaD939768058cc0170c'
+  },
+  {
+    apr: 0,
+    asset: ['ION', 'MODE'],
+    getApr: () => Promise.resolve(0),
+    getTvl: () => Promise.resolve(0),
+    link: 'https://velodrome.finance/deposit?token0=0x18470019bF0E94611f15852F7e93cf5D65BC34CA&token1=0x4200000000000000000000000000000000000006&type=-1',
+    network: 'mode',
+    poolChain: mode.id,
+    protocol: 'Velodrome',
+    tvl: 0,
+    tvlpool: '0x690A74d2eC0175a69C0962B309E03021C0b5002E'
   },
   {
     apr: 0,
@@ -49,7 +76,30 @@ const earnOpps: EarnRow[] = [
     link: 'https://app.steer.finance/vault/0x17694615caba46ef765a3673fa488e04332b522a/34443',
     network: 'mode',
     protocol: 'Steer',
-    tvl: 0
+    tvl: 0,
+    poolChain: mode.id
+  },
+  {
+    apr: 0,
+    asset: ['ionUSDC'],
+    getApr: () => Promise.resolve(0),
+    getTvl: () => Promise.resolve(0),
+    link: 'https://davos.xyz/app/loans/mint/?network=mode&token=ionUSDC',
+    network: 'mode',
+    protocol: 'Davos',
+    tvl: 0,
+    poolChain: mode.id
+  },
+  {
+    apr: 0,
+    asset: ['ionUSDT'],
+    getApr: () => Promise.resolve(0),
+    getTvl: () => Promise.resolve(0),
+    link: 'https://davos.xyz/app/loans/mint/?network=mode&token=ionUSDT',
+    network: 'mode',
+    protocol: 'Davos',
+    tvl: 0,
+    poolChain: mode.id
   }
 ];
 
@@ -60,7 +110,9 @@ export default function Earn() {
     const populateVals = async () => {
       await Promise.all(
         rows.map(async (row) => {
+          //@ts-ignore
           row.apr = await row.getApr();
+          //@ts-ignore
           row.tvl = await row.getTvl();
         })
       );
@@ -89,7 +141,24 @@ export default function Earn() {
         </div>
 
         {/* this will get mapped out in future with the possible api data structure mentioned below */}
-        <EarnRows rows={rows} />
+        {rows.map(
+          (
+            { apr, asset, network, protocol, tvl, link, poolChain, tvlpool },
+            idx
+          ) => (
+            <EarnRows
+              apr={apr}
+              asset={asset}
+              network={network}
+              protocol={protocol}
+              tvl={tvl}
+              tvlpool={tvlpool}
+              poolChain={poolChain}
+              link={link}
+              key={idx}
+            />
+          )
+        )}
       </div>
     </>
   );
