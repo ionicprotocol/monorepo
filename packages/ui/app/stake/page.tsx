35c97a616133765546817af3783a151339f16226
shikhar360 <sssonishikhar360@gmail.com>
2024-08-26 13:26:42 +0530

    tokenarr modifying

--- packages/ui/app/stake/page.tsx
+++ packages/ui/app/stake/page.tsx
@@ -586,6 +586,9 @@ export default function Stake() {
     return getToken(+chain);
   }, [chain]);
 
+  // const tokenArrOfChain = {
+  //   [mode.id]: ['eth', 'weth', 'mode']
+  // };
   return (
     <main className={``}>
       <div className="w-full flex items-center justify-center md:py-20 py-8 transition-all duration-200 ease-linear bg-black dark:bg-black relative">
@@ -666,6 +669,7 @@ export default function Stake() {
                   token={getPoolToken(selectedtoken as 'eth' | 'mode' | 'weth')}
                   chain={+chain}
                   tokenSelector={true}
+                  // tokenArr={}
                 />
               </>
             )}
