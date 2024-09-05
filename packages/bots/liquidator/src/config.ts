888bda2e8499e40f8e566f7f6cd9368bf7b90a7b
amish kohli <amishkohli@amishs-Laptop.local>
2024-09-05 23:33:40 +0530

    rpc logic testing

--- packages/bots/liquidator/src/config.ts
+++ packages/bots/liquidator/src/config.ts
@@ -5,7 +5,9 @@ const config = {
   environment: process.env.NODE_ENV ?? "development",
   logLevel: process.env.LOG_LEVEL ?? "info",
   chainId: parseInt(process.env.TARGET_CHAIN_ID ?? "34443", 10),
-  rpcUrls: process.env.WEB3_HTTP_PROVIDER_URLS ? process.env.WEB3_HTTP_PROVIDER_URLS.split(",") : ["https://mainnet.mode.network/"], // Updated to handle multiple RPC URLs
+  rpcUrls: process.env.WEB3_HTTP_PROVIDER_URLS
+    ? process.env.WEB3_HTTP_PROVIDER_URLS.split(",")
+    : ["https://mainnet.mode.network/"], // Updated to handle multiple RPC URLs
   adminPrivateKey: process.env.ETHEREUM_ADMIN_PRIVATE_KEY ?? "",
   adminAccount: process.env.ETHEREUM_ADMIN_ACCOUNT ?? "",
   excludedComptrollers: process.env.EXCLUDED_COMPTROLLERS ? process.env.EXCLUDED_COMPTROLLERS.split(",") : [],
