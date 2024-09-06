6d949c525a8552e1f0e6fd89c73287f62afd7b2a
shikhar360 <sssonishikhar360@gmail.com>
2024-09-06 15:36:00 +0530

    added strategy

--- packages/ui/utils/earnUtils.ts
+++ packages/ui/utils/earnUtils.ts
@@ -11,6 +11,8 @@ export type EarnRow = {
   protocol: string;
   tvl: number;
   tvlpool?: string;
+  img: string;
+  strategy: string;
   rewards: Record<number, IRewards>;
   live?: boolean;
 };
@@ -42,6 +44,8 @@ export const earnOpps: EarnRow[] = [
     network: 'mode',
     poolChain: mode.id,
     protocol: 'Velodrome',
+    strategy: 'Liquidty Pool',
+    img: '/img/symbols/32/color/velo.png',
     tvl: 0,
     tvlpool: '0xC6A394952c097004F83d2dfB61715d245A38735a'
   },
@@ -65,6 +69,8 @@ export const earnOpps: EarnRow[] = [
     network: 'base',
     poolChain: base.id,
     protocol: 'Aerodrome Finance',
+    strategy: 'Liquidty Pool',
+    img: '/img/symbols/32/color/aero.png',
     tvl: 0,
     tvlpool: '0x0FAc819628a7F612AbAc1CaD939768058cc0170c'
   },
@@ -88,6 +94,8 @@ export const earnOpps: EarnRow[] = [
     network: 'mode',
     poolChain: mode.id,
     protocol: 'Velodrome',
+    strategy: 'Liquidty Pool',
+    img: '/img/symbols/32/color/velo.png',
     tvl: 0,
     tvlpool: '0x690A74d2eC0175a69C0962B309E03021C0b5002E'
   },
@@ -136,6 +144,8 @@ export const earnOpps: EarnRow[] = [
     link: 'https://app.steer.finance/vault/0x17694615caba46ef765a3673fa488e04332b522a/34443',
     network: 'mode',
     protocol: 'Steer',
+    strategy: 'Liquidty Pool',
+    img: '/img/symbols/32/color/steer.png',
     tvl: 0,
     poolChain: mode.id
   },
@@ -158,6 +168,8 @@ export const earnOpps: EarnRow[] = [
     link: 'https://peapods.finance/app?pod=0xFa1D1f89e64A1b5ba50fb867d2aa660D9E6dE029',
     network: 'mode',
     protocol: 'Peapods',
+    strategy: 'Volatility Farming / Single Staking',
+    img: '/img/symbols/32/color/peapods.png',
     tvl: 0,
     poolChain: mode.id
   },
@@ -180,6 +192,8 @@ export const earnOpps: EarnRow[] = [
     link: 'https://perps.ionic.money/',
     network: 'mode',
     protocol: 'Lynx',
+    strategy: 'Perps / Single Staking',
+    img: '/img/symbols/32/color/lynx.png',
     tvl: 0,
     poolChain: mode.id
   },
@@ -202,6 +216,8 @@ export const earnOpps: EarnRow[] = [
     link: 'https://davos.xyz/app/loans/mint/?network=mode&token=ionUSDC',
     network: 'mode',
     protocol: 'Davos',
+    strategy: 'CDP Stablecoin',
+    img: '/img/symbols/32/color/davos.png',
     tvl: 0,
     poolChain: mode.id
   },
@@ -224,6 +240,8 @@ export const earnOpps: EarnRow[] = [
     link: 'https://davos.xyz/app/loans/mint/?network=mode&token=ionUSDT',
     network: 'mode',
     protocol: 'Davos',
+    strategy: 'CDP Stablecoin',
+    img: '/img/symbols/32/color/davos.png',
     tvl: 0,
     poolChain: mode.id
   },
@@ -246,6 +264,8 @@ export const earnOpps: EarnRow[] = [
     link: 'https://www.tren.finance',
     network: 'mode',
     protocol: 'Tren',
+    strategy: 'CDP Stablecoin',
+    img: '/img/symbols/32/color/tren.png',
     tvl: 0,
     poolChain: mode.id
   },
@@ -268,6 +288,9 @@ export const earnOpps: EarnRow[] = [
     link: '#',
     network: 'mode',
     protocol: 'Otomato',
+    strategy: '-',
+    // img: '/img/symbols/32/color/otomato.png',
+    img: 'https://img.icons8.com/fluency-systems-regular/50/ffffff/info--v1.png',
     tvl: 0,
     poolChain: mode.id
   }
