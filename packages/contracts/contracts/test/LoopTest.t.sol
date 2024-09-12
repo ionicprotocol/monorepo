d15ce70632c7d1a649ec7099a6f1f3d32cf61f8d
Rahul Sethuram <rahul.eth@icloud.com>
2024-09-12 16:41:34 +0400

    fix: no test in prod

--- packages/contracts/contracts/test/LoopTest.t.sol
+++ packages/contracts/contracts/test/LoopTest.t.sol
@@ -1,67 +1,52 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.0;
+// // SPDX-License-Identifier: UNLICENSED
+// pragma solidity >=0.8.0;
 
-import "forge-std/Test.sol";
+// import "forge-std/Test.sol";
 
-import { LeveredPosition } from "../ionic/levered/LeveredPosition.sol";
-import { ILeveredPositionFactory } from "../ionic/levered/ILeveredPositionFactory.sol";
-import { ICErc20 } from "../compound/CTokenInterfaces.sol";
-import { ILiquidatorsRegistry } from "../liquidators/registry/ILiquidatorsRegistry.sol";
-import { IRedemptionStrategy } from "../liquidators/IRedemptionStrategy.sol";
-import { IERC20Upgradeable } from "openzeppelin-contracts-upgradeable/contracts/token/ERC20/IERC20Upgradeable.sol";
+// import { LeveredPosition } from "../ionic/levered/LeveredPosition.sol";
+// import { ILeveredPositionFactory } from "../ionic/levered/ILeveredPositionFactory.sol";
+// import { ICErc20 } from "../compound/CTokenInterfaces.sol";
+// import { ILiquidatorsRegistry } from "../liquidators/registry/ILiquidatorsRegistry.sol";
+// import { IRedemptionStrategy } from "../liquidators/IRedemptionStrategy.sol";
+// import { IERC20Upgradeable } from "openzeppelin-contracts-upgradeable/contracts/token/ERC20/IERC20Upgradeable.sol";
 
-contract ezEthWethLeveredPositionTest is Test {
-  address me = 0x1155b614971f16758C92c4890eD338C9e3ede6b7;
+// contract ezEthWethLeveredPositionTest is Test {
+//   address me = 0x1155b614971f16758C92c4890eD338C9e3ede6b7;
 
-  function setUp() public {}
+//   function setUp() public {}
 
-  function test_ezEthWeth() public {
-    vm.createSelectFork(vm.rpcUrl("base_archive"));
-    vm.rollFork(19676819);
+//   function test_ezEthWeth() public {
+//     vm.createSelectFork(vm.rpcUrl("base_archive"));
+//     vm.rollFork(19677401);
 
-    ILeveredPositionFactory factory = ILeveredPositionFactory(0x0Bd42a5226db7FCEb9D3e50539778A15C3665da8);
-    ICErc20 collateralMarket = ICErc20(0x079f84161642D81aaFb67966123C9949F9284bf5);
-    ICErc20 stableMarket = ICErc20(0x49420311B518f3d0c94e897592014de53831cfA3);
-    uint256 depositAmount = 8338179027698429;
+//     ILeveredPositionFactory factory = ILeveredPositionFactory(0x0Bd42a5226db7FCEb9D3e50539778A15C3665da8);
+//     ICErc20 collateralMarket = ICErc20(0x079f84161642D81aaFb67966123C9949F9284bf5);
+//     ICErc20 stableMarket = ICErc20(0x49420311B518f3d0c94e897592014de53831cfA3);
+//     uint256 depositAmount = 8338179027698429;
 
-    ILiquidatorsRegistry registry = ILiquidatorsRegistry(0xc71B968C6C23e2723Bae32957D815C9bE3ca1b34);
-    IRedemptionStrategy redemptionStrategy = IRedemptionStrategy(0x83fa5224C7218a731DB18E26ad6Df47dcf7fB73d); // new univ3liquidatorfunder
-    vm.startPrank(registry.owner());
-    registry._setRedemptionStrategy(
-      redemptionStrategy,
-      IERC20Upgradeable(address(collateralMarket.underlying())),
-      IERC20Upgradeable(address(stableMarket.underlying()))
-    );
-    registry._setRedemptionStrategy(
-      redemptionStrategy,
-      IERC20Upgradeable(address(stableMarket.underlying())),
-      IERC20Upgradeable(address(collateralMarket.underlying()))
-    );
-    vm.stopPrank();
+//     IERC20Upgradeable collateralToken = IERC20Upgradeable(collateralMarket.underlying());
 
-    IERC20Upgradeable collateralToken = IERC20Upgradeable(collateralMarket.underlying());
+//     vm.startPrank(me);
+//     collateralToken.approve(address(factory), depositAmount);
+//     LeveredPosition position = factory.createAndFundPositionAtRatio(
+//       collateralMarket,
+//       stableMarket,
+//       collateralToken,
+//       depositAmount,
+//       3 ether
+//     );
+//     vm.stopPrank();
 
-    vm.startPrank(me);
-    collateralToken.approve(address(factory), depositAmount);
-    LeveredPosition position = factory.createAndFundPositionAtRatio(
-      collateralMarket,
-      stableMarket,
-      collateralToken,
-      depositAmount,
-      3 ether
-    );
-    vm.stopPrank();
+//     uint256 _maxRatio;
+//     uint256 _minRatio;
 
-    uint256 _maxRatio;
-    uint256 _minRatio;
+//     _maxRatio = position.getMaxLeverageRatio();
+//     emit log_named_uint("max ratio", _maxRatio);
+//     _minRatio = position.getMinLeverageRatio();
+//     emit log_named_uint("min ratio", _minRatio);
+//     assertGt(_maxRatio, _minRatio, "max ratio <= min ratio");
 
-    _maxRatio = position.getMaxLeverageRatio();
-    emit log_named_uint("max ratio", _maxRatio);
-    _minRatio = position.getMinLeverageRatio();
-    emit log_named_uint("min ratio", _minRatio);
-    assertGt(_maxRatio, _minRatio, "max ratio <= min ratio");
-
-    uint256 currentRatio = position.getCurrentLeverageRatio();
-    emit log_named_uint("current ratio", currentRatio);
-  }
-}
+//     uint256 currentRatio = position.getCurrentLeverageRatio();
+//     emit log_named_uint("current ratio", currentRatio);
+//   }
+// }
--- packages/contracts/tasks/chain-specific/base/liquidation.ts
+++ packages/contracts/tasks/chain-specific/base/liquidation.ts
@@ -3,49 +3,55 @@ import { setLiquidationStrategies } from "../../liquidation";
 import { Address } from "viem";
 import { assetSymbols } from "@ionicprotocol/types";
 import { base } from "@ionicprotocol/chains";
+import {
+  weETH_MARKET,
+  ezETH_MARKET,
+  wstETH_MARKET,
+  cbETH_MARKET,
+  AERO_MARKET,
+  bsdETH_MARKET,
+  hyUSD_MARKET,
+  RSR_MARKET,
+  USDC_MARKET,
+  eUSD_MARKET,
+  WETH_MARKET
+} from ".";
 
 task("base:liquidation:set-redemption-strategies", "Set redemption strategy").setAction(
   async (_, { viem, getNamedAccounts, deployments }) => {
     const { deployer } = await getNamedAccounts();
-    const wethToken = base.assets.find((asset) => asset.symbol === assetSymbols.WETH);
-    const aeroToken = base.assets.find((asset) => asset.symbol === assetSymbols.AERO);
-    const usdcToken = base.assets.find((asset) => asset.symbol === assetSymbols.USDC);
-    const ezETHToken = base.assets.find((asset) => asset.symbol === assetSymbols.ezETH);
-    if (!wethToken || !aeroToken || !usdcToken || !ezETHToken) {
-      throw new Error("Tokens not found");
-    }
+    const collaterals = [
+      weETH_MARKET,
+      ezETH_MARKET,
+      wstETH_MARKET,
+      cbETH_MARKET,
+      AERO_MARKET,
+      bsdETH_MARKET,
+      hyUSD_MARKET,
+      RSR_MARKET
+    ];
+    const borrows = [USDC_MARKET, eUSD_MARKET, WETH_MARKET];
     const uniLiquidator = await deployments.get("UniswapV3LiquidatorFunder");
-    await setLiquidationStrategies(viem, deployments, deployer as Address, [
-      {
-        inputToken: wethToken.underlying,
-        outputToken: aeroToken.underlying,
-        strategy: uniLiquidator.address as Address
-      },
-      {
-        inputToken: aeroToken.underlying,
-        outputToken: wethToken.underlying,
-        strategy: uniLiquidator.address as Address
-      },
-      {
-        inputToken: usdcToken.underlying,
-        outputToken: wethToken.underlying,
-        strategy: uniLiquidator.address as Address
-      },
-      {
-        inputToken: wethToken.underlying,
-        outputToken: usdcToken.underlying,
-        strategy: uniLiquidator.address as Address
-      },
-      {
-        inputToken: ezETHToken.underlying,
-        outputToken: usdcToken.underlying,
-        strategy: uniLiquidator.address as Address
-      },
-      {
-        inputToken: wethToken.underlying,
-        outputToken: ezETHToken.underlying,
-        strategy: uniLiquidator.address as Address
+    const pairs: { inputToken: Address; outputToken: Address; strategy: Address }[] = [];
+    for (const collateral of collaterals) {
+      const collateralContract = await viem.getContractAt("ICErc20", collateral as Address);
+      const collateralUnderlying = await collateralContract.read.underlying();
+      for (const borrow of borrows) {
+        const borrowContract = await viem.getContractAt("ICErc20", borrow as Address);
+        const borrowUnderlying = await borrowContract.read.underlying();
+        pairs.push({
+          inputToken: collateralUnderlying,
+          outputToken: borrowUnderlying,
+          strategy: uniLiquidator.address as Address
+        });
+
+        pairs.push({
+          inputToken: borrowUnderlying,
+          outputToken: collateralUnderlying,
+          strategy: uniLiquidator.address as Address
+        });
       }
-    ]);
+    }
+    await setLiquidationStrategies(viem, deployments, deployer as Address, pairs);
   }
 );
