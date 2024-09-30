24a1c94b7eb1180e3780ead5a34f1d15e9808016
Rahul Sethuram <rahul.eth@icloud.com>
2024-09-30 16:43:43 +0400

    feat: deployments

--- packages/chains/src/lisk/index.ts
+++ packages/chains/src/lisk/index.ts
@@ -1,6 +1,6 @@
 import { ChainConfig, SupportedChains } from "@ionicprotocol/types";
 
-import deployments from "../../../sdk/deployments/base.json";
+import deployments from "../../../sdk/deployments/lisk.json";
 
 import chainAddresses from "./addresses";
 import { assets } from "./assets";
