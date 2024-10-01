db8089d562e00dac33c75c54577a9c583b1f40aa
Rahul Sethuram <rahul.eth@icloud.com>
2024-10-01 14:04:52 +0400

    fix: dmbtc

--- packages/chains/src/mode/assets.ts
+++ packages/chains/src/mode/assets.ts
@@ -157,7 +157,8 @@ export const assets: SupportedAsset[] = [
     oracleSpecificParams: {
       feed: "0xc9d8b075a5c69303365ae23633d4e085199bf5c520a3b90fed1322a0342ffc33"
     } as PythSpecificParams,
-    extraDocs: defaultDocs("https://explorer.mode.network", dMBTC)
+    extraDocs: defaultDocs("https://explorer.mode.network", dMBTC),
+    initialCf: "72.5"
   },
   {
     symbol: assetSymbols.STONE,
--- packages/contracts/tasks/chain-specific/base/markets.ts
+++ packages/contracts/tasks/chain-specific/base/markets.ts
@@ -35,7 +35,7 @@ task("markets:deploy:base:new", "deploy base market").setAction(async (_, { viem
 });
 
 task("base:set-caps:new", "one time setup").setAction(async (_, { viem, run }) => {
-  const asset = base.assets.find((asset) => asset.symbol === assetSymbols.EURC);
+  const asset = base.assets.find((asset) => asset.symbol === assetSymbols.USDplus);
   if (!asset) {
     throw new Error("OGN not found in base assets");
   }
