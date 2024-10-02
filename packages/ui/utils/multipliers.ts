acfbe5141e08d0e345a85f28fec3887c48502d55
Rahul Sethuram <rahul.eth@icloud.com>
2024-10-02 12:51:37 +0400

    feat: usd+ rewards

--- packages/ui/utils/multipliers.ts
+++ packages/ui/utils/multipliers.ts
@@ -572,6 +572,21 @@ export const multipliers: Record<
           ionAPR: true,
           flywheel: true
         }
+      },
+      'USD+': {
+        borrow: {
+          ionic: 0,
+          turtle: false,
+          rewards: false,
+          ionAPR: false
+        },
+        supply: {
+          ionic: 0,
+          turtle: false,
+          rewards: true,
+          ionAPR: true,
+          flywheel: true
+        }
       }
     }
   },
