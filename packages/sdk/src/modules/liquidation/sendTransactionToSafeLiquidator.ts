acd0fc9d9e9e149aaf234fd43234b80204697d98
Rahul Sethuram <rahul.eth@icloud.com>
2024-10-04 08:07:34 +0400

    fix: lint

--- packages/sdk/src/modules/liquidation/sendTransactionToSafeLiquidator.ts
+++ packages/sdk/src/modules/liquidation/sendTransactionToSafeLiquidator.ts
@@ -1,4 +1,4 @@
-import { SendTransactionReturnType, TransactionReceipt } from "viem";
+import { TransactionReceipt } from "viem";
 
 import { IonicBase } from "../../IonicSdk";
 
@@ -8,30 +8,27 @@ export default async function sendTransactionToSafeLiquidator(
   sdk: IonicBase,
   params: FlashSwapLiquidationTxParams
 ): Promise<TransactionReceipt> {
-  let sentTx: SendTransactionReturnType;
-  // Sign transaction
-  // Send transaction
-    sentTx = await sdk.contracts.IonicLiquidator.write.safeLiquidateToTokensWithFlashLoan(
-      [
-        {
-          borrower: params.borrower,
-          cErc20: params.cErc20,
-          cTokenCollateral: params.cTokenCollateral,
-          debtFundingStrategies: params.debtFundingStrategies,
-          debtFundingStrategiesData: params.debtFundingStrategiesData,
-          flashSwapContract: params.flashSwapContract,
-          minProfitAmount: params.minProfitAmount,
-          redemptionStrategies: params.redemptionStrategies,
-          repayAmount: params.repayAmount,
-          strategyData: params.strategyData
-        }
-      ],
-      { account: sdk.walletClient!.account!.address, chain: sdk.walletClient!.chain }
-    );
-    const receipt = await sdk.publicClient.waitForTransactionReceipt({ hash: sentTx });
-    if (receipt.status === "reverted") {
-      throw `Error sending safeLiquidateToTokensWithFlashLoan transaction: Transaction reverted with status 0`;
-    }
-    sdk.logger.info("Successfully sent safeLiquidateToTokensWithFlashLoan transaction hash:", sentTx);
-    return receipt;
+  const sentTx = await sdk.contracts.IonicLiquidator.write.safeLiquidateToTokensWithFlashLoan(
+    [
+      {
+        borrower: params.borrower,
+        cErc20: params.cErc20,
+        cTokenCollateral: params.cTokenCollateral,
+        debtFundingStrategies: params.debtFundingStrategies,
+        debtFundingStrategiesData: params.debtFundingStrategiesData,
+        flashSwapContract: params.flashSwapContract,
+        minProfitAmount: params.minProfitAmount,
+        redemptionStrategies: params.redemptionStrategies,
+        repayAmount: params.repayAmount,
+        strategyData: params.strategyData
+      }
+    ],
+    { account: sdk.walletClient!.account!.address, chain: sdk.walletClient!.chain }
+  );
+  const receipt = await sdk.publicClient.waitForTransactionReceipt({ hash: sentTx });
+  if (receipt.status === "reverted") {
+    throw `Error sending safeLiquidateToTokensWithFlashLoan transaction: Transaction reverted with status 0`;
   }
+  sdk.logger.info("Successfully sent safeLiquidateToTokensWithFlashLoan transaction hash:", sentTx);
+  return receipt;
+}
