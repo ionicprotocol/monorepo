ad4527f07e38900a723eb97813eccecf55f2f1d5
Rahul Sethuram <rahul.eth@icloud.com>
2024-10-04 14:43:38 +0400

    fix: registry

--- packages/contracts/contracts/security/OracleRegistry.sol
+++ packages/contracts/contracts/security/OracleRegistry.sol
@@ -2,45 +2,56 @@
 pragma solidity ^0.8.22;
 
 import { IHypernativeOracle } from "../external/hypernative/interfaces/IHypernativeOracle.sol";
+import { Ownable2Step } from "@openzeppelin/contracts/access/Ownable2Step.sol";
 
-contract OracleRegistry {
+contract OracleRegistry is Ownable2Step {
   bytes32 private constant HYPERNATIVE_ORACLE_STORAGE_SLOT =
     bytes32(uint256(keccak256("eip1967.hypernative.oracle")) - 1);
-  bytes32 private constant HYPERNATIVE_ADMIN_STORAGE_SLOT =
-    bytes32(uint256(keccak256("eip1967.hypernative.admin")) - 1);
   bytes32 private constant HYPERNATIVE_MODE_STORAGE_SLOT =
     bytes32(uint256(keccak256("eip1967.hypernative.is_strict_mode")) - 1);
 
   event OracleAdminChanged(address indexed previousAdmin, address indexed newAdmin);
   event OracleAddressChanged(address indexed previousOracle, address indexed newOracle);
 
-  function oracleRegister(address _account) public virtual {
+  constructor() Ownable2Step() {}
+
+  function oracleRegister(address _account) public {
     address oracleAddress = _getAddressBySlot(HYPERNATIVE_ORACLE_STORAGE_SLOT);
     IHypernativeOracle oracle = IHypernativeOracle(oracleAddress);
-    if (_hypernativeOracleIsStrictMode()) {
+    if (hypernativeOracleIsStrictMode()) {
       oracle.registerStrict(_account);
     } else {
       oracle.register(_account);
     }
   }
 
+  function setOracle(address _oracle) public onlyOwner {
+    _setOracle(_oracle);
+  }
+
+  function setIsStrictMode(bool _mode) public onlyOwner {
+    _setIsStrictMode(_mode);
+  }
+
+  function hypernativeOracleIsStrictMode() public view returns (bool) {
+    return _getValueBySlot(HYPERNATIVE_MODE_STORAGE_SLOT) == 1;
+  }
+
+  function hypernativeOracle() public view returns (address) {
+    return _getAddressBySlot(HYPERNATIVE_ORACLE_STORAGE_SLOT);
+  }
+
   /**
    * @dev Admin only function, sets new oracle admin. set to address(0) to revoke oracle
    */
   function _setOracle(address _oracle) internal {
-    address oldOracle = _hypernativeOracle();
+    address oldOracle = hypernativeOracle();
     _setAddressBySlot(HYPERNATIVE_ORACLE_STORAGE_SLOT, _oracle);
     emit OracleAddressChanged(oldOracle, _oracle);
   }
 
   function _setIsStrictMode(bool _mode) internal {
     _setValueBySlot(HYPERNATIVE_MODE_STORAGE_SLOT, _mode ? 1 : 0);
-  }
-
-  function _changeOracleAdmin(address _newAdmin) internal {
-    address oldAdmin = _hypernativeOracleAdmin();
-    _setAddressBySlot(HYPERNATIVE_ADMIN_STORAGE_SLOT, _newAdmin);
-    emit OracleAdminChanged(oldAdmin, _newAdmin);
   }
 
   function _setAddressBySlot(bytes32 slot, address newAddress) internal {
@@ -55,14 +66,6 @@ contract OracleRegistry {
     }
   }
 
-  function _hypernativeOracleAdmin() internal view returns (address) {
-    return _getAddressBySlot(HYPERNATIVE_ADMIN_STORAGE_SLOT);
-  }
-
-  function _hypernativeOracleIsStrictMode() internal view returns (bool) {
-    return _getValueBySlot(HYPERNATIVE_MODE_STORAGE_SLOT) == 1;
-  }
-
   function _getAddressBySlot(bytes32 slot) internal view returns (address addr) {
     assembly {
       addr := sload(slot)
@@ -73,9 +76,5 @@ contract OracleRegistry {
     assembly {
       _value := sload(_slot)
     }
-  }
-
-  function _hypernativeOracle() internal view returns (address) {
-    return _getAddressBySlot(HYPERNATIVE_ORACLE_STORAGE_SLOT);
   }
 }
