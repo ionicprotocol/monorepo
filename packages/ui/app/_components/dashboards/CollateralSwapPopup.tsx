e0e059b274fa5f2a25edee17bc9283191a024fa3
Rahul Sethuram <rahul.eth@icloud.com>
2024-10-11 11:48:36 +0400

    fix: console

--- packages/ui/app/_components/dashboards/CollateralSwapPopup.tsx
+++ packages/ui/app/_components/dashboards/CollateralSwapPopup.tsx
@@ -268,7 +268,6 @@ export default function CollateralSwapPopup({
       swapFromAmount,
       swappedFromAsset.underlyingDecimals
     );
-    console.log('🚀 ~ handleSwitch ~ _amount:', _amount);
 
     addStepsForAction([
       {
