01bdca3d9d7b50d96494a6d60b8d3c87040601be
Veliko Minkov <2662912+vminkov@users.noreply.github.com>
2024-10-20 14:50:46 +0300

    supply vaults almost fully compiling

--- packages/contracts/contracts/ionic/strategies/CompoundMarketERC4626.sol
+++ packages/contracts/contracts/ionic/strategies/CompoundMarketERC4626.sol
@@ -1,176 +1,176 @@
-//// SPDX-License-Identifier: UNLICENSED
-//pragma solidity ^0.8.10;
-//
-//import { IonicERC4626 } from "./IonicERC4626.sol";
-//import { ICErc20 } from "../../compound/CTokenInterfaces.sol";
-//import { IonicComptroller } from "../../compound/ComptrollerInterface.sol";
-//import { IGenericLender } from "../../external/angle/IGenericLender.sol";
-//import { OptimizedVaultsRegistry } from "../vault/OptimizedVaultsRegistry.sol";
-//import { OptimizedAPRVaultBase } from "../vault/OptimizedAPRVaultBase.sol";
-//import { IonicFlywheel } from "./flywheel/IonicFlywheel.sol";
-//
-//import { ERC20Upgradeable } from "openzeppelin-contracts-upgradeable/contracts/token/ERC20/ERC20Upgradeable.sol";
-//import { ERC20 } from "solmate/tokens/ERC20.sol";
-//
-//contract CompoundMarketERC4626 is IonicERC4626, IGenericLender {
-//  ICErc20 public market;
-//  uint256 public blocksPerYear;
-//  OptimizedVaultsRegistry public registry;
-//
-//  event ClaimedVaultRewards(address indexed rewardToken, address indexed vault, uint256 amount);
-//
-//  modifier onlyRegisteredVaults() {
-//    OptimizedAPRVaultBase[] memory vaults = registry.getAllVaults();
-//    bool isMsgSender = false;
-//    for (uint256 i = 0; i < vaults.length; i++) {
-//      if (msg.sender == address(vaults[i])) {
-//        isMsgSender = true;
-//        break;
-//      }
-//    }
-//    require(isMsgSender, "!caller not a vault");
-//    _;
-//  }
-//
-//  constructor() {
-//    _disableInitializers();
-//  }
-//
-//  function initialize(
-//    ICErc20 market_,
-//    uint256 blocksPerYear_,
-//    OptimizedVaultsRegistry registry_
-//  ) public initializer {
-//    __MidasER4626_init(ERC20Upgradeable(market_.underlying()));
-//    market = market_;
-//    blocksPerYear = blocksPerYear_;
-//    registry = registry_;
-//  }
-//
-//  function reinitialize(address registry_) public onlyOwnerOrAdmin {
-//    registry = OptimizedVaultsRegistry(registry_);
-//  }
-//
-//  function lenderName() public view returns (string memory) {
-//    return string(bytes.concat("Midas Optimized ", bytes(name())));
-//  }
-//
-//  function totalAssets() public view override returns (uint256) {
-//    return market.balanceOfUnderlying(address(this));
-//  }
-//
-//  function balanceOfUnderlying(address account) public view returns (uint256) {
-//    return convertToAssets(balanceOf(account));
-//  }
-//
-//  function afterDeposit(uint256 amount, uint256) internal override onlyRegisteredVaults {
-//    ERC20Upgradeable(asset()).approve(address(market), amount);
-//    require(market.mint(amount) == 0, "deposit to market failed");
-//  }
-//
-//  function beforeWithdraw(uint256 amount, uint256) internal override onlyRegisteredVaults {
-//    require(market.redeemUnderlying(amount) == 0, "redeem from market failed");
-//  }
-//
-//  // TODO rewards APY?
-//  function aprAfterDeposit(uint256 amount) public view returns (uint256) {
-//    return market.supplyRatePerBlockAfterDeposit(amount) * blocksPerYear;
-//  }
-//
-//  function aprAfterWithdraw(uint256 amount) public view override returns (uint256) {
-//    return market.supplyRatePerBlockAfterWithdraw(amount) * blocksPerYear;
-//  }
-//
-//  function emergencyWithdrawAndPause() external override {
-//    require(msg.sender == owner() || msg.sender == address(registry), "not owner or vaults registry");
-//    require(market.redeemUnderlying(type(uint256).max) == 0, "redeem all failed");
-//    _pause();
-//  }
-//
-//  function unpause() external override onlyOwner {
-//    _unpause();
-//  }
-//
-//  /*------------------------------------------------------------
-//                        IGenericLender FNs
-//    ------------------------------------------------------------*/
-//
-//  /// @notice Returns an estimation of the current Annual Percentage Rate on the lender
-//  function apr() public view override returns (uint256) {
-//    return market.supplyRatePerBlock() * blocksPerYear;
-//  }
-//
-//  /// @notice Returns an estimation of the current Annual Percentage Rate weighted by the assets under
-//  /// management of the lender
-//  function weightedApr() external view returns (uint256) {
-//    return (apr() * totalAssets()) / 1e18;
-//  }
-//
-//  /// @notice Returns an estimation of the hypothetical Annual Percentage Rate weighted by the assets under
-//  /// management of the lender plus the amount, if deposited
-//  function weightedAprAfterDeposit(uint256 amount) public view returns (uint256) {
-//    return (aprAfterDeposit(amount) * (totalAssets() + amount)) / 1e18;
-//  }
-//
-//  /// @notice Withdraws a given amount from lender
-//  /// @param amount The amount the caller wants to withdraw
-//  /// @return Amount actually withdrawn
-//  function withdraw(uint256 amount) public override returns (uint256) {
-//    withdraw(amount, msg.sender, msg.sender);
-//    return amount;
-//  }
-//
-//  /// @notice Withdraws as much as possible from the lending platform
-//  /// @return Whether everything was withdrawn or not
-//  function withdrawAll() public override returns (bool) {
-//    return withdraw(maxWithdraw(msg.sender), msg.sender, msg.sender) > 0;
-//  }
-//
-//  /// @notice Removes tokens from this Strategy that are not the type of tokens
-//  /// managed by this Strategy. This may be used in case of accidentally
-//  /// sending the wrong kind of token to this Strategy.
-//  ///
-//  /// @param _token The token to transfer out of this poolManager.
-//  /// @param to Address to send the tokens to.
-//  function sweep(address _token, address to) public onlyOwner {
-//    require(_token != asset(), "!asset");
-//
-//    ERC20Upgradeable token = ERC20Upgradeable(_token);
-//    token.transfer(to, token.balanceOf(address(this)));
-//  }
-//
-//  function claimRewards() public onlyRegisteredVaults {
-//    IonicComptroller pool = IonicComptroller(market.comptroller());
-//    address[] memory poolFlywheels = pool.getRewardsDistributors();
-//
-//    for (uint256 j = 0; j < poolFlywheels.length; j++) {
-//      IonicFlywheel flywheel = IonicFlywheel(poolFlywheels[j]);
-//      ERC20 rewardToken = flywheel.rewardToken();
-//
-//      // accrue and claim the rewards
-//      flywheel.accrue(ERC20(address(market)), address(this));
-//      flywheel.claimRewards(address(this));
-//
-//      uint256 totalRewards = rewardToken.balanceOf(address(this));
-//      // avoid rounding errors for too little amounts
-//      if (totalRewards > 1000) {
-//        // the rewards that are in the underlying asset are autocompounded
-//        if (address(rewardToken) == address(asset())) {
-//          afterDeposit(totalRewards, 0);
-//        } else {
-//          // redistribute the claimed rewards among the vaults
-//          OptimizedAPRVaultBase[] memory vaults = registry.getAllVaults();
-//          for (uint256 i = 0; i < vaults.length; i++) {
-//            address vaultAddress = address(vaults[i]);
-//            uint256 vaultSharesInAdapter = balanceOf(vaultAddress);
-//            uint256 vaultShareOfRewards = (vaultSharesInAdapter * totalRewards) / totalSupply();
-//            if (vaultShareOfRewards > 0) {
-//              rewardToken.transfer(vaultAddress, vaultShareOfRewards);
-//              emit ClaimedVaultRewards(address(rewardToken), vaultAddress, vaultShareOfRewards);
-//            }
-//          }
-//        }
-//      }
-//    }
-//  }
-//}
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+import { IonicERC4626 } from "./IonicERC4626.sol";
+import { ICErc20 } from "../../compound/CTokenInterfaces.sol";
+import { IonicComptroller } from "../../compound/ComptrollerInterface.sol";
+import { IGenericLender } from "../../external/angle/IGenericLender.sol";
+import { OptimizedVaultsRegistry } from "../vault/OptimizedVaultsRegistry.sol";
+import { OptimizedAPRVaultBase } from "../vault/OptimizedAPRVaultBase.sol";
+import { IonicFlywheel } from "./flywheel/IonicFlywheel.sol";
+
+import { ERC20Upgradeable } from "openzeppelin-contracts-upgradeable/contracts/token/ERC20/ERC20Upgradeable.sol";
+import { ERC20 } from "solmate/tokens/ERC20.sol";
+
+contract CompoundMarketERC4626 is IonicERC4626, IGenericLender {
+  ICErc20 public market;
+  uint256 public blocksPerYear;
+  OptimizedVaultsRegistry public registry;
+
+  event ClaimedVaultRewards(address indexed rewardToken, address indexed vault, uint256 amount);
+
+  modifier onlyRegisteredVaults() {
+    OptimizedAPRVaultBase[] memory vaults = registry.getAllVaults();
+    bool isMsgSender = false;
+    for (uint256 i = 0; i < vaults.length; i++) {
+      if (msg.sender == address(vaults[i])) {
+        isMsgSender = true;
+        break;
+      }
+    }
+    require(isMsgSender, "!caller not a vault");
+    _;
+  }
+
+  constructor() {
+    _disableInitializers();
+  }
+
+  function initialize(
+    ICErc20 market_,
+    uint256 blocksPerYear_,
+    OptimizedVaultsRegistry registry_
+  ) public initializer {
+    __IonicER4626_init(ERC20Upgradeable(market_.underlying()));
+    market = market_;
+    blocksPerYear = blocksPerYear_;
+    registry = registry_;
+  }
+
+  function reinitialize(address registry_) public onlyOwnerOrAdmin {
+    registry = OptimizedVaultsRegistry(registry_);
+  }
+
+  function lenderName() public view returns (string memory) {
+    return string(bytes.concat("Ionic Optimized ", bytes(name())));
+  }
+
+  function totalAssets() public view override returns (uint256) {
+    return market.balanceOfUnderlying(address(this));
+  }
+
+  function balanceOfUnderlying(address account) public view returns (uint256) {
+    return convertToAssets(balanceOf(account));
+  }
+
+  function afterDeposit(uint256 amount, uint256) internal override onlyRegisteredVaults {
+    ERC20Upgradeable(asset()).approve(address(market), amount);
+    require(market.mint(amount) == 0, "deposit to market failed");
+  }
+
+  function beforeWithdraw(uint256 amount, uint256) internal override onlyRegisteredVaults {
+    require(market.redeemUnderlying(amount) == 0, "redeem from market failed");
+  }
+
+  // TODO rewards APY?
+  function aprAfterDeposit(uint256 amount) public view returns (uint256) {
+    return market.supplyRatePerBlockAfterDeposit(amount) * blocksPerYear;
+  }
+
+  function aprAfterWithdraw(uint256 amount) public view override returns (uint256) {
+    return market.supplyRatePerBlockAfterWithdraw(amount) * blocksPerYear;
+  }
+
+  function emergencyWithdrawAndPause() external override {
+    require(msg.sender == owner() || msg.sender == address(registry), "not owner or vaults registry");
+    require(market.redeemUnderlying(type(uint256).max) == 0, "redeem all failed");
+    _pause();
+  }
+
+  function unpause() external override onlyOwner {
+    _unpause();
+  }
+
+  /*------------------------------------------------------------
+                        IGenericLender FNs
+    ------------------------------------------------------------*/
+
+  /// @notice Returns an estimation of the current Annual Percentage Rate on the lender
+  function apr() public view override returns (uint256) {
+    return market.supplyRatePerBlock() * blocksPerYear;
+  }
+
+  /// @notice Returns an estimation of the current Annual Percentage Rate weighted by the assets under
+  /// management of the lender
+  function weightedApr() external view returns (uint256) {
+    return (apr() * totalAssets()) / 1e18;
+  }
+
+  /// @notice Returns an estimation of the hypothetical Annual Percentage Rate weighted by the assets under
+  /// management of the lender plus the amount, if deposited
+  function weightedAprAfterDeposit(uint256 amount) public view returns (uint256) {
+    return (aprAfterDeposit(amount) * (totalAssets() + amount)) / 1e18;
+  }
+
+  /// @notice Withdraws a given amount from lender
+  /// @param amount The amount the caller wants to withdraw
+  /// @return Amount actually withdrawn
+  function withdraw(uint256 amount) public override returns (uint256) {
+    withdraw(amount, msg.sender, msg.sender);
+    return amount;
+  }
+
+  /// @notice Withdraws as much as possible from the lending platform
+  /// @return Whether everything was withdrawn or not
+  function withdrawAll() public override returns (bool) {
+    return withdraw(maxWithdraw(msg.sender), msg.sender, msg.sender) > 0;
+  }
+
+  /// @notice Removes tokens from this Strategy that are not the type of tokens
+  /// managed by this Strategy. This may be used in case of accidentally
+  /// sending the wrong kind of token to this Strategy.
+  ///
+  /// @param _token The token to transfer out of this poolManager.
+  /// @param to Address to send the tokens to.
+  function sweep(address _token, address to) public onlyOwner {
+    require(_token != asset(), "!asset");
+
+    ERC20Upgradeable token = ERC20Upgradeable(_token);
+    token.transfer(to, token.balanceOf(address(this)));
+  }
+
+  function claimRewards() public onlyRegisteredVaults {
+    IonicComptroller pool = IonicComptroller(market.comptroller());
+    address[] memory poolFlywheels = pool.getRewardsDistributors();
+
+    for (uint256 j = 0; j < poolFlywheels.length; j++) {
+      IonicFlywheel flywheel = IonicFlywheel(poolFlywheels[j]);
+      ERC20 rewardToken = flywheel.rewardToken();
+
+      // accrue and claim the rewards
+      flywheel.accrue(ERC20(address(market)), address(this));
+      flywheel.claimRewards(address(this));
+
+      uint256 totalRewards = rewardToken.balanceOf(address(this));
+      // avoid rounding errors for too little amounts
+      if (totalRewards > 1000) {
+        // the rewards that are in the underlying asset are autocompounded
+        if (address(rewardToken) == address(asset())) {
+          afterDeposit(totalRewards, 0);
+        } else {
+          // redistribute the claimed rewards among the vaults
+          OptimizedAPRVaultBase[] memory vaults = registry.getAllVaults();
+          for (uint256 i = 0; i < vaults.length; i++) {
+            address vaultAddress = address(vaults[i]);
+            uint256 vaultSharesInAdapter = balanceOf(vaultAddress);
+            uint256 vaultShareOfRewards = (vaultSharesInAdapter * totalRewards) / totalSupply();
+            if (vaultShareOfRewards > 0) {
+              rewardToken.transfer(vaultAddress, vaultShareOfRewards);
+              emit ClaimedVaultRewards(address(rewardToken), vaultAddress, vaultShareOfRewards);
+            }
+          }
+        }
+      }
+    }
+  }
+}
--- packages/contracts/contracts/ionic/vault/OptimizedAPRVaultFirstExtension.sol
+++ packages/contracts/contracts/ionic/vault/OptimizedAPRVaultFirstExtension.sol
@@ -2,13 +2,13 @@
 pragma solidity ^0.8.10;
 
 import "./OptimizedAPRVaultExtension.sol";
-//import { OptimizedVaultsRegistry } from "./OptimizedVaultsRegistry.sol";
+import { OptimizedVaultsRegistry } from "./OptimizedVaultsRegistry.sol";
 import { IonicFlywheel } from "../strategies/flywheel/IonicFlywheel.sol";
-//import { AdapterConfig } from "./OptimizedAPRVaultStorage.sol";
+import { AdapterConfig } from "./OptimizedAPRVaultStorage.sol";
 import { VaultFees } from "./IVault.sol";
 
 import { ERC20 } from "solmate/tokens/ERC20.sol";
-//import { FuseFlywheelDynamicRewards } from "fuse-flywheel/rewards/FuseFlywheelDynamicRewards.sol";
+import { FuseFlywheelDynamicRewards } from "fuse-flywheel/rewards/FuseFlywheelDynamicRewards.sol";
 import { IFlywheelBooster } from "flywheel/interfaces/IFlywheelBooster.sol";
 import { IFlywheelRewards } from "flywheel/interfaces/IFlywheelRewards.sol";
 import { FlywheelCore } from "flywheel/FlywheelCore.sol";
@@ -19,161 +19,161 @@ import { IERC20Upgradeable as IERC20 } from "openzeppelin-contracts-upgradeable/
 import { IERC20MetadataUpgradeable as IERC20Metadata } from "openzeppelin-contracts-upgradeable/contracts/token/ERC20/extensions/ERC4626Upgradeable.sol";
 import { MathUpgradeable as Math } from "openzeppelin-contracts-upgradeable/contracts/utils/math/MathUpgradeable.sol";
 
-//contract OptimizedAPRVaultFirstExtension is OptimizedAPRVaultExtension {
-//  using SafeERC20 for IERC20;
-//  using Math for uint256;
-//
-//  error AssetInvalid();
-//  error InvalidConfig();
-//
-//  constructor() {
-//    _disableInitializers();
-//  }
-//
-//  function _getExtensionFunctions() external pure virtual override returns (bytes4[] memory) {
-//    uint8 fnsCount = 6;
-//    bytes4[] memory functionSelectors = new bytes4[](fnsCount);
-//    functionSelectors[--fnsCount] = this.initialize.selector;
-//    functionSelectors[--fnsCount] = this.proposeAdapters.selector;
-//    functionSelectors[--fnsCount] = this.getAllFlywheels.selector;
-//    functionSelectors[--fnsCount] = this.addRewardToken.selector;
-//    functionSelectors[--fnsCount] = this.claimRewards.selector;
-//    functionSelectors[--fnsCount] = this.claimRewardsForUser.selector;
-//
-//    require(fnsCount == 0, "use the correct array length");
-//    return functionSelectors;
-//  }
-//
-//  function initialize(bytes calldata data) public initializer {
-//    require(msg.sender == address(this), "!not self call");
-//
-//    (
-//      IERC20 asset_,
-//      AdapterConfig[10] memory adapters_,
-//      uint8 adaptersCount_,
-//      VaultFees memory fees_,
-//      address feeRecipient_,
-//      uint256 depositLimit_,
-//      OptimizedVaultsRegistry registry_,
-//      address flywheelLogic_
-//    ) = abi.decode(
-//        data,
-//        (IERC20, AdapterConfig[10], uint8, VaultFees, address, uint256, OptimizedVaultsRegistry, address)
-//      );
-//
-//    if (address(asset_) == address(0)) revert AssetInvalid();
-//    __ERC4626_init(asset_);
-//
-//    _name = string(bytes.concat("Midas Optimized ", bytes(IERC20Metadata(address(asset_)).name()), " Vault"));
-//    _symbol = string(bytes.concat("mo-", bytes(IERC20Metadata(address(asset_)).symbol())));
-//    _decimals = IERC20Metadata(address(asset_)).decimals() + DECIMAL_OFFSET; // Asset decimals + decimal offset to combat inflation attacks
-//
-//    depositLimit = depositLimit_;
-//    registry = registry_;
-//    flywheelLogic = flywheelLogic_;
-//    INITIAL_CHAIN_ID = block.chainid;
-//    INITIAL_DOMAIN_SEPARATOR = computeDomainSeparator();
-//    feesUpdatedAt = block.timestamp;
-//    highWaterMark = 1e9;
-//    quitPeriod = 3 days;
-//
-//    // vault fees
-//    if (fees_.deposit >= 1e18 || fees_.withdrawal >= 1e18 || fees_.management >= 1e18 || fees_.performance >= 1e18)
-//      revert InvalidVaultFees();
-//    fees = fees_;
-//
-//    // fee recipient
-//    if (feeRecipient_ == address(0)) revert InvalidFeeRecipient();
-//    feeRecipient = feeRecipient_;
-//
-//    // adapters config
-//    _verifyAdapterConfig(adapters_, adaptersCount_);
-//    adaptersCount = adaptersCount_;
-//    for (uint8 i; i < adaptersCount_; i++) {
-//      adapters[i] = adapters_[i];
-//      asset_.approve(address(adapters_[i].adapter), type(uint256).max);
-//    }
-//  }
-//
-//  function _verifyAdapterConfig(AdapterConfig[10] memory newAdapters, uint8 adapterCount_) internal view {
-//    if (adapterCount_ == 0 || adapterCount_ > 10) revert InvalidConfig();
-//
-//    uint256 totalAllocation;
-//    for (uint8 i; i < adapterCount_; i++) {
-//      if (newAdapters[i].adapter.asset() != asset()) revert AssetInvalid();
-//
-//      uint256 allocation = uint256(newAdapters[i].allocation);
-//      if (allocation == 0) revert InvalidConfig();
-//
-//      totalAllocation += allocation;
-//    }
-//    if (totalAllocation != 1e18) revert InvalidConfig();
-//  }
-//
-//  /*------------------------------------------------------------
-//                            ADAPTER LOGIC
-//    ------------------------------------------------------------*/
-//
-//  event NewAdaptersProposed(AdapterConfig[10] newAdapter, uint8 adaptersCount, uint256 timestamp);
-//
-//  /**
-//   * @notice Propose a new adapter for this vault. Caller must be Owner.
-//   * @param newAdapters A new ERC4626 that should be used as a yield adapter for this asset.
-//   * @param newAdaptersCount Amount of new adapters.
-//   */
-//  function proposeAdapters(AdapterConfig[10] calldata newAdapters, uint8 newAdaptersCount) external onlyOwner {
-//    _verifyAdapterConfig(newAdapters, newAdaptersCount);
-//
-//    for (uint8 i; i < newAdaptersCount; i++) {
-//      proposedAdapters[i] = newAdapters[i];
-//    }
-//
-//    proposedAdaptersCount = newAdaptersCount;
-//
-//    proposedAdapterTime = block.timestamp;
-//
-//    emit NewAdaptersProposed(newAdapters, proposedAdaptersCount, block.timestamp);
-//  }
-//
-//  function getAllFlywheels() external view returns (IonicFlywheel[] memory allFlywheels) {
-//    allFlywheels = new IonicFlywheel[](rewardTokens.length);
-//    for (uint256 i = 0; i < rewardTokens.length; i++) {
-//      allFlywheels[i] = flywheelForRewardToken[rewardTokens[i]];
-//    }
-//  }
-//
-//  /// @notice claim all token rewards
-//  function claimRewards() public {
-//    _claimRewards(msg.sender);
-//  }
-//
-//  function claimRewardsForUser(address user) public {
-//    _claimRewards(user);
-//  }
-//
-//  function _claimRewards(address user) internal {
-//    for (uint256 i = 0; i < rewardTokens.length; i++) {
-//      IonicFlywheel flywheel = flywheelForRewardToken[rewardTokens[i]];
-//      flywheel.accrue(ERC20(address(this)), user);
-//      flywheel.claimRewards(user);
-//    }
-//  }
-//
-//  function _afterTokenTransfer(
-//    address from,
-//    address to,
-//    uint256 amount
-//  ) internal override {
-//    super._afterTokenTransfer(from, to, amount);
-//    for (uint256 i; i < rewardTokens.length; ++i) {
-//      flywheelForRewardToken[rewardTokens[i]].accrue(ERC20(address(this)), from, to);
-//    }
-//  }
-//
-//  function addRewardToken(IERC20 token_) public {
-//    require(msg.sender == owner() || msg.sender == address(this), "!owner or self");
-//    require(address(flywheelForRewardToken[token_]) == address(0), "already added");
-//
+contract OptimizedAPRVaultFirstExtension is OptimizedAPRVaultExtension {
+  using SafeERC20 for IERC20;
+  using Math for uint256;
+
+  error AssetInvalid();
+  error InvalidConfig();
+
+  constructor() {
+    _disableInitializers();
+  }
+
+  function _getExtensionFunctions() external pure virtual override returns (bytes4[] memory) {
+    uint8 fnsCount = 6;
+    bytes4[] memory functionSelectors = new bytes4[](fnsCount);
+    functionSelectors[--fnsCount] = this.initialize.selector;
+    functionSelectors[--fnsCount] = this.proposeAdapters.selector;
+    functionSelectors[--fnsCount] = this.getAllFlywheels.selector;
+    functionSelectors[--fnsCount] = this.addRewardToken.selector;
+    functionSelectors[--fnsCount] = this.claimRewards.selector;
+    functionSelectors[--fnsCount] = this.claimRewardsForUser.selector;
+
+    require(fnsCount == 0, "use the correct array length");
+    return functionSelectors;
+  }
+
+  function initialize(bytes calldata data) public initializer {
+    require(msg.sender == address(this), "!not self call");
+
+    (
+      IERC20 asset_,
+      AdapterConfig[10] memory adapters_,
+      uint8 adaptersCount_,
+      VaultFees memory fees_,
+      address feeRecipient_,
+      uint256 depositLimit_,
+      OptimizedVaultsRegistry registry_,
+      address flywheelLogic_
+    ) = abi.decode(
+        data,
+        (IERC20, AdapterConfig[10], uint8, VaultFees, address, uint256, OptimizedVaultsRegistry, address)
+      );
+
+    if (address(asset_) == address(0)) revert AssetInvalid();
+    __ERC4626_init(asset_);
+
+    _name = string(bytes.concat("Midas Optimized ", bytes(IERC20Metadata(address(asset_)).name()), " Vault"));
+    _symbol = string(bytes.concat("mo-", bytes(IERC20Metadata(address(asset_)).symbol())));
+    _decimals = IERC20Metadata(address(asset_)).decimals() + DECIMAL_OFFSET; // Asset decimals + decimal offset to combat inflation attacks
+
+    depositLimit = depositLimit_;
+    registry = registry_;
+    flywheelLogic = flywheelLogic_;
+    INITIAL_CHAIN_ID = block.chainid;
+    INITIAL_DOMAIN_SEPARATOR = computeDomainSeparator();
+    feesUpdatedAt = block.timestamp;
+    highWaterMark = 1e9;
+    quitPeriod = 3 days;
+
+    // vault fees
+    if (fees_.deposit >= 1e18 || fees_.withdrawal >= 1e18 || fees_.management >= 1e18 || fees_.performance >= 1e18)
+      revert InvalidVaultFees();
+    fees = fees_;
+
+    // fee recipient
+    if (feeRecipient_ == address(0)) revert InvalidFeeRecipient();
+    feeRecipient = feeRecipient_;
+
+    // adapters config
+    _verifyAdapterConfig(adapters_, adaptersCount_);
+    adaptersCount = adaptersCount_;
+    for (uint8 i; i < adaptersCount_; i++) {
+      adapters[i] = adapters_[i];
+      asset_.approve(address(adapters_[i].adapter), type(uint256).max);
+    }
+  }
+
+  function _verifyAdapterConfig(AdapterConfig[10] memory newAdapters, uint8 adapterCount_) internal view {
+    if (adapterCount_ == 0 || adapterCount_ > 10) revert InvalidConfig();
+
+    uint256 totalAllocation;
+    for (uint8 i; i < adapterCount_; i++) {
+      if (newAdapters[i].adapter.asset() != asset()) revert AssetInvalid();
+
+      uint256 allocation = uint256(newAdapters[i].allocation);
+      if (allocation == 0) revert InvalidConfig();
+
+      totalAllocation += allocation;
+    }
+    if (totalAllocation != 1e18) revert InvalidConfig();
+  }
+
+  /*------------------------------------------------------------
+                            ADAPTER LOGIC
+    ------------------------------------------------------------*/
+
+  event NewAdaptersProposed(AdapterConfig[10] newAdapter, uint8 adaptersCount, uint256 timestamp);
+
+  /**
+   * @notice Propose a new adapter for this vault. Caller must be Owner.
+   * @param newAdapters A new ERC4626 that should be used as a yield adapter for this asset.
+   * @param newAdaptersCount Amount of new adapters.
+   */
+  function proposeAdapters(AdapterConfig[10] calldata newAdapters, uint8 newAdaptersCount) external onlyOwner {
+    _verifyAdapterConfig(newAdapters, newAdaptersCount);
+
+    for (uint8 i; i < newAdaptersCount; i++) {
+      proposedAdapters[i] = newAdapters[i];
+    }
+
+    proposedAdaptersCount = newAdaptersCount;
+
+    proposedAdapterTime = block.timestamp;
+
+    emit NewAdaptersProposed(newAdapters, proposedAdaptersCount, block.timestamp);
+  }
+
+  function getAllFlywheels() external view returns (IonicFlywheel[] memory allFlywheels) {
+    allFlywheels = new IonicFlywheel[](rewardTokens.length);
+    for (uint256 i = 0; i < rewardTokens.length; i++) {
+      allFlywheels[i] = flywheelForRewardToken[rewardTokens[i]];
+    }
+  }
+
+  /// @notice claim all token rewards
+  function claimRewards() public {
+    _claimRewards(msg.sender);
+  }
+
+  function claimRewardsForUser(address user) public {
+    _claimRewards(user);
+  }
+
+  function _claimRewards(address user) internal {
+    for (uint256 i = 0; i < rewardTokens.length; i++) {
+      IonicFlywheel flywheel = flywheelForRewardToken[rewardTokens[i]];
+      flywheel.accrue(ERC20(address(this)), user);
+      flywheel.claimRewards(user);
+    }
+  }
+
+  function _afterTokenTransfer(
+    address from,
+    address to,
+    uint256 amount
+  ) internal override {
+    super._afterTokenTransfer(from, to, amount);
+    for (uint256 i; i < rewardTokens.length; ++i) {
+      flywheelForRewardToken[rewardTokens[i]].accrue(ERC20(address(this)), from, to);
+    }
+  }
+
+  function addRewardToken(IERC20 token_) public {
+    require(msg.sender == owner() || msg.sender == address(this), "!owner or self");
+    require(address(flywheelForRewardToken[token_]) == address(0), "already added");
+
 //    TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy(flywheelLogic, owner(), "");
 //    IonicFlywheel newFlywheel = IonicFlywheel(address(proxy));
 //
@@ -197,5 +197,5 @@ import { MathUpgradeable as Math } from "openzeppelin-contracts-upgradeable/cont
 //    newFlywheel.addStrategyForRewards(ERC20(address(this)));
 //    flywheelForRewardToken[token_] = newFlywheel;
 //    rewardTokens.push(token_);
-//  }
-//}
+  }
+}
