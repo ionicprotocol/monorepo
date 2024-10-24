de47599d82102c202330aed799715633464ebff2
Rahul Sethuram <rahul.eth@icloud.com>
2024-10-24 15:34:00 +0400

    fix: dont approve unless needed

--- packages/ui/app/xION/page.tsx
+++ packages/ui/app/xION/page.tsx
@@ -1,13 +1,19 @@
 /* eslint-disable @next/next/no-img-element */
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import dynamic from 'next/dynamic';
 import { useSearchParams } from 'next/navigation';
 
 import { xErc20LayerZeroAbi } from 'sdk/src';
-import { erc20Abi, formatEther, parseEther, parseUnits } from 'viem';
+import {
+  erc20Abi,
+  formatEther,
+  parseEther,
+  parseUnits,
+  zeroAddress
+} from 'viem';
 import { mode } from 'viem/chains';
 import {
   useAccount,
@@ -49,6 +55,12 @@ export default function XION() {
   const [destinationAddress, setDestinationAddress] = useState<
     Address | undefined
   >(address);
+  const { data: allowance } = useReadContract({
+    abi: erc20Abi,
+    address: getToken(+chain),
+    functionName: 'allowance',
+    args: [address ?? zeroAddress, BridgingContractAddress[+chain]]
+  });
   const { data: sourceLimits } = useReadContract({
     abi: ixErc20,
     address: getToken(+chain),
@@ -112,8 +124,11 @@ export default function XION() {
     bridgeStatus: 'unknown'
   });
 
-  // console.log(bridgeArgs);
-  // const [, setInit] = useLocalStorage('bridgeTx', '');
+  useEffect(() => {
+    if (address && allowance && allowance >= parseEther(deposit)) {
+      setProgress(2);
+    }
+  }, [allowance, deposit, address]);
 
   async function approval(amount: bigint) {
     try {
