dd3d91a3fb27571de90e2b067594c2e73ae4547f
vidvidvid <weetopol@gmail.com>
2024-10-30 11:12:14 +0100

    fix upcoming chains images

--- packages/ui/app/market/page.tsx
+++ packages/ui/app/market/page.tsx
@@ -150,7 +150,7 @@ export default function Market() {
               'Ink',
               'Kroma',
               'Unichain',
-              'World'
+              'Worldchain'
             ]}
           />
         </div>
