e19fa13fbdfb690d6835bea5945584b9bb2538ef
Jourdan Dunkley <chasejamaica@yahoo.com>
2024-11-05 10:38:12 -0500

    min lock duration now variable

--- packages/contracts/contracts/veION/veION.sol
+++ packages/contracts/contracts/veION/veION.sol
@@ -26,7 +26,6 @@ contract veION is Ownable2StepUpgradeable, ERC721Upgradeable, IveION {
   uint256 internal constant MAXTIME = 2 * 365 * 86400;
   uint256 internal constant MULTIPLIER = 1 ether;
   uint256 public constant PRECISION = 1e18;
-  uint256 internal constant MIN_LOCK_DURATION = 180 * 86400;
 
   // State Variables
   uint256 public s_tokenId;
@@ -39,6 +38,7 @@ contract veION is Ownable2StepUpgradeable, ERC721Upgradeable, IveION {
   address public s_voter;
   uint256 public s_aeroVoterBoost;
   uint256 public s_minimumAeroContribution;
+  uint256 public s_minimumLockDuration;
   AddressesProvider public ap;
 
   // Mappings
@@ -154,7 +154,7 @@ contract veION is Ownable2StepUpgradeable, ERC721Upgradeable, IveION {
 
     if (_tokenAmount < s_minimumLockAmount[lpType]) revert MinimumNotMet();
     if (unlockTime > block.timestamp + MAXTIME) revert LockDurationTooLong();
-    if (_duration < MIN_LOCK_DURATION) revert LockDurationTooShort();
+    if (_duration < s_minimumLockDuration) revert LockDurationTooShort();
 
     if (lockedBalance.isPermanent) {
       s_permanentLockBalance[lpType] += _tokenAmount;
@@ -672,7 +672,7 @@ contract veION is Ownable2StepUpgradeable, ERC721Upgradeable, IveION {
       uint256 unlockTime = ((block.timestamp + _duration[i]) / WEEK) * WEEK;
 
       if (_tokenAmount[i] == 0) revert ZeroAmount();
-      if (_duration[i] < MIN_LOCK_DURATION) revert LockDurationTooShort();
+      if (_duration[i] < s_minimumLockDuration) revert LockDurationTooShort();
       if (unlockTime <= block.timestamp) revert LockDurationNotInFuture();
       if (unlockTime > block.timestamp + MAXTIME) revert LockDurationTooLong();
       if (_tokenAmount[i] < s_minimumLockAmount[_lpType]) revert MinimumNotMet();
@@ -836,6 +836,11 @@ contract veION is Ownable2StepUpgradeable, ERC721Upgradeable, IveION {
 
   function setMinimumAeroContribution(uint256 _minimumAeroContribution) external onlyOwner {
     s_minimumAeroContribution = _minimumAeroContribution;
+  }
+
+  function setMinimumLockDuration(uint256 _minimumLockDuration) external onlyOwner {
+    require(_minimumLockDuration > 0, "Minimum lock duration must be greater than zero");
+    s_minimumLockDuration = _minimumLockDuration;
   }
 
   function setIonicPool(address _ionicPool) external onlyOwner {
