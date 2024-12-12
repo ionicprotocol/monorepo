3ecc446e0b843594644577d67ebfcb47955ea7c4
Rahul Sethuram <rahul.eth@icloud.com>
2024-12-12 13:03:59 +0400

    feat: eoracle

--- packages/contracts/tasks/market/index.ts
+++ packages/contracts/tasks/market/index.ts
@@ -7,3 +7,19 @@ import "./deploy-dynamic-rewards-market";
 import "./deploy-static-rewards-market";
 import "./deploy";
 import "./borrow";
+import { Address } from "viem";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+export const getMarketInfo = async (viem: HardhatRuntimeEnvironment["viem"], _comptroller: Address) => {
+  const comptroller = await viem.getContractAt("IonicComptroller", _comptroller);
+  const cTokens = await comptroller.read.getAllMarkets();
+  for (const cToken of cTokens) {
+    const cTokenContract = await viem.getContractAt("ICErc20", cToken);
+    const symbol = await cTokenContract.read.symbol();
+    const underlying = await cTokenContract.read.underlying();
+    const underlyingContract = await viem.getContractAt("ICErc20", underlying);
+    const underlyingSymbol = await underlyingContract.read.symbol();
+    const underlyingDecimals = await underlyingContract.read.decimals();
+    console.log(`${symbol},${cToken},${underlying},${underlyingSymbol},${underlyingDecimals}`);
+  }
+};
