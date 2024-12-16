81c26dc596454743bee94b55bd0a4b04d7bf18df
amish kohli <amishkohli@amishs-Laptop.local>
2024-12-16 16:35:28 +0530

    mode_updater

--- packages/bots/pyth-updater/src/config/service.ts
+++ packages/bots/pyth-updater/src/config/service.ts
@@ -5,8 +5,8 @@ const config = {
   environment: process.env.NODE_ENV ?? 'development',
   logLevel: process.env.LOG_LEVEL ?? 'info',
   chainId: parseInt(process.env.TARGET_CHAIN_ID ?? '8453', 10),
-  rpcUrls: process.env.BASE_MAINNET_RPCS
-    ? process.env.BASE_MAINNET_RPCS.split(',')
+  rpcUrls: process.env.WEB3_HTTP_PROVIDER_URLS
+    ? process.env.WEB3_HTTP_PROVIDER_URLS.split(',')
     : ['https://base-rpc.publicnode.com'], // Updated to handle multiple RPC URLs
   adminPrivateKey: process.env.PYTH_UPDATER_ETHEREUM_ADMIN_PRIVATE_KEY ?? '',
   adminAccount: process.env.PYTH_UPDATER_ETHEREUM_ADMIN_ACCOUNT ?? '',
