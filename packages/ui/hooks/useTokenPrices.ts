5bdaa5d6934085422a130087ecda9cdcd880b8b6
vidvidvid <weetopol@gmail.com>
2024-12-27 15:49:47 +0100

    transform useAllUsdPrices to supabase
    fix

--- packages/ui/hooks/useTokenPrices.ts
+++ packages/ui/hooks/useTokenPrices.ts
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
+
 import { COINGECKO_API, DEFI_LLAMA_API } from '@ui/constants/index';
 
 export interface TokenConfig {
