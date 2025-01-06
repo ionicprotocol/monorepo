479264513031e04c3c224ce3ea125dac14ca3378
vidvidvid <weetopol@gmail.com>
2025-01-06 12:29:18 +0100

    add coingecko link to footer, improve ui

--- packages/ui/app/layout.tsx
+++ packages/ui/app/layout.tsx
@@ -2,8 +2,6 @@
 
 import { Suspense } from 'react';
 
-import Image from 'next/image';
-import Link from 'next/link';
 import Script from 'next/script';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -11,6 +9,7 @@ import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
 import { Toaster } from 'react-hot-toast';
 import { WagmiProvider } from 'wagmi';
 
+import Footer from '@ui/components/Footer';
 import Navbar from '@ui/components/Navbar';
 import { Toaster as ToastProvider } from '@ui/components/ui/toaster';
 import { TooltipProvider } from '@ui/components/ui/tooltip';
@@ -63,191 +62,7 @@ export default function RootLayout({
                   <div className="relative px-4 overflow-x-hidden pt-24 md:pt-[128px] pb-4 sm:pb-[280px] min-h-screen w-[100vw]">
                     <Navbar />
                     <main>{children}</main>
-                    <footer
-                      className={`sm:absolute bottom-4 right-4 left-4 bg-grayone px-[3%] mt-3 rounded-xl py-4 sm:py-10`}
-                    >
-                      <div className="text-center sm:text-left sm:flex">
-                        <div className="flex-initial sm:mr-20">
-                          <div className="mb-4 sm:mb-20">
-                            <Link
-                              className={`flex justify-center sm:justify-start items-center sm:pr-10`}
-                              href={'https://ionic.money'}
-                              target="_blank"
-                            >
-                              <Image
-                                alt="logo"
-                                className={`h-5 `}
-                                height="20"
-                                src="/img/logo/logo.png"
-                                width="80"
-                              />
-                            </Link>
-                          </div>
-
-                          <div className="flex justify-center sm:justify-start mb-4 sm:mb-0">
-                            <Link
-                              className={`flex items-center  pr-5`}
-                              href={'https://ionic.money'}
-                              target="_blank"
-                            >
-                              <Image
-                                alt="logo"
-                                className={`h-5 `}
-                                height="20"
-                                src="/images/globe.png"
-                                width="20"
-                              />
-                            </Link>
-
-                            <Link
-                              className={`flex items-center  pr-5`}
-                              href={'https://t.me/ionicmoney'}
-                              target="_blank"
-                            >
-                              <Image
-                                alt="logo"
-                                className={`h-5 `}
-                                height="20"
-                                src="/images/tg.png"
-                                width="20"
-                              />
-                            </Link>
-
-                            <Link
-                              className={`flex items-center  pr-5`}
-                              href={'https://twitter.com/ionicmoney'}
-                              target="_blank"
-                            >
-                              <Image
-                                alt="logo"
-                                className={`h-5 `}
-                                height="20"
-                                src="/images/x.png"
-                                width="20"
-                              />
-                            </Link>
-
-                            <Link
-                              className={`flex items-center `}
-                              href={'https://discord.gg/FmgedqR9wn'}
-                              target="_blank"
-                            >
-                              <Image
-                                alt="logo"
-                                className={`h-5 `}
-                                height="20"
-                                src="/images/discord.png"
-                                width="20"
-                              />
-                            </Link>
-                          </div>
-                        </div>
-
-                        <div className="flex-initial sm:mr-20">
-                          <h4 className="text-lg text-bold mb-2">Resources</h4>
-
-                          <ul className="text-sm">
-                            <li className="mb-1">
-                              <a
-                                href="https://doc.ionic.money/ionic-documentation/audit"
-                                target="_blank"
-                                rel="noreferrer"
-                              >
-                                Audit
-                              </a>
-                            </li>
-                            <li className="mb-1">
-                              <a
-                                href="https://doc.ionic.money/"
-                                target="_blank"
-                                rel="noreferrer"
-                              >
-                                Documentation
-                              </a>
-                            </li>
-                            <li className="mb-1">
-                              <a
-                                href="https://github.com/orgs/ionicprotocol/repositories"
-                                target="_blank"
-                                rel="noreferrer"
-                              >
-                                GitHub
-                              </a>
-                            </li>
-                            <li className="mb-1">
-                              <a
-                                href="https://status.ionic.money/"
-                                target="_blank"
-                                rel="noreferrer"
-                              >
-                                Status
-                              </a>
-                            </li>
-                          </ul>
-                        </div>
-
-                        <div className="flex-initial mt-4 sm:mt-0 sm:mr-20">
-                          <h4 className="text-lg text-bold mb-2">Tools</h4>
-
-                          <ul className="text-sm">
-                            <li className="mb-1">
-                              <a
-                                href="https://app.anthias.xyz/protocols/ionic/ionic_v1_mode/positions"
-                                target="_blank"
-                                rel="noreferrer"
-                              >
-                                Analytics
-                              </a>
-                            </li>
-                            <li className="mb-1">
-                              <a
-                                href="https://defillama.com/protocol/ionic-protocol"
-                                target="_blank"
-                                rel="noreferrer"
-                              >
-                                DeFi Llama
-                              </a>
-                            </li>
-                            <li className="mb-1.5">
-                              <a
-                                href="https://dune.com/mrwildcat/ionic-protocol"
-                                target="_blank"
-                                rel="noreferrer"
-                              >
-                                Dune
-                              </a>
-                            </li>
-                            <li className="mb-1.5">
-                              <a
-                                href="https://id.ionic.money/#/"
-                                target="_blank"
-                                rel="noreferrer"
-                              >
-                                ID
-                              </a>
-                            </li>
-                            <li className="mb-1.5">
-                              <a
-                                href="https://linktr.ee/ionicmoney"
-                                target="_blank"
-                                rel="noreferrer"
-                              >
-                                Linktree
-                              </a>
-                            </li>
-                          </ul>
-                        </div>
-
-                        <div className="flex-initial mt-4 sm:mt-0 sm:mr-20">
-                          <Image
-                            alt="logo"
-                            height={30}
-                            src="/img/pyth.svg"
-                            width={100}
-                          />
-                        </div>
-                      </div>
-                    </footer>
+                    <Footer />
                     <ToastProvider />
                     <Toaster
                       toastOptions={{
---
+++ packages/ui/components/Footer.tsx
@@ -0,0 +1,209 @@
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { BookOpen, Wrench } from 'lucide-react';
+
+import { Separator } from '@ui/components/ui/separator';
+
+const Footer = () => {
+  return (
+    <footer className="sm:absolute bottom-4 right-4 left-4 bg-grayone px-6 mt-3 rounded-xl py-3">
+      {/* Logo and Social Links Row */}
+      <div className="flex flex-col sm:flex-row items-center sm:items-start justify-between gap-4 mt-2">
+        <div className="flex items-center gap-6">
+          <Link
+            href="https://ionic.money"
+            target="_blank"
+          >
+            <Image
+              alt="Ionic Logo"
+              className="h-5 w-auto"
+              height="20"
+              src="/img/logo/logo.png"
+              width="80"
+            />
+          </Link>
+
+          <div className="flex items-center gap-4">
+            <Link
+              href="https://ionic.money"
+              target="_blank"
+            >
+              <Image
+                alt="Website"
+                className="h-5 w-5"
+                height="20"
+                src="/images/globe.png"
+                width="20"
+              />
+            </Link>
+            <Link
+              href="https://t.me/ionicmoney"
+              target="_blank"
+            >
+              <Image
+                alt="Telegram"
+                className="h-5 w-5"
+                height="20"
+                src="/images/tg.png"
+                width="20"
+              />
+            </Link>
+            <Link
+              href="https://twitter.com/ionicmoney"
+              target="_blank"
+            >
+              <Image
+                alt="Twitter"
+                className="h-5 w-5"
+                height="20"
+                src="/images/x.png"
+                width="20"
+              />
+            </Link>
+            <Link
+              href="https://discord.gg/FmgedqR9wn"
+              target="_blank"
+            >
+              <Image
+                alt="Discord"
+                className="h-5 w-5"
+                height="20"
+                src="/images/discord.png"
+                width="20"
+              />
+            </Link>
+          </div>
+        </div>
+
+        <Image
+          alt="Pyth Network"
+          className="hidden sm:block"
+          height={24}
+          src="/img/pyth.svg"
+          width={80}
+        />
+      </div>
+
+      <Separator className="my-4" />
+
+      {/* Links Grid */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="space-y-3">
+          <div className="flex items-center gap-2">
+            <BookOpen className="h-4 w-4 text-primary" />
+            <h4 className="text-sm font-medium tracking-wide uppercase">
+              Resources
+            </h4>
+          </div>
+          <div className="grid grid-cols-2 sm:grid-cols-3 gap-x-4 gap-y-1">
+            <a
+              href="https://doc.ionic.money/ionic-documentation/audit"
+              target="_blank"
+              rel="noreferrer"
+              className="text-sm text-muted-foreground hover:text-primary transition-colors"
+            >
+              Audit
+            </a>
+            <a
+              href="https://doc.ionic.money/"
+              target="_blank"
+              rel="noreferrer"
+              className="text-sm text-muted-foreground hover:text-primary transition-colors"
+            >
+              Documentation
+            </a>
+            <a
+              href="https://github.com/orgs/ionicprotocol/repositories"
+              target="_blank"
+              rel="noreferrer"
+              className="text-sm text-muted-foreground hover:text-primary transition-colors"
+            >
+              GitHub
+            </a>
+            <a
+              href="https://status.ionic.money/"
+              target="_blank"
+              rel="noreferrer"
+              className="text-sm text-muted-foreground hover:text-primary transition-colors"
+            >
+              Status
+            </a>
+            <a
+              href="https://linktr.ee/ionicmoney"
+              target="_blank"
+              rel="noreferrer"
+              className="text-sm text-muted-foreground hover:text-primary transition-colors"
+            >
+              Linktree
+            </a>
+            <a
+              href="https://www.coingecko.com/en/coins/ionic-protocol"
+              target="_blank"
+              rel="noreferrer"
+              className="text-sm text-muted-foreground hover:text-primary transition-colors"
+            >
+              CoinGecko
+            </a>
+          </div>
+        </div>
+
+        <div className="space-y-3">
+          <div className="flex items-center gap-2">
+            <Wrench className="h-4 w-4 text-primary" />
+            <h4 className="text-sm font-medium tracking-wide uppercase">
+              Tools & Analytics
+            </h4>
+          </div>
+          <div className="grid grid-cols-2 gap-x-4 gap-y-2">
+            <a
+              href="https://app.anthias.xyz/protocols/ionic/ionic_v1_mode/positions"
+              target="_blank"
+              rel="noreferrer"
+              className="text-sm text-muted-foreground hover:text-primary transition-colors"
+            >
+              Analytics
+            </a>
+            <a
+              href="https://defillama.com/protocol/ionic-protocol"
+              target="_blank"
+              rel="noreferrer"
+              className="text-sm text-muted-foreground hover:text-primary transition-colors"
+            >
+              DeFi Llama
+            </a>
+            <a
+              href="https://dune.com/mrwildcat/ionic-protocol"
+              target="_blank"
+              rel="noreferrer"
+              className="text-sm text-muted-foreground hover:text-primary transition-colors"
+            >
+              Dune
+            </a>
+            <a
+              href="https://id.ionic.money/#/"
+              target="_blank"
+              rel="noreferrer"
+              className="text-sm text-muted-foreground hover:text-primary transition-colors"
+            >
+              ID
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* Mobile Pyth Logo */}
+      <div className="mt-4 flex justify-center sm:hidden">
+        <Image
+          alt="Pyth Network"
+          height={24}
+          src="/img/pyth.svg"
+          width={80}
+        />
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
