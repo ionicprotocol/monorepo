15265437de36f854ebeecd17defca60cc2b1ea0f
Rahul Sethuram <rahul.eth@icloud.com>
2025-01-13 20:21:52 +0400

    feat: rewards

--- packages/contracts/tasks/chain-specific/base/rewards.ts
+++ packages/contracts/tasks/chain-specific/base/rewards.ts
@@ -759,3 +759,44 @@ task("base:send-ion:epoch6", "send ion to a market").setAction(async (_, { viem,
 
   await sendRewardsToMarkets(viem, ION, rewardsToSend, deployer as Address);
 });
+
+task("base:add-rewards:epoch7:supply", "add rewards to a market").setAction(
+  async (_, { viem, deployments, getNamedAccounts }) => {
+    const { deployer, multisig } = await getNamedAccounts();
+    const rewardToken = ION;
+    const rewardTokenName = "ION";
+    const market = KLIMA_MARKET;
+    const _market = await viem.getContractAt("EIP20Interface", market);
+    const name = await _market.read.name();
+
+    const rewardAmount = (2500).toString();
+
+    console.log("setting rewards for token: ", name, rewardAmount);
+    await new Promise((resolve) => setTimeout(resolve, 4000));
+
+    // Sending tokens
+    const _rewardToken = await viem.getContractAt("EIP20Interface", rewardToken);
+    let balance = await _rewardToken.read.balanceOf([market]);
+    console.log("balance: ", balance);
+    if (balance < parseEther(rewardAmount)) {
+      const tx = await _rewardToken.write.transfer([market, parseEther(rewardAmount) - balance]);
+      console.log(`Sent ${rewardAmount} ${rewardTokenName} to ${market} - ${tx}`);
+    } else {
+      console.log(`Market already has enough ${rewardTokenName} - ${market}`);
+    }
+
+    await setupRewards(
+      "supply",
+      market,
+      rewardTokenName,
+      rewardToken,
+      SUPPLY_DURATION,
+      deployer as Address,
+      viem,
+      deployments,
+      multisig as Address,
+      "IonicFlywheel_ION_epoch7",
+      "IonicFlywheelDynamicRewards_ION_epoch7"
+    );
+  }
+);
