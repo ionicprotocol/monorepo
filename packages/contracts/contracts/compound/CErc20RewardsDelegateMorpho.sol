824639eb661c6a2354f367875b6e7564f6c7f245
Ante Sosa <antesosa95@gmail.com>
2025-01-21 16:56:46 +0100

    update morpho market contract

--- packages/contracts/contracts/compound/CErc20RewardsDelegateMorpho.sol
+++ packages/contracts/contracts/compound/CErc20RewardsDelegateMorpho.sol
@@ -4,8 +4,6 @@ pragma solidity >=0.8.0;
 import "./CErc20RewardsDelegate.sol";
 
 contract CErc20RewardsDelegateMorpho is CErc20RewardsDelegate {
-  uint256 constant MINT_WINDOW = 3 * 24 * 60 * 60;
-
   event RewardsClaimedAndSet(address indexed account, address indexed reward, uint256 claimedAmount);
 
   /**
@@ -13,20 +11,13 @@ contract CErc20RewardsDelegateMorpho is CErc20RewardsDelegate {
    * @param rewardToken The reward strategy for which the rewards are being claimed.
    * @param claimable The amount of tokens to claim and forward.
    * @param proof The proof required to validate the claim.
-   * @param flywheelRewards The flywheel rewards contract to forward rewards to.
    * @dev Only callable by the governance.
    */
-  function claim(address rewardToken, uint256 claimable, bytes32[] memory proof, address flywheelRewards) external {
+  function claim(address rewardToken, uint256 claimable, bytes32[] memory proof) external {
     uint256 claimedAmount = IMorphoClaim(morphoURD).claim(address(this), rewardToken, claimable, proof);
-    EIP20Interface(rewardToken).approve(flywheelRewards, claimedAmount);
-    IFlywheelInstantRewards(flywheelRewards).setRewards(address(this), claimedAmount);
-    ICTokenMinter(cTokenMinter).setMintWindow(block.timestamp, block.timestamp + MINT_WINDOW);
+    EIP20Interface(rewardToken).transfer(comptroller.admin(), claimedAmount);
     emit RewardsClaimedAndSet(address(this), rewardToken, claimedAmount);
   }
-}
-
-interface IFlywheelInstantRewards {
-  function setRewards(address _strategy, uint256 _amount) external;
 }
 
 interface IMorphoClaim {
@@ -36,8 +27,4 @@ interface IMorphoClaim {
     uint256 claimable,
     bytes32[] memory proof
   ) external returns (uint256 amount);
-}
-
-interface ICTokenMinter {
-  function setMintWindow(uint256 _newStartTime, uint256 _newEndTime) external;
 }
