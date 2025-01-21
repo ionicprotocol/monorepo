482ecaa2b2e331697fda74958129b97baadd5a7d
Jourdan Dunkley <chasejamaica@yahoo.com>
2025-01-21 10:42:23 -0500

    contracts as sources in hardhat config

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -50,7 +50,7 @@ const config: HardhatUserConfig = {
     contracts: [{ artifacts: "./out" }]
   },
   paths: {
-    sources: "./contracts/veION",
+    sources: "./contracts",
     tests: "./contracts/test",
     artifacts: "./artifacts"
   },
