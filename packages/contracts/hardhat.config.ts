e51d49a2cfa4b8dbc751c6b028bad0a11c3b6b19
Rahul Sethuram <rahul.eth@icloud.com>
2025-01-24 15:35:00 +0400

    feat: deploy

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -50,7 +50,7 @@ const config: HardhatUserConfig = {
     contracts: [{ artifacts: "./out" }]
   },
   paths: {
-    sources: "./contracts",
+    sources: "./contracts/src",
     tests: "./contracts/test",
     artifacts: "./artifacts"
   },
