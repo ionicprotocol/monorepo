ddc7cdef0366803f3089ed2ea9e28e6e8c7202e8
vidvidvid <weetopol@gmail.com>
2025-02-25 14:40:05 +0100

    lint fix

--- packages/ui/components/markets/PoolToggle.tsx
+++ packages/ui/components/markets/PoolToggle.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import dynamic from 'next/dynamic';
 import Link from 'next/link';
 import { usePathname, useSearchParams } from 'next/navigation';
--- packages/ui/context/VeIonVoteContext.tsx
+++ packages/ui/context/VeIonVoteContext.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { createContext, useContext, useMemo } from 'react';
 
 import { useSearchParams } from 'next/navigation';
