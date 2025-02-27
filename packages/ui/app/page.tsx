3d44dc5b488e2080376398e7655612e9054c7728
vidvidvid <weetopol@gmail.com>
2025-02-27 10:51:15 +0100

    default to base network

--- packages/ui/app/page.tsx
+++ packages/ui/app/page.tsx
@@ -9,7 +9,7 @@ import ResultHandler from '../components/ResultHandler';
 const Home = () => {
   const router = useRouter();
   useEffect(() => {
-    router.push('/market?chain=34443&pool=0');
+    router.push('/market?chain=8453&pool=0');
   }, [router]);
   return (
     <div className={`flex items-center justify-center w-full min-h-[70vh]`}>
