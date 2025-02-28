b7f4af0e3c1a5122560fc034732214e4c97dc7f2
amish kohli <amishkohli@amishs-Laptop.local>
2025-02-28 16:13:03 +0530

    fix apy cap

--- packages/functions/src/controllers/asset-master-data.ts
+++ packages/functions/src/controllers/asset-master-data.ts
@@ -14,6 +14,7 @@ const FLYWHEEL_TYPE_MAP: Record<number, { supply?: string[], borrow?: string[] }
     borrow: []
   }
 };
+// merge in main branch
 interface AssetMasterData {
   // Key identifiers
   chain_id: SupportedChains;
@@ -208,9 +209,9 @@ export const updateAssetMasterData = async (chainId: SupportedChains) => {
                     });
                     
                     if (boosterAddress === '0x0000000000000000000000000000000000000000') {
-                      rewardApySupply += Math.min(apyForMarket * 100, 1000);
+                      rewardApySupply += apyForMarket * 100;
                     } else {
-                      rewardApyBorrow += Math.min(apyForMarket * 100, 1000);
+                      rewardApyBorrow += apyForMarket * 100;
                     }
                     rewardTokens.add(reward.rewardToken.toLowerCase());
                   }
